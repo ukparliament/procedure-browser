480b60fdf87aa2ee84bef760848a888d7f8c3dfe
fantasticlife <michael@smethur.st>
2024-10-02 19:21:15 +0100

    added config.autoloader = :classic

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module ProcedureBrowser
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    config.autoloader = :classic
   end
 end
