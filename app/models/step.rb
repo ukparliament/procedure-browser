a5da2b6f9aadc94eccb7b028a6ab195c5052be0e
fantasticlife <michael@smethur.st>
2024-10-03 13:03:39 +0100

    added more attribute accessors

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,6 +2,14 @@ class Step
   
   attr_accessor :identifier
   attr_accessor :label
+  attr_accessor :description
+  attr_accessor :scope_note
+  attr_accessor :link_note
+  attr_accessor :date_note
+  attr_accessor :publication_name
+  attr_accessor :publication_url
+  attr_accessor :step_type_identifier
+  attr_accessor :step_type_label
   
   def id
     self.identifier.split( '/' ).last
