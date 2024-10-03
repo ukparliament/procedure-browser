d1eeccacb25fb00df5da77425fc67448033ef0f5
fantasticlife <michael@smethur.st>
2024-10-03 11:10:23 +0100

    renamed

--- lib/sparql/get/response.rb
+++ lib/sparql/get/response.rb
@@ -1,17 +1,18 @@
-module GET_SPARQL_RESPONSE
+# We include code required.
+require 'uri'
+require 'net/http'
+require 'csv'
 
-  Sparql::GetResponse = true
+module GET_RESPONSE
 
-  require 'uri'
-  require 'net/http'
-  require 'csv'
-
+  Sparql::Get::Response = true
+  
   # A method to get a response from SPARQL as a CSV.
   def get_sparql_response_as_csv( request_body )
   
     # We get the response from the SPARQL query.
     response = Net::HTTP.post( $SPARQL_REQUEST_URI, request_body, $SPARQL_REQUEST_HEADERS )
-
+    
     # We parse the body of the response as CSV.
     CSV.parse( response.body, headers: true )
   end
