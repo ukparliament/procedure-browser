526264fb97c64d1ff8005e40ef5f212caf71b5b5
fantasticlife <michael@smethur.st>
2024-10-03 13:05:33 +0100

    populated step object

--- lib/sparql/get/step.rb
+++ lib/sparql/get/step.rb
@@ -18,5 +18,28 @@ module GET_STEP
   
     # We get the SPARQL response as a CSV.
     csv = get_sparql_response_as_csv( request_body )
+    
+    # We take the one and only row from the CSV ...
+    csv.take( 1 ).each do |row|
+    
+    
+    puts row
+    
+      # ... and create a new step object.
+      step = Step.new
+      step.identifier = row['Step']
+      step.label = row['stepName']
+      step.description = row['description']
+      step.scope_note = row['scopeNote']
+      step.link_note = row['linkNote']
+      step.date_note = row['dateNote']
+      step.publication_name = row['publicationName']
+      step.publication_url = row['publicationUrl']
+      step.step_type_identifier = row['stepType']
+      step.step_type_label = row['stepTypeLabel']
+      
+      # We return the step object.
+      return step
+    end
   end
 end
