dfdbcf6a7ebb8f826d4fd218841326ff409eef92
fantasticlife <michael@smethur.st>
2024-10-04 21:45:43 +0100

    creates crumb container

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
 
   $SPARQL_REQUEST_URI = URI( 'https://api.parliament.uk/sparql' )
   $SPARQL_REQUEST_HEADERS = { 'Content-Type': 'application/sparql-query' }
+  
+
+  before_action do
+    #expires_in 3.minutes, :public => true
+    create_crumb_container
+  end
+  
+  def create_crumb_container
+    @crumb = []
+  end
 end
