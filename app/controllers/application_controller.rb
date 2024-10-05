0388923381e511137c6a1499306e21cdc6ed354a
fantasticlife <michael@smethur.st>
2024-10-05 10:34:37 +0100

    creates an empty queries array. used to display sarql queries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,14 @@ class ApplicationController < ActionController::Base
   before_action do
     #expires_in 3.minutes, :public => true
     create_crumb_container
+    create_queries_container
   end
   
   def create_crumb_container
     @crumb = []
+  end
+  
+  def create_queries_container
+    @queries = []
   end
 end
