906764de1a8f92b7f94c6c7143993951f9c837d2
fantasticlife <michael@smethur.st>
2024-10-05 10:33:30 +0100

    addes sparql query request body to array of queries

--- lib/sparql/get/response.rb
+++ lib/sparql/get/response.rb
@@ -10,6 +10,9 @@ module GET_RESPONSE
   # A method to get a response from SPARQL as a CSV.
   def get_sparql_response_as_csv( request_body )
   
+    # We add the SPARQL query request body to the array of queries.
+    @queries << request_body
+  
     # We get the response from the SPARQL query.
     response = Net::HTTP.post( $SPARQL_REQUEST_URI, request_body, $SPARQL_REQUEST_HEADERS )
     
