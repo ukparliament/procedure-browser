0eb9fa217c4c65556e4c8136377b128070e74e81
fantasticlife <michael@smethur.st>
2024-10-08 16:27:44 +0100

    single query to frab step type and its steps

--- app/controllers/step_type_controller.rb
+++ app/controllers/step_type_controller.rb
@@ -1,12 +1,12 @@
 # We require the module files for getting step types and a step type.
 require 'sparql/get/step_types'
-require 'sparql/get/step_type'
+require 'sparql/get/step_type_steps'
 
 class StepTypeController < ApplicationController
 
   # We include the get step types and get step type modules.
   include GET_STEP_TYPES
-  include GET_STEP_TYPE
+  include GET_STEP_TYPE_STEPS
 
   def index
     @step_types = get_step_types
@@ -19,7 +19,12 @@ class StepTypeController < ApplicationController
   
   def show
     step_type_id = params[:step_type]
-    @step_type = get_step_type( step_type_id )
+    @step_type_steps = get_step_type_steps( step_type_id )
+    
+    @step_type = StepType.new
+    @step_type.identifier = @step_type_steps.first.step_type_identifier
+    @step_type.label = @step_type_steps.first.step_type_label
+    @step_type.description = @step_type_steps.first.step_type_description
     
     @page_title = @step_type.label
     @description = "#{@step_type.description}."
