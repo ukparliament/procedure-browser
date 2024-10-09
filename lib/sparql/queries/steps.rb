a1d4ac74f98faf26532254eacf86828845b477d9
fantasticlife <michael@smethur.st>
2024-10-09 13:17:13 +0100

    new query from jayne

--- lib/sparql/queries/steps.rb
+++ lib/sparql/queries/steps.rb
@@ -9,12 +9,19 @@ module STEPS_QUERY
       PREFIX : <https://id.parliament.uk/schema/>
       PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
       PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-      SELECT ?Step ?stepName  WHERE {
+      SELECT ?Step ?stepName ?stepType ?stepTypeName ?legislature ?legislatureName ?CommonsId ?LordsId  WHERE {
 
         ?Step a :ProcedureStep;
-                  :name ?stepName.
-
-      } order by asc(UCASE(str(?stepName)))
+                  :name ?stepName;
+                  :procedureStepHasProcedureStepType ?stepType.
+        ?stepType :name ?stepTypeName.
+      Optional {?Step :procedureStepInLegislature ?legislature.
+          ?legislature :name ?legislatureName.}
+        Optional {?Step :procedureStepHasHouse ?CommonsId.
+              filter (?CommonsId IN (id:1AFu55Hs))}
+        Optional {?Step :procedureStepHasHouse ?LordsId.
+             filter (?LordsId IN (id:WkUWUBMx))}
+      } order by ?stepName ?stepTypeName
     "
   end
 end
