23a7f0316580fcb291340de238a7f5e9617c9f0b
fantasticlife <michael@smethur.st>
2024-10-10 13:04:20 +0100

    added date display format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 
   $SPARQL_REQUEST_URI = URI( 'https://api.parliament.uk/sparql' )
   $SPARQL_REQUEST_HEADERS = { 'Content-Type': 'application/sparql-query' }
+  $DATE_DISPLAY_FORMAT = '%-d %B %Y'
   
 
   before_action do
