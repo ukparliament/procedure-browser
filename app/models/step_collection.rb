cca54037999ddd0f99cf416ab1b0c83a4ee932e2
fantasticlife <michael@smethur.st>
2024-10-10 23:14:49 +0100

    basic model

---
+++ app/models/step_collection.rb
@@ -0,0 +1,18 @@
+class StepCollection
+  
+  attr_accessor :identifier
+  attr_accessor :label
+  attr_accessor :procedure_identifier
+  attr_accessor :procedure_label
+  attr_accessor :house_identifier
+  attr_accessor :house_label
+  attr_accessor :membership_identifier
+  
+  def id
+    self.identifier.split( '/' ).last
+  end
+  
+  def procedure_id
+    self.procedure_identifier.split( '/' ).last
+  end
+end
