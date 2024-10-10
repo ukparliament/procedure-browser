ccdf1e6e64ae8540956fed7852eef6252a7b576a
fantasticlife <michael@smethur.st>
2024-10-10 13:09:12 +0100

    new query from jayne removing step stuff

--- lib/sparql/queries/step_work_packageable_things.rb
+++ lib/sparql/queries/step_work_packageable_things.rb
@@ -9,27 +9,20 @@ module STEP_WORK_PACKAGEABLE_THINGS_QUERY
       PREFIX : <https://id.parliament.uk/schema/>
       PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
       PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-      SELECT ?Step ?stepName ?stepType ?stepTypeLabel ?legislature ?legislatureName ?CommonsId ?LordsId ?workPackage ?procedure ?procedureName ?workPackagedThing ?workpackagedThingName WHERE {
+      SELECT ?businessItemDate ?workPackage ?procedure ?procedureName ?workPackagedThing ?workpackagedThingName WHERE {
 
         ?Step a :ProcedureStep;
                   :name ?stepName;
-                  :procedureStepHasBusinessItem ?businessItem;
-                  :procedureStepHasProcedureStepType ?stepType.
+                  :procedureStepHasBusinessItem ?businessItem.
           filter (?Step IN (id:#{step_id}))
-             ?stepType :name ?stepTypeLabel.
-        optional {?Step :procedureStepInLegislature ?legislature.
-                ?legislature :name ?legislatureName.}
-        optional {?Step:procedureStepHasHouse ?CommonsId.
-                    filter (?CommonsId IN (id:1AFu55Hs))}
-        optional {?Step :procedureStepHasHouse ?LordsId.
-          filter (?LordsId IN (id:WkUWUBMx))}
+        optional{ ?businessItem :businessItemDate ?businessItemDate.}
+        optional {?businessItem :businessItemHasBusinessItemWebLink ?businessItemLink }
         ?businessItem :businessItemHasWorkPackage ?workPackage.
         ?workPackage :workPackageHasProcedure ?procedure;
                      :workPackageHasWorkPackagedThing ?workPackagedThing.
         ?procedure :name ?procedureName.
         ?workPackagedThing :name ?workpackagedThingName. 
-
-      } order by ?stepName ?procedureName ?workpackagedThingName
+      } order by DESC(?businessItemDate)  ?workpackagedThingName
     "
   end
 end
