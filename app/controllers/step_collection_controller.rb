4cf7314b6aeba320e394a01f61369c70f6c79013
fantasticlife <michael@smethur.st>
2024-10-22 19:24:59 +0100

    added sep collection index method

--- app/controllers/step_collection_controller.rb
+++ app/controllers/step_collection_controller.rb
@@ -1,5 +1,21 @@
 class StepCollectionController < ApplicationController
 
+  # We include modules required to get all step collections and a step collection.
+  include Sparql::Get::StepCollections
+  include Sparql::Queries::StepCollections
+  #include Sparql::Get::StepCollection
+  #include Sparql::Queries::StepCollection
+  include Sparql::Get::Response
+  
+  def index
+    @step_collections = get_step_collections
+    
+    @page_title = 'Step collections'
+    @description = 'Collections of steps.'
+    @crumb << { label: 'Step collections', url: nil }
+    @section = 'step-collections'
+  end
+  
   def show
     @page_title = 'Coming soon'
     render :template => 'under_construction/notice'
