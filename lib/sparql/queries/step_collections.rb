2444c5cecb9b78ee237b3d9037a557c21c973358
fantasticlife <michael@smethur.st>
2024-10-22 19:23:01 +0100

    new query to get all step collections from jayne

---
+++ lib/sparql/queries/step_collections.rb
@@ -0,0 +1,22 @@
+module Sparql::Queries::StepCollections
+
+  # A SPARQL query to get all step collections.
+  def step_collections_query
+    "
+      PREFIX id: <https://id.parliament.uk/>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      SELECT ?StepCollection ?stepCollectionName ?procedure ?procedureName ?House ?HouseName  WHERE {
+
+        ?StepCollection a :ProcedureStepCollection;
+                  :name ?stepCollectionName.
+      optional {?StepCollection :procedureStepCollectionHasProcedure ?procedure.
+          ?procedure :name ?procedureName.}
+      optional {?StepCollection :procedureStepCollectionHasHouse ?House.
+          ?House :name ?HouseName.}
+
+      }
+    "
+  end
+end
