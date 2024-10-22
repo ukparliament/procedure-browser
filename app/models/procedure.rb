21ba03d71b51129a22e4c1e9eef8f6fae7708b74
fantasticlife <michael@smethur.st>
2024-10-22 09:12:09 +0100

    added attribue accessor for step display depth value

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -3,6 +3,7 @@ class Procedure
   attr_accessor :identifier
   attr_accessor :label
   attr_accessor :description
+  attr_accessor :step_display_depth_value
   
   def id
     self.identifier.split( '/' ).last
