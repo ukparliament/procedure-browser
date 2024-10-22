b2ef41acb025d26999c797fb14eb9f1c7ff26ba3
fantasticlife <michael@smethur.st>
2024-10-22 13:06:57 +0100

    added means to get an array of routes

--- app/controllers/step_route_controller.rb
+++ app/controllers/step_route_controller.rb
@@ -3,14 +3,14 @@ class StepRouteController < ApplicationController
   # We include modules required to get a step and the procedures for a step.
   include Sparql::Get::Step
   include Sparql::Queries::Step
-  #include Sparql::Get::StepRoutes
-  #include Sparql::Queries::StepRoutes
+  include Sparql::Get::StepRoutes
+  include Sparql::Queries::StepRoutes
   include Sparql::Get::Response
 
   def index
     step_id = params[:step]
     @step = get_step( step_id )
-    #@step_procedures = get_step_procedures( step_id )
+    @step_routes = get_step_routes( step_id )
     
     @page_title = "#{@step.label} - Routes"
     @multiline_page_title = "#{@step.label} <span class='subhead'>Routes</span>".html_safe
