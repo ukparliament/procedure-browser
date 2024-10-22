0492fa88715f6458a9ce8180d9a7552d76004998
fantasticlife <michael@smethur.st>
2024-10-22 13:08:05 +0100

    basic model

---
+++ app/models/route.rb
@@ -0,0 +1,48 @@
+class Route
+  
+  attr_accessor :identifier
+  attr_accessor :start_on
+  attr_accessor :end_on
+  attr_accessor :procedure_identifier
+  attr_accessor :procedure_label
+  attr_accessor :from_step_identifier
+  attr_accessor :from_step_label
+  attr_accessor :from_step_type_identifier
+  attr_accessor :from_step_type_label
+  attr_accessor :from_step_commons_identifier
+  attr_accessor :from_step_lords_identifier
+  attr_accessor :from_step_legislature_identifier
+  attr_accessor :from_step_legislature_label
+  attr_accessor :to_step_identifier
+  attr_accessor :to_step_label
+  attr_accessor :to_step_type_identifier
+  attr_accessor :to_step_type_label
+  attr_accessor :to_step_commons_identifier
+  attr_accessor :to_step_lords_identifier
+  attr_accessor :to_step_legislature_identifier
+  attr_accessor :to_step_legislature_label
+  
+  def id
+    self.identifier.split( '/' ).last
+  end
+  
+  def procedure_id
+    self.procedure_identifier.split( '/' ).last
+  end
+  
+  def from_step_id
+    self.from_step_identifier.split( '/' ).last
+  end
+  
+  def from_step_type_id
+    self.from_step_type_identifier.split( '/' ).last
+  end
+  
+  def to_step_id
+    self.to_step_identifier.split( '/' ).last
+  end
+  
+  def to_step_type_id
+    self.to_step_type_identifier.split( '/' ).last
+  end
+end
