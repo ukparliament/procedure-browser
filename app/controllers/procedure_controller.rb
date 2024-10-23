ab9848cc63274422bc411225a4dbff04f04ff2fc
fantasticlife <michael@smethur.st>
2024-10-23 13:50:02 +0100

    code to get all procedures

--- app/controllers/procedure_controller.rb
+++ app/controllers/procedure_controller.rb
@@ -1,5 +1,21 @@
 class ProcedureController < ApplicationController
 
+  # We include modules required to get all procedures and a procedure.
+  include Sparql::Get::Procedures
+  include Sparql::Queries::Procedures
+  #include Sparql::Get::Procedure
+  #include Sparql::Queries::Procedure
+  include Sparql::Get::Response
+
+  def index
+    @procedures = get_procedures
+    
+    @page_title = 'Procedures'
+    @description = 'Procedures.'
+    @crumb << { label: 'Procedures', url: nil }
+    @section = 'procedures'
+  end
+
   def show
     @page_title = 'Coming soon'
     render :template => 'under_construction/notice'
