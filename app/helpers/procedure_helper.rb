d0926a1c446f72ddf07274c3ed1691f846498061
fantasticlife <michael@smethur.st>
2024-10-26 15:52:03 +0100

    added procedure dates helper method

--- app/helpers/procedure_helper.rb
+++ app/helpers/procedure_helper.rb
@@ -1,2 +1,10 @@
 module ProcedureHelper
+
+  def procedure_dates( procedure )
+    procedure_dates = ''
+    procedure_dates += procedure.start_on.strftime( $DATE_DISPLAY_FORMAT ) if procedure.start_on
+    procedure_dates += ' - ' if procedure.start_on
+    procedure_dates += procedure.end_on.strftime( $DATE_DISPLAY_FORMAT ) if procedure.end_on
+    procedure_dates
+  end
 end
