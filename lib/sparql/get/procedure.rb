281b45f207d7d8383479048c4173e5efce525282
fantasticlife <michael@smethur.st>
2024-10-26 15:49:24 +0100

    new model to get a procedure

---
+++ lib/sparql/get/procedure.rb
@@ -0,0 +1,29 @@
+module Sparql::Get::Procedure
+
+  # A method to get a procedure.
+  def get_procedure( procedure_id )
+  
+    # We get the procedure query.
+    request_body = procedure_query( procedure_id )
+  
+    # We get the SPARQL response as a CSV.
+    csv = get_sparql_response_as_csv( request_body )
+    
+    # We take the one and only row from the CSV ...
+    csv.take( 1 ).each do |row|
+    
+      # ... and create a new procedure object.
+      procedure = Procedure.new
+      procedure.identifier = row['Procedure']
+      procedure.label = row['ProcedureName']
+      procedure.description = row['ProcedureDescription']
+      procedure.start_on = row['StartDate'].to_date if row['StartDate']
+      procedure.end_on = row['EndDate'].to_date if row['EndDate']
+      procedure.calculation_style_identifer = row['procedureCalculationStyle']
+      procedure.calculation_style_label = row['procedureCalculationStyleName']
+      
+      # We return the procedure object.
+      return procedure
+    end
+  end
+end
