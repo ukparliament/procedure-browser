9950a2ce282cde63c52cbdab8aede7e7d2ee532c
fantasticlife <michael@smethur.st>
2024-11-06 19:25:19 +0000

    added default results per page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   $SPARQL_REQUEST_URI = URI( 'https://api.parliament.uk/sparql' )
   $SPARQL_REQUEST_HEADERS = { 'Content-Type': 'application/sparql-query' }
   $DATE_DISPLAY_FORMAT = '%-d %B %Y'
+  $DEFAULT_RESULTS_PER_PAGE = 20
   
 
   before_action do
