727226f7039c3bc4f69bdd9d7ccfa35ee5c623ec
fantasticlife <michael@smethur.st>
2024-11-07 19:05:40 +0000

    new modules to query for and get a work package

--- lib/sparql/get/work_package.rb
+++ lib/sparql/get/work_package.rb
@@ -7,16 +7,29 @@ module Sparql::Get::WorkPackage
     request_body = work_package_query( work_package_id )
   
     # We get the SPARQL response as a CSV.
-    #csv = get_sparql_response_as_csv( request_body )
+    csv = get_sparql_response_as_csv( request_body )
     
     # We take the one and only row from the CSV ...
-    #csv.take( 1 ).each do |row|
+    csv.take( 1 ).each do |row|
     
       # ... and create a new work package object.
-      #work_package = WorkPackage.new
+      work_package = WorkPackage.new
+      work_package.identifier = row['workPackage']
+      work_package.work_packageable_thing_identifer = row['Paper']
+      work_package.work_packageable_thing_label = row['Papername']
+      work_package.making_available_identifier = row['laying']
+      work_package.made_available_on = row['laidDate'].to_date if row['laidDate']
+      work_package.procedure_identifier = row['procedure']
+      work_package.procedure_label = row['procedureName']
+      work_package.calculation_style_identifier = row['calculationStyle']
+      work_package.calculation_style_label = row['calculationStyleName']
+      
+      
+      
+      
       
       # We return the work package object.
-      #return work_package
-    #end
+      return work_package
+    end
   end
 end
--- lib/sparql/queries/work_package.rb
+++ lib/sparql/queries/work_package.rb
@@ -3,7 +3,24 @@ module Sparql::Queries::WorkPackage
   # A SPARQL query to get a work package.
   def work_package_query( work_package_id )
     "
-      
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX id: <https://id.parliament.uk/>
+      SELECT * where {
+  
+       ?Paper a :WorkPackagedThing .  
+           ?Paper :name ?Papername ;
+           :workPackagedThingHasWorkPackage ?workPackage.
+         optional {?Paper :laidThingHasLaying ?laying.
+          ?laying :layingDate ?laidDate;
+                  :businessItemHasProcedureStep ?step.
+          filter (?step in (id:isWn7s3K, id:cspzmb6w))}
+          ?workPackage :workPackageHasProcedure ?procedure.
+        ?procedure :name ?procedureName.
+       optional { ?workPackage :workPackageHasCalculationStyle ?calculationStyle. 
+          ?calculationStyle :name ?calculationStyleName. }
+        filter (?workPackage in (id:#{work_package_id}))
+      } 
     "
   end
 end
