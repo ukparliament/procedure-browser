08fb6a463c8ae26eade8d569f27abe37ffd3f042
fantasticlife <michael@smethur.st>
2024-11-07 19:05:10 +0000

    new modules to query for and get a work package count

--- lib/sparql/get/work_package_count.rb
+++ lib/sparql/get/work_package_count.rb
@@ -7,16 +7,13 @@ module Sparql::Get::WorkPackageCount
     request_body = work_package_count_query
   
     # We get the SPARQL response as a CSV.
-    #csv = get_sparql_response_as_csv( request_body )
+    csv = get_sparql_response_as_csv( request_body )
     
     # We take the one and only row from the CSV ...
-    #csv.take( 1 ).each do |row|
-    
-      # ... and create a new work package object.
-      #work_package = WorkPackage.new
+    csv.take( 1 ).each do |row|
       
-      # We return the work package object.
-      #return work_package
-    #end
+      # We return the count.
+      return row[0].to_i
+    end
   end
 end
--- lib/sparql/queries/work_package_count.rb
+++ lib/sparql/queries/work_package_count.rb
@@ -3,6 +3,21 @@ module Sparql::Queries::WorkPackageCount
   # A SPARQL query to get a count of all work packages.
   def work_package_count_query
     "
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX id: <https://id.parliament.uk/>
+      select distinct  (COUNT(DISTINCT ?workPackage) AS ?count) where {
+
+       ?Paper a :WorkPackagedThing .  
+           ?Paper :name ?Papername ;
+           :workPackagedThingHasWorkPackage ?workPackage.
+        optional {?Paper :laidThingHasLaying ?laying.
+          ?laying :layingDate ?laidDate}
+          ?workPackage :workPackageHasProcedure ?procedure.
+        ?procedure :name ?procedureName.
+       optional { ?workPackage :workPackageHasCalculationStyle ?calculationStyle. 
+          ?calculationStyle :name ?calculationStyleName. }
+         }    
       
     "
   end
