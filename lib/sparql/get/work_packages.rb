84a412cab83e76acfe6513f8c2e70138ee99fe4e
fantasticlife <michael@smethur.st>
2024-11-07 19:06:06 +0000

    new modules to query for and get a list of work packages

--- lib/sparql/get/work_packages.rb
+++ lib/sparql/get/work_packages.rb
@@ -10,20 +10,29 @@ module Sparql::Get::WorkPackages
     request_body = work_packages_query( results_per_page, result_offset )
   
     # We get the SPARQL response as a CSV.
-    #csv = get_sparql_response_as_csv( request_body )
+    csv = get_sparql_response_as_csv( request_body )
   
     # We construct an array to hold the work packages.
     work_packages = []
   
     # For each row in the CSV ...
-    #csv.each do |row|
+    csv.each do |row|
   
       # ... we create a new work package object ...
-      #work_package = WorkPackage.new
+      work_package = WorkPackage.new
+      work_package.identifier = row['workPackage']
+      work_package.work_packageable_thing_identifer = row['Paper']
+      work_package.work_packageable_thing_label = row['Papername']
+      work_package.making_available_identifier = row['laying']
+      work_package.made_available_on = row['laidDate'].to_date if row['laidDate']
+      work_package.procedure_identifier = row['procedure']
+      work_package.procedure_label = row['procedureName']
+      work_package.calculation_style_identifier = row['calculationStyle']
+      work_package.calculation_style_label = row['calculationStyleName']
       
       # ... and add it to the array of work packages.
-      #work_packages << work_package
-    #end
+      work_packages << work_package
+    end
   
     # We return the array of work packages.
     work_packages
--- lib/sparql/queries/work_packages.rb
+++ lib/sparql/queries/work_packages.rb
@@ -3,7 +3,25 @@ module Sparql::Queries::WorkPackages
   # A SPARQL query to get all work packages taking parameters of limit and offset.
   def work_packages_query( limit, offset )
     "
-      
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX id: <https://id.parliament.uk/>
+      SELECT * where {
+  
+       ?Paper a :WorkPackagedThing .  
+           ?Paper :name ?Papername ;
+           :workPackagedThingHasWorkPackage ?workPackage.
+        optional {?Paper :laidThingHasLaying ?laying.
+          ?laying :layingDate ?laidDate;
+                  :businessItemHasProcedureStep ?step.
+          filter (?step in (id:isWn7s3K, id:cspzmb6w))}
+          ?workPackage :workPackageHasProcedure ?procedure.
+        ?procedure :name ?procedureName.
+       optional { ?workPackage :workPackageHasCalculationStyle ?calculationStyle. 
+          ?calculationStyle :name ?calculationStyleName. }
+      } Order by desc (?laidDate)
+
+      LIMIT #{limit} OFFSET #{offset}
     "
   end
 end
