453d397930e485eb503dc76b2a9cdc48c8bf2170
fantasticlife <michael@smethur.st>
2024-11-12 15:47:38 +0000

    new all work packages query to include presentation date

--- lib/sparql/queries/work_packages.rb
+++ lib/sparql/queries/work_packages.rb
@@ -3,25 +3,25 @@ module Sparql::Queries::WorkPackages
   # A SPARQL query to get all work packages taking parameters of limit and offset.
   def work_packages_query( limit, offset )
     "
-      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-      PREFIX : <https://id.parliament.uk/schema/>
-      PREFIX id: <https://id.parliament.uk/>
-      SELECT * where {
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX : <https://id.parliament.uk/schema/>
+        PREFIX id: <https://id.parliament.uk/>
+        SELECT * where {
   
-       ?Paper a :WorkPackagedThing .  
-           ?Paper :name ?Papername ;
-           :workPackagedThingHasWorkPackage ?workPackage.
-        optional {?Paper :laidThingHasLaying ?laying.
-          ?laying :layingDate ?laidDate;
-                  :businessItemHasProcedureStep ?step.
-          filter (?step in (id:isWn7s3K, id:cspzmb6w))}
-          ?workPackage :workPackageHasProcedure ?procedure.
-        ?procedure :name ?procedureName.
-       optional { ?workPackage :workPackageHasCalculationStyle ?calculationStyle. 
-          ?calculationStyle :name ?calculationStyleName. }
-      } Order by desc (?laidDate)
+         ?Paper a :WorkPackagedThing .  
+             ?Paper :name ?Papername ;
+             :workPackagedThingHasWorkPackage ?workPackage.
+          optional {?workPackage :workPackageHasBusinessItem ?event .
+            ?event :businessItemHasProcedureStep ?step;
+                   :businessItemDate ?eventDate.
+            filter (?step in (id:isWn7s3K, id:cspzmb6w, id:ITNO9JWr, id:otscOTzB))}
+            ?workPackage :workPackageHasProcedure ?procedure.
+          ?procedure :name ?procedureName.
+         optional { ?workPackage :workPackageHasCalculationStyle ?calculationStyle. 
+            ?calculationStyle :name ?calculationStyleName. }
+        } Order by desc (?eventDate)
 
-      LIMIT #{limit} OFFSET #{offset}
-    "
+        LIMIT #{limit} OFFSET #{offset}
+      "
   end
 end
