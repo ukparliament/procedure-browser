6dc0b4e3faa7ff58d086d317e55f70b07ffd4a6f
fantasticlife <michael@smethur.st>
2024-11-12 14:03:36 +0000

    new query for events in a work package with step depth

--- lib/sparql/queries/work_package_events.rb
+++ lib/sparql/queries/work_package_events.rb
@@ -11,20 +11,22 @@ module Sparql::Queries::WorkPackageEvents
        ?Paper a :WorkPackagedThing .  
            ?Paper :name ?Papername ;
            :workPackagedThingHasWorkPackage ?workPackage.
-          filter (?workPackage IN (id:#{work_package_id}))
-           ?workPackage :workPackageHasBusinessItem ?event;
-                        :workPackageHasProcedure ?procedure.
+        filter (?workPackage IN (id:#{work_package_id}))
+           ?workPackage :workPackageHasBusinessItem ?event. 
+           ?workPackage :workPackageHasProcedure ?procedure.
          optional {?event :businessItemDate ?eventDate}
         optional {?event :businessItemHasBusinessItemWebLink ?eventLink}
        ?event :businessItemHasProcedureStep ?eventProcedureStep. 
        ?eventProcedureStep :name ?eventProcedureStepName.
-          Optional {?eventProcedureStep :procedureStepInLegislature ?legislature.
+       ?eventProcedureStep :procedureStepHasStepDisplayDepthInProcedure ?stepDepth.
+        ?procedure :procedureHasStepDisplayDepthInProcedure ?stepDepth.
+       ?stepDepth :stepDisplayDepthInProcedureHasDepth ?depthValue.
+        Optional {?eventProcedureStep :procedureStepInLegislature ?legislature.
           ?legislature :name ?legislatureName.}
         Optional {?eventProcedureStep:procedureStepHasHouse ?CommonsId.
               filter (?CommonsId IN (id:1AFu55Hs))}
         Optional {?eventProcedureStep :procedureStepHasHouse ?LordsId.
-          filter (?LordsId IN (id:WkUWUBMx))} }  
-      order by desc(?eventDate)
+          filter (?LordsId IN (id:WkUWUBMx))} }  order by ?eventDate ?depthValue
     "
   end
 end
