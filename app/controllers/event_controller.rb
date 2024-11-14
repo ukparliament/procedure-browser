6e3a126a07c85bb4725b443a4c5e9c489a8ff863
fantasticlife <michael@smethur.st>
2024-11-14 20:24:41 +0000

    added code to render an event list

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,5 +1,29 @@
 class EventController < ApplicationController
 
+  # We include modules required to get all steps and a step.
+  include Sparql::Get::EventSteps
+  include Sparql::Queries::EventSteps
+  include Sparql::Get::Response
+
   def index
+  end
+  
+  def show
+    event_id = params[:event]
+    @event_steps = get_event_steps( event_id )
+    
+    @event = Event.new
+    @event.date = @event_steps.first.event_date
+    @event.link = @event_steps.first.event_link
+    
+    if @event.date
+      @page_title = "An event taking place on #{@event.date.strftime( $DATE_DISPLAY_FORMAT )}"
+      #@description = "An event taking place on #{@event.date.strftime( $DATE_DISPLAY_FORMAT )}."
+    else
+      @page_title = "An event"
+      #@description = "An event."
+    end
+    @crumb << { label: 'Event', url: nil }
+    @section = 'work-packages'
   end
 end
