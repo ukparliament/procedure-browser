953f826613330b41bae7e784b538ffb43e867754
fantasticlife <michael@smethur.st>
2024-11-15 13:21:37 +0000

    query and getter for future events in a work package

---
+++ lib/sparql/queries/work_package_future_events.rb
@@ -0,0 +1,34 @@
+module Sparql::Queries::WorkPackageFutureEvents
+
+  # A SPARQL query to get a work package and its future events.
+  def work_package_future_events_query( work_package_id )
+    "
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX id: <https://id.parliament.uk/>
+      SELECT * where {
+  
+       ?Paper a :WorkPackagedThing .  
+           ?Paper :name ?Papername ;
+           :workPackagedThingHasWorkPackage ?workPackage.
+        filter (?workPackage IN (id:#{work_package_id}))
+           ?workPackage :workPackageHasBusinessItem ?event. 
+           ?workPackage :workPackageHasProcedure ?procedure.
+         optional {?event :businessItemDate ?eventDate}
+        optional {?event :businessItemHasBusinessItemWebLink ?eventLink}
+       ?event :businessItemHasProcedureStep ?eventProcedureStep. 
+       ?eventProcedureStep :name ?eventProcedureStepName.
+       ?eventProcedureStep :procedureStepHasStepDisplayDepthInProcedure ?stepDepth.
+        ?procedure :procedureHasStepDisplayDepthInProcedure ?stepDepth.
+       ?stepDepth :stepDisplayDepthInProcedureHasDepth ?depthValue.
+        Optional {?eventProcedureStep :procedureStepInLegislature ?legislature.
+          ?legislature :name ?legislatureName.}
+        Optional {?eventProcedureStep:procedureStepHasHouse ?CommonsId.
+              filter (?CommonsId IN (id:1AFu55Hs))}
+        Optional {?eventProcedureStep :procedureStepHasHouse ?LordsId.
+          filter (?LordsId IN (id:WkUWUBMx))} 
+      FILTER ( xsd:dateTime(?eventDate) >= now() )}    
+      order by ?eventDate ?depthValue
+    "
+  end
+end
