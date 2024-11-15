5f8be38525e3b8defabd0eeef7d0107595fb618e
fantasticlife <michael@smethur.st>
2024-11-15 13:35:23 +0000

    basic controller to list undated events for a work package

---
+++ app/helpers/work_package_undated_event_helper.rb
@@ -0,0 +1,2 @@
+module WorkPackageUndatedEventHelper
+end
---
+++ test/controllers/work_package_undated_event_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class WorkPackageUndatedEventControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
