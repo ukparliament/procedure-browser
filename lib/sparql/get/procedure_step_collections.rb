82276ec888f14fe1a735abec0d45d2b0ebae7bc9
fantasticlife <michael@smethur.st>
2024-11-19 10:20:57 +0000

    new modules to query for and get step collections for a procedure

---
+++ lib/sparql/get/procedure_step_collections.rb
@@ -0,0 +1,27 @@
+module Sparql::Get::ProcedureStepCollections
+  
+  def get_procedure_step_collections( procedure )
+    
+    # We get the procedure step collections query.
+    request_body = procedure_step_collections_query( procedure )
+    
+    # We get the SPARQL response as a CSV.
+    #csv = get_sparql_response_as_csv( request_body )
+    
+    # We construct an array to hold the step collections.
+    step_collections = []
+  
+    # For each row in the CSV ...
+    #csv.each do |row|
+  
+      # ... we create a new step collection object ...
+      #step_collection = StepCollection.new
+      
+      # ... and add it to the array of step collections.
+      #step_collections << step_collection
+    #end
+  
+    # We return the array of step collections.
+    step_collections
+  end
+end
---
+++ lib/sparql/queries/procedure_step_collections.rb
@@ -0,0 +1,9 @@
+module Sparql::Queries::ProcedureStepCollections
+
+  # A SPARQL query to get step collections for a procedure.
+  def procedure_step_collections_query( procedure_id )
+    "
+      
+    "
+  end
+end
