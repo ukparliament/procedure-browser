33be92844865aedf91b7fc1ad7539455ffdc9548
fantasticlife <michael@smethur.st>
2024-11-19 10:20:21 +0000

    basic controller to list step collections for a procedure

---
+++ app/controllers/procedure_step_collection_controller.rb
@@ -0,0 +1,24 @@
+class ProcedureStepCollectionController < ApplicationController
+
+  # We include modules required to get a procedure and the step types for a procedure.
+  include Sparql::Get::Procedure
+  include Sparql::Queries::Procedure
+  include Sparql::Get::ProcedureStepCollections
+  include Sparql::Queries::ProcedureStepCollections
+  include Sparql::Get::Response
+
+  def index
+    procedure_id = params[:procedure]
+    @procedure = get_procedure( procedure_id )
+    @procedure_step_collections = get_procedure_step_collections( procedure_id )
+    
+    @page_title = "#{@procedure.label} - Step collections"
+    @multiline_page_title = "#{@procedure.label} <span class='subhead'>Step collections</span>".html_safe
+    @description = "Step collections for #{@procedure.label}."
+    @crumb << { label: 'Procedures', url: procedure_list_url }
+    @crumb << { label: @procedure.label, url: procedure_show_url }
+    @crumb << { label: 'Step collections', url: nil }
+    @section = 'procedures'
+    @subsection = 'step-collections'
+  end
+end
---
+++ test/controllers/procedure_step_collection_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ProcedureStepCollectionControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
