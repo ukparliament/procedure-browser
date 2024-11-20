b2618029f7227b91ae44d15456f6025b182fffac
fantasticlife <michael@smethur.st>
2024-11-20 20:25:18 +0000

    new query and getter for a calculation style

---
+++ lib/sparql/get/calculation_style.rb
@@ -0,0 +1,22 @@
+module Sparql::Get::CalculationStyle
+
+  # A method to get a calculation style.
+  def get_calculation_style( calculation_style_id )
+  
+    # We get the calculation style query.
+    #request_body = calculation_style_query( calculation_style_id )
+  
+    # We get the SPARQL response as a CSV.
+    #csv = get_sparql_response_as_csv( request_body )
+    
+    # We take the one and only row from the CSV ...
+    #csv.take( 1 ).each do |row|
+    
+      # ... and create a new calculation style object.
+      #calculation_style = CalculationStyle.new
+      
+      # We return the calculation style object.
+      #return calculation_style
+    #end
+  end
+end
---
+++ lib/sparql/queries/calculation_style.rb
@@ -0,0 +1,9 @@
+module Sparql::Queries::CalculationStyle
+
+  # A SPARQL query to get a calculation style.
+  def calculation_style_query( calculation_style_id )
+    "
+      
+    "
+  end
+end
