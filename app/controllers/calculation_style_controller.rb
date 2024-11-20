9573d9d7311a4e1b000dfef17ebfc00886583f82
fantasticlife <michael@smethur.st>
2024-11-20 20:23:22 +0000

    filled out index and show methods with calls modules

--- app/controllers/calculation_style_controller.rb
+++ app/controllers/calculation_style_controller.rb
@@ -1,7 +1,29 @@
 class CalculationStyleController < ApplicationController
 
+  # We include modules required to get all calculation styles and a calculation style.
+  include Sparql::Get::CalculationStyles
+  include Sparql::Queries::CalculationStyles
+  include Sparql::Get::CalculationStyle
+  include Sparql::Queries::CalculationStyle
+  include Sparql::Get::Response
+  
+  def index
+    @steps = get_calculation_styles
+    
+    @page_title = 'Calculation styles'
+    @description = 'Calculation styles as set out in legislation.'
+    @crumb << { label: 'Calculation styles', url: nil }
+    @section = 'calculation-styles'
+  end
+
   def show
-    @page_title = 'Coming soon'
-    render :template => 'under_construction/notice' 
+    calculation_style_id = params[:calculation_style]
+    @calculation_style = get_calculation_style( calculation_style_id )
+    
+    #@page_title = @calculation_style.label
+    #@description = "#{@calculation_style.label}."
+    @crumb << { label: 'Calculation styles', url: calculation_style_list_url }
+    #@crumb << { label: @calculation_style.label, url: nil }
+    @section = 'calculation-styles'
   end
 end
