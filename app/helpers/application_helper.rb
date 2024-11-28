8384e0496866d36e2b2d3e398802f5e9a0167030
fantasticlife <michael@smethur.st>
2024-11-28 09:27:36 +0000

    added method for list_item_count_sentence_with_actualisation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,6 +14,24 @@ module ApplicationHelper
     list_item_count_sentence = content_tag( 'p', list_item_count_sentence )
   end
   
+  def list_item_count_sentence_with_actualisation( first_type_of_thing, first_number_of_things, second_type_of_thing, second_number_of_things  )
+    list_item_count_sentence = 'There '
+    if first_number_of_things == 1
+      list_item_count_sentence += 'is '
+    else
+      list_item_count_sentence += 'are '
+    end
+    list_item_count_sentence += ActiveSupport::NumberHelper.number_to_delimited( first_number_of_things )
+    list_item_count_sentence += ' '
+    list_item_count_sentence += first_type_of_thing.pluralize( first_number_of_things )
+    list_item_count_sentence += ' actualising '
+    list_item_count_sentence += ActiveSupport::NumberHelper.number_to_delimited( second_number_of_things )
+    list_item_count_sentence += ' '
+    list_item_count_sentence += second_type_of_thing.pluralize( second_number_of_things )
+    list_item_count_sentence += '.'
+    list_item_count_sentence = content_tag( 'p', list_item_count_sentence )
+  end
+  
   def class_display_label( object )
     class_display_label = ''
     words = object.class.name.split /(?=[A-Z])/
