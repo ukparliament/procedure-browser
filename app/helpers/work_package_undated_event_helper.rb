cfd2722238c76d434cbba4578f0e0fa89da9a662
fantasticlife <michael@smethur.st>
2024-11-28 08:39:43 +0000

    work package undaed event controller replaced by work package undated business item controller

--- app/helpers/work_package_undated_event_helper.rb
+++
@@ -1,2 +0,0 @@
-module WorkPackageUndatedEventHelper
-end
--- test/controllers/work_package_undated_event_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class WorkPackageUndatedEventControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
