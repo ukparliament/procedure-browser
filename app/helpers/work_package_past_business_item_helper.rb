ea0deb72ff86c259cb6ef865dd8c8f58eb5d30d5
fantasticlife <michael@smethur.st>
2024-11-28 08:12:30 +0000

    basic controller and views to list past business items for a work package

---
+++ app/helpers/work_package_past_business_item_helper.rb
@@ -0,0 +1,2 @@
+module WorkPackagePastBusinessItemHelper
+end
---
+++ test/controllers/work_package_past_business_item_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class WorkPackagePastBusinessItemControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
