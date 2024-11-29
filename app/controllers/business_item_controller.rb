e24a698feda0a1f94ece03bdfb6c07a06ebb15a5
fantasticlife <michael@smethur.st>
2024-11-29 12:48:34 +0000

    reworked business item controller, now we know a business item always belongs to one work package

--- app/controllers/business_item_controller.rb
+++ app/controllers/business_item_controller.rb
@@ -16,15 +16,31 @@ class BusinessItemController < ApplicationController
     @business_item.identifier = @business_item_steps.first.business_item_identifier
     @business_item.date = @business_item_steps.first.business_item_date
     @business_item.link = @business_item_steps.first.business_item_link
+    @business_item.work_package_identifier = @business_item_steps.first.work_package_identifier
+    @business_item.work_packageable_thing_identifier = @business_item_steps.first.work_packageable_thing_identifier
+    @business_item.work_packageable_thing_label = @business_item_steps.first.work_packageable_thing_label
+    @business_item.work_package_made_available_on = @business_item_steps.first.work_package_made_available_on.to_date if @business_item_steps.first.work_package_made_available_on
+    @business_item.procedure_identifier = @business_item_steps.first.procedure_identifier
+    @business_item.procedure_label = @business_item_steps.first.procedure_label
+    @business_item.calculation_style_identifier = @business_item_steps.first.calculation_style_identifier
+    @business_item.calculation_style_label = @business_item_steps.first.calculation_style_label
     
+    
+    @crumb << { label: 'Work packages', url: work_package_list_url }
+    @crumb << { label: @business_item.work_packageable_thing_label, url: work_package_show_url( :work_package => @business_item.work_package_identifier ) }
     if @business_item.date
-      @page_title = "A business item taking place on #{@business_item.date.strftime( $DATE_DISPLAY_FORMAT )}"
-      @description = "A business item taking place on #{@business_item.date.strftime( $DATE_DISPLAY_FORMAT )}."
+      @page_title = "Work package for #{@business_item.work_packageable_thing_label} - Business item on #{@business_item.date.strftime( $DATE_DISPLAY_FORMAT )}"
+      @multiline_page_title = "#{@business_item.work_packageable_thing_label} <span class='subhead'>Work package - Business item on #{@business_item.date.strftime( $DATE_DISPLAY_FORMAT )}</span>".html_safe
+      @description = "A business item taking place on #{@business_item.date.strftime( $DATE_DISPLAY_FORMAT )} as part of a work package for #{@business_item.work_packageable_thing_label}."
+      @crumb << { label: "Business item on #{@business_item.date.strftime( $DATE_DISPLAY_FORMAT )}", url: nil }
     else
-      @page_title = "A business item"
-      @description = "A business item."
+      @page_title = "Work package for #{@business_item.work_packageable_thing_label} - Business item"
+      @multiline_page_title = "#{@business_item.work_packageable_thing_label} <span class='subhead'>Work package - Business item</span>".html_safe
+      @description = "A business item as part of a work package for #{@business_item.work_packageable_thing_label}."
+      @crumb << { label: 'Business item', url: nil }
     end
-    @crumb << { label: 'Business item', url: nil }
     @section = 'work-packages'
+    
+    render :template => 'business_item_step/index'
   end
 end
