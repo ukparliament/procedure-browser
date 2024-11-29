c4061869fe9fffb492d4cf33375e77e216994b62
fantasticlife <michael@smethur.st>
2024-11-29 12:46:12 +0000

    new business item steps query returing more data for the work package etc

--- lib/sparql/queries/business_item_steps.rb
+++ lib/sparql/queries/business_item_steps.rb
@@ -3,22 +3,33 @@ module Sparql::Queries::BusinessItemSteps
   # A SPARQL query to get steps for a business item.
   def business_item_steps_query( business_item_id )
     "
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX : <https://id.parliament.uk/schema/>
-        PREFIX id: <https://id.parliament.uk/>
-        SELECT ?event ?step ?stepName ?eventDate ?url ?legislature ?legislatureName ?CommonsId ?LordsId WHERE {
-        ?event a :BusinessItem;
-               :businessItemHasProcedureStep ?step.
-          optional {?event :date ?eventDate.}
-          optional {?event :businessItemHasBusinessItemWebLink ?url}
-          ?step :name ?stepName.
-           Optional {?step :procedureStepInLegislature ?legislature.
-            ?legislature :name ?legislatureName.}
-          Optional {?step:procedureStepHasHouse ?CommonsId.
-                filter (?CommonsId IN (id:1AFu55Hs))}
-          Optional {?step :procedureStepHasHouse ?LordsId.
-            filter (?LordsId IN (id:WkUWUBMx))} 
-          filter (?event in (id:#{business_item_id}))
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX id: <https://id.parliament.uk/>
+      SELECT ?businessItem ?workPackage ?workPackagedThing ?workPackagedThingLabel ?procedure ?procedureLabel ?calculationStyle ?calculationStyleLabel ?madeAvailableDate ?businessItemstep ?businessItemStepName ?Date ?url ?legislature ?legislatureName ?CommonsId ?LordsId WHERE {
+      ?businessItem a :BusinessItem;
+                    :businessItemHasWorkPackage ?workPackage;
+             :businessItemHasProcedureStep ?businessItemstep.
+        ?workPackage :workPackageHasProcedure ?procedure;
+                                    :workPackageHasWorkPackagedThing ?workPackagedThing.
+        ?procedure :name ?procedureLabel.
+        ?workPackagedThing :name ?workPackagedThingLabel. 
+        optional {?workPackage :workPackageHasCalculationStyle ?calculationStyle.
+          ?calculationStyle :name ?calculationStyleLabel. }
+        optional {?businessItem :date ?Date.}
+        optional {?businessItem :businessItemHasBusinessItemWebLink ?url}
+        ?businessItemstep :name ?businessItemStepName.
+         Optional {?businessItemstep :procedureStepInLegislature ?legislature.
+          ?legislature :name ?legislatureName.}
+        Optional {?businessItemstep:procedureStepHasHouse ?CommonsId.
+              filter (?CommonsId IN (id:1AFu55Hs))}
+        Optional {?businessItemstep :procedureStepHasHouse ?LordsId.
+          filter (?LordsId IN (id:WkUWUBMx))} 
+        filter (?businessItem in (id:#{business_item_id}))
+        optional {  ?workPackage :workPackageHasBusinessItem ?bi.
+        ?bi :businessItemHasProcedureStep ?procedureStep.
+        ?bi :date ?madeAvailableDate.
+          filter (?procedureStep in (id:cspzmb6w, id:isWn7s3K, id:ITNO9JWr, id:otscOTzB))}
       }
     "
   end
