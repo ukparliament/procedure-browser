2bd710120bfbe6a04b2cc813448796d99795bcfd
fantasticlife <michael@smethur.st>
2024-11-29 13:18:56 +0000

    removed defunct work package event controller

--- app/controllers/work_package_event_controller.rb
+++
@@ -1,2 +0,0 @@
-class WorkPackageEventController < ApplicationController
-end
--- test/controllers/work_package_event_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class WorkPackageEventControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
