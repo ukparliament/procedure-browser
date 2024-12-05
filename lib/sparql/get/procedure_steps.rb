e526e2d28c44e7014b8078ae04b2ec29404ec2cb
fantasticlife <michael@smethur.st>
2024-12-05 21:04:08 +0000

    new query from jayne to get steps in a procedure with a revised get module

--- lib/sparql/get/procedure_steps.rb
+++ lib/sparql/get/procedure_steps.rb
@@ -6,21 +6,31 @@ module Sparql::Get::ProcedureSteps
     request_body = procedure_steps_query( procedure )
     
     # We get the SPARQL response as a CSV.
-    #csv = get_sparql_response_as_csv( request_body )
+    csv = get_sparql_response_as_csv( request_body )
     
     # We construct an array to hold the steps.
     steps = []
   
     # For each row in the CSV ...
-    #csv.each do |row|
+    csv.each do |row|
   
       # ... we create a new step object ...
-      #step = Step.new
+      step = Step.new
+      step.identifier = row['step']
+      step.label = row['stepName']
+      step.procedure_identifier = row['Procedure']
+      step.procedure_label = row['ProcedureName']
+      step.legislature_identifier = row['legislature']
+      step.legislature_label = row['legislatureName']
+      step.commons_identifier = row['CommonsId']
+      step.lords_identifier = row['LordsId']
+      step.step_type_identifier = row['stepType']
+      step.step_type_label = row['stepTypeLabel']
       
       # ... and add it to the array of steps.
-      #steps << step
-    #end
-  
+      steps << step
+    end
+    
     # We return the array of steps.
     steps
   end
--- lib/sparql/queries/procedure_steps.rb
+++ lib/sparql/queries/procedure_steps.rb
@@ -3,6 +3,26 @@ module Sparql::Queries::ProcedureSteps
   # A SPARQL query to get steps for a procedure.
   def procedure_steps_query( procedure_id )
     "
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX id: <https://id.parliament.uk/>
+      SELECT distinct ?Procedure ?ProcedureName ?step ?stepName ?legislature ?legislatureName ?CommonsId ?LordsId ?stepType ?stepTypeLabel WHERE {
+        ?Procedure a :Procedure;
+                   :name ?ProcedureName.
+           filter (?Procedure in (id:#{procedure_id}))
+        optional {  ?Procedure :procedureHasProcedureRoute ?Route.
+          ?Route :procedureRouteIsFromProcedureStep|:procedureRouteIsToProcedureStep ?step.
+          ?step :name ?stepName.
+        ?step :procedureStepHasProcedureStepType ?stepType.
+               ?stepType :name ?stepTypeLabel.}
+          Optional {?step :procedureStepInLegislature ?legislature.
+          ?legislature :name ?legislatureName.}
+          Optional {?step :procedureStepHasHouse ?CommonsId
+          filter (?CommonsId IN (id:1AFu55Hs))}
+          Optional {?step :procedureStepHasHouse ?LordsId.
+            filter (?LordsId IN (id:WkUWUBMx))}
+        } order by ?stepName 
       
     "
   end
