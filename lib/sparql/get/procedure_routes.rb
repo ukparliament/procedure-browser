583c03570ebd7801cb79ec24f173610a06e59b8d
fantasticlife <michael@smethur.st>
2024-12-12 15:33:30 +0000

    populated query and getter for procedure route listing

--- lib/sparql/get/procedure_routes.rb
+++ lib/sparql/get/procedure_routes.rb
@@ -6,20 +6,41 @@ module Sparql::Get::ProcedureRoutes
     request_body = procedure_routes_query( procedure )
     
     # We get the SPARQL response as a CSV.
-    #csv = get_sparql_response_as_csv( request_body )
+    csv = get_sparql_response_as_csv( request_body )
     
     # We construct an array to hold the routes.
     routes = []
   
     # For each row in the CSV ...
-    #csv.each do |row|
+    csv.each do |row|
   
       # ... we create a new route object ...
-      #route = Route.new
+      route = Route.new
+      route.identifier = row['Route']
+      route.start_on = row['StartDate'].to_date if row['StartDate']
+      route.end_on = row['EndDate'].to_date if row['EndDate']
+      route.procedure_identifier = row['Procedure']
+      route.procedure_label = row['ProcedureName']
+      route.from_step_identifier = row['FromStep']
+      route.from_step_label = row['FromStepName']
+      route.from_step_type_identifier = row['FromStepType']
+      route.from_step_type_label = row['FromStepTypeName']
+      route.from_step_commons_identifier = row['FromStepCommonsId']
+      route.from_step_lords_identifier = row['FromStepLordsId']
+      route.from_step_legislature_identifier = row['FromSteplegislature']
+      route.from_step_legislature_label = row['FromSteplegislatureName']
+      route.to_step_identifier = row['ToStep']
+      route.to_step_label = row['ToStepName']
+      route.to_step_type_identifier = row['ToStepType']
+      route.to_step_type_label = row['ToStepTypeName']
+      route.to_step_commons_identifier = row['ToStepCommonsId']
+      route.to_step_lords_identifier = row['ToStepLordsId']
+      route.to_step_legislature_identifier = row['ToSteplegislature']
+      route.to_step_legislature_label = row['ToSteplegislatureName']
       
       # ... and add it to the array of routes.
-      #routes << route
-    #end
+      routes << route
+    end
   
     # We return the array of routes.
     routes
--- lib/sparql/queries/procedure_routes.rb
+++ lib/sparql/queries/procedure_routes.rb
@@ -3,7 +3,39 @@ module Sparql::Queries::ProcedureRoutes
   # A SPARQL query to get routes for a procedure.
   def procedure_routes_query( procedure_id )
     "
-      
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX id: <https://id.parliament.uk/>
+      SELECT * WHERE {
+        ?Procedure a :Procedure;
+                   :name ?ProcedureName.
+         filter (?Procedure in (id:#{procedure_id}))
+        ?Procedure :procedureHasProcedureRoute ?Route. 
+        Optional {  ?Route :startDate ?StartDate. }
+        Optional {?Route :endDate ?EndDate.}
+        ?Route :procedureRouteIsFromProcedureStep ?FromStep. 
+        ?FromStep :name ?FromStepName;
+                  :procedureStepHasProcedureStepType ?FromStepType.
+        ?FromStepType :name ?FromStepTypeName. 
+          Optional {?FromStep :procedureStepInLegislature ?FromSteplegislature.
+          ?FromSteplegislature :name ?FromSteplegislatureName.}
+          Optional {?FromStep:procedureStepHasHouse ?FromStepCommonsId
+          filter (?FromStepCommonsId IN (id:1AFu55Hs))}
+          Optional {?FromStep :procedureStepHasHouse ?FromStepLordsId.
+            filter (?FromStepLordsId IN (id:WkUWUBMx))}
+          ?Route :procedureRouteIsToProcedureStep ?ToStep. 
+        ?ToStep :name ?ToStepName;
+                  :procedureStepHasProcedureStepType ?ToStepType.
+        ?ToStepType :name ?ToStepTypeName. 
+        Optional {?ToStep :procedureStepInLegislature ?ToSteplegislature.
+          ?ToSteplegislature :name ?ToSteplegislatureName.}
+          Optional {?ToStep:procedureStepHasHouse ?ToStepCommonsId
+          filter (?ToStepCommonsId IN (id:1AFu55Hs))}
+          Optional {?ToStep :procedureStepHasHouse ?ToStepLordsId.
+            filter (?ToStepLordsId IN (id:WkUWUBMx))}
+  
+        }
     "
   end
 end
