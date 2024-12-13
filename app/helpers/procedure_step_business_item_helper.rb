7473daef6793eb0e0a5b780dae2fc24a0079e966
fantasticlife <michael@smethur.st>
2024-12-13 12:38:06 +0000

    added procedure_step_business_item_description method

--- app/helpers/procedure_step_business_item_helper.rb
+++ app/helpers/procedure_step_business_item_helper.rb
@@ -1,2 +1,10 @@
 module ProcedureStepBusinessItemHelper
+
+  def procedure_step_business_item_description( business_item )
+    procedure_step_business_item_description = ''
+    if business_item.date
+      procedure_step_business_item_description += "On #{business_item.date.strftime( $DATE_DISPLAY_FORMAT )}."
+    end
+    procedure_step_business_item_description.html_safe
+  end
 end
