f03a0cfc691fbb8106ebb7385be38cbfb2ff4e14
fantasticlife <michael@smethur.st>
2024-12-13 12:39:38 +0000

    new query from jayne for business items for a step in a procedure, including work package stuff

--- lib/sparql/queries/procedure_step_business_items.rb
+++ lib/sparql/queries/procedure_step_business_items.rb
@@ -3,32 +3,38 @@ module Sparql::Queries::ProcedureStepBusinessItems
   # A SPARQL query to get business items for a step in a procedure.
   def procedure_step_business_items_query( procedure_id, step_id )
     "
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX : <https://id.parliament.uk/schema/>
-    PREFIX id: <https://id.parliament.uk/>
-    SELECT distinct ?Procedure ?ProcedureName ?step ?stepName ?legislature ?legislatureName ?CommonsId ?LordsId ?stepType ?stepTypeLabel ?businessItem ?businessItemDate ?businessItemLink  WHERE {
-      ?Procedure a :Procedure;
-                 :name ?ProcedureName.
-         filter (?Procedure in (id:#{procedure_id}))
-      ?Procedure :procedureHasWorkPackage ?workPackage.
-      optional {  ?Procedure :procedureHasProcedureRoute ?Route.
-        ?Route :procedureRouteIsFromProcedureStep|:procedureRouteIsToProcedureStep ?step.
-        ?step :name ?stepName.
-      ?step :procedureStepHasProcedureStepType ?stepType.
-             ?stepType :name ?stepTypeLabel.}
-        Optional {?step :procedureStepInLegislature ?legislature.
-        ?legislature :name ?legislatureName.}
-        Optional {?step :procedureStepHasHouse ?CommonsId
-        filter (?CommonsId IN (id:1AFu55Hs))}
-        Optional {?step :procedureStepHasHouse ?LordsId.
-          filter (?LordsId IN (id:WkUWUBMx))}
-      filter (?step IN (id:#{step_id}))
-     ?step :procedureStepHasBusinessItem ?businessItem.
-      optional{ ?businessItem :businessItemDate ?businessItemDate.}
-            optional {?businessItem :businessItemHasBusinessItemWebLink ?businessItemLink }
-      ?businessItem :businessItemHasWorkPackage ?workPackage.
-       } order by ?stepName
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX id: <https://id.parliament.uk/>
+      SELECT distinct ?businessItem ?businessItemDate ?businessItemLink ?workPackage ?workPackagedThing ?workPackagedThingName ?madeAvailableDate ?Procedure ?ProcedureName ?step ?stepName ?legislature ?legislatureName ?CommonsId ?LordsId ?stepType ?stepTypeLabel  WHERE {
+        ?Procedure a :Procedure;
+                   :name ?ProcedureName.
+           filter (?Procedure in (id:#{procedure_id}))
+        ?Procedure :procedureHasWorkPackage ?workPackage.
+        ?workPackage :workPackageHasWorkPackagedThing ?workPackagedThing.
+        ?workPackagedThing :name ?workPackagedThingName. 
+        optional {  ?Procedure :procedureHasProcedureRoute ?Route.
+          ?Route :procedureRouteIsFromProcedureStep|:procedureRouteIsToProcedureStep ?step.
+          ?step :name ?stepName.
+        ?step :procedureStepHasProcedureStepType ?stepType.
+               ?stepType :name ?stepTypeLabel.}
+          Optional {?step :procedureStepInLegislature ?legislature.
+          ?legislature :name ?legislatureName.}
+          Optional {?step :procedureStepHasHouse ?CommonsId
+          filter (?CommonsId IN (id:1AFu55Hs))}
+          Optional {?step :procedureStepHasHouse ?LordsId.
+            filter (?LordsId IN (id:WkUWUBMx))}
+        filter (?step IN (id:#{step_id}))
+       ?step :procedureStepHasBusinessItem ?businessItem.
+        optional{ ?businessItem :businessItemDate ?businessItemDate.}
+              optional {?businessItem :businessItemHasBusinessItemWebLink ?businessItemLink }
+        ?businessItem :businessItemHasWorkPackage ?workPackage.
+       optional {?workPackage :workPackageHasBusinessItem ?bi1 .
+                  ?bi1 :businessItemHasProcedureStep ?madeAvailable;
+                         :businessItemDate ?madeAvailableDate.
+                  filter (?madeAvailable in (id:isWn7s3K, id:cspzmb6w, id:ITNO9JWr, id:otscOTzB))}
+         } order by ?stepName
     "
   end
 end
