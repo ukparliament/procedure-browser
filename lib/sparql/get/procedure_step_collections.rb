9240c2cf733fb81d4df7d7f55549adb28b7b8162
fantasticlife <michael@smethur.st>
2024-12-13 22:14:29 +0000

    populated query and getter for step collections in a procedure

--- lib/sparql/get/procedure_step_collections.rb
+++ lib/sparql/get/procedure_step_collections.rb
@@ -6,20 +6,22 @@ module Sparql::Get::ProcedureStepCollections
     request_body = procedure_step_collections_query( procedure )
     
     # We get the SPARQL response as a CSV.
-    #csv = get_sparql_response_as_csv( request_body )
+    csv = get_sparql_response_as_csv( request_body )
     
     # We construct an array to hold the step collections.
     step_collections = []
   
     # For each row in the CSV ...
-    #csv.each do |row|
+    csv.each do |row|
   
       # ... we create a new step collection object ...
-      #step_collection = StepCollection.new
+      step_collection = StepCollection.new
+      step_collection.identifier = row['stepCollection']
+      step_collection.label = row['stepCollectionLabel']
       
       # ... and add it to the array of step collections.
-      #step_collections << step_collection
-    #end
+      step_collections << step_collection
+    end
   
     # We return the array of step collections.
     step_collections
--- lib/sparql/queries/procedure_step_collections.rb
+++ lib/sparql/queries/procedure_step_collections.rb
@@ -3,7 +3,17 @@ module Sparql::Queries::ProcedureStepCollections
   # A SPARQL query to get step collections for a procedure.
   def procedure_step_collections_query( procedure_id )
     "
-      
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX id: <https://id.parliament.uk/>
+      SELECT * WHERE {
+        ?Procedure a :Procedure;
+                   :name ?ProcedureName.
+         filter (?Procedure in (id:#{procedure_id}))
+         ?Procedure :procedureHasProcedureStepCollection ?stepCollection.
+        ?stepCollection :name ?stepCollectionLabel. 
+       }
     "
   end
 end
