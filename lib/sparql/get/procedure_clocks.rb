24d94c6f559c062073a52dba3b94d8b71896e30e
fantasticlife <michael@smethur.st>
2024-12-13 21:22:00 +0000

    populated query and getter for clocks in a procedure

--- lib/sparql/get/procedure_clocks.rb
+++ lib/sparql/get/procedure_clocks.rb
@@ -6,20 +6,41 @@ module Sparql::Get::ProcedureClocks
     request_body = procedure_clocks_query( procedure )
     
     # We get the SPARQL response as a CSV.
-    #csv = get_sparql_response_as_csv( request_body )
+    csv = get_sparql_response_as_csv( request_body )
     
     # We construct an array to hold the clocks.
     clocks = []
   
     # For each row in the CSV ...
-    #csv.each do |row|
+    csv.each do |row|
   
       # ... we create a new clock object ...
-      #clock = Clock.new
-      
+      clock = Clock.new
+      clock.identifier = row['Clock']
+      clock.label = row['ClockLabel']
+      clock.day_count = row['ClockDayCount']
+      clock.procedure_identifier = row['Procedure']
+      clock.procedure_label = row['ProcedureName']
+      clock.from_step_identifier = row['StartStep']
+      clock.from_step_label = row['StartStepName']
+      clock.from_step_type_identifier = row['StartStepType']
+      clock.from_step_type_label = row['StartStepTypeName']
+      clock.from_step_commons_identifier = row['StartStepCommonsId']
+      clock.from_step_lords_identifier = row['StartStepLordsId']
+      clock.from_step_legislature_identifier = row['StartSteplegislature']
+      clock.from_step_legislature_label = row['StartSteplegislatureName']
+      clock.to_step_identifier = row['EndStep']
+      clock.to_step_label = row['EndStepName']
+      clock.to_step_type_identifier = row['EndStepType']
+      clock.to_step_type_label = row['EndStepTypeName']
+      clock.to_step_commons_identifier = row['EndStepCommonsId']
+      clock.to_step_lords_identifier = row['EndStepLordsId']
+      clock.to_step_legislature_identifier = row['EndSteplegislature']
+      clock.to_step_legislature_label = row['EndSteplegislatureName']
+      																	
       # ... and add it to the array of routes.
-      #clocks << clock
-    #end
+      clocks << clock
+    end
   
     # We return the array of clocks.
     clocks
--- lib/sparql/queries/procedure_clocks.rb
+++ lib/sparql/queries/procedure_clocks.rb
@@ -3,7 +3,39 @@ module Sparql::Queries::ProcedureClocks
   # A SPARQL query to get clocks for a procedure.
   def procedure_clocks_query( procedure_id )
     "
-      
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX id: <https://id.parliament.uk/>
+      SELECT * WHERE {
+        ?Procedure a :Procedure;
+                   :name ?ProcedureName.
+         filter (?Procedure in (id:#{procedure_id}))
+        ?Procedure :procedureHasClock ?Clock. 
+        ?Clock :clockLabel ?ClockLabel.
+        optional {?Clock :clockDayCount ?ClockDayCount.}
+                  ?Clock :clockHasStartProcedureStep ?StartStep. 
+        ?StartStep :name ?StartStepName;
+                  :procedureStepHasProcedureStepType ?StartStepType.
+        ?StartStepType :name ?StartStepTypeName. 
+          Optional {?StartStep :procedureStepInLegislature ?StartSteplegislature.
+          ?StartSteplegislature :name ?StartSteplegislatureName.}
+          Optional {?StartStep:procedureStepHasHouse ?StartStepCommonsId
+          filter (?StartStepCommonsId IN (id:1AFu55Hs))}
+          Optional {?StartStep :procedureStepHasHouse ?StartStepLordsId.
+            filter (?StartStepLordsId IN (id:WkUWUBMx))}
+          ?Clock :clockHasEndProcedureStep ?EndStep. 
+        ?EndStep :name ?EndStepName;
+                  :procedureStepHasProcedureStepType ?EndStepType.
+        ?EndStepType :name ?EndStepTypeName. 
+        Optional {?EndStep :procedureStepInLegislature ?EndSteplegislature.
+          ?EndSteplegislature :name ?EndSteplegislatureName.}
+          Optional {?EndStep:procedureStepHasHouse ?EndStepCommonsId
+          filter (?EndStepCommonsId IN (id:1AFu55Hs))}
+          Optional {?EndStep :procedureStepHasHouse ?EndStepLordsId.
+            filter (?EndStepLordsId IN (id:WkUWUBMx))}
+  
+        }
     "
   end
 end
