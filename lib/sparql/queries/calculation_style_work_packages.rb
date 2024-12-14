c62c44d91f392fd7e3cfec92736c4810610473f4
fantasticlife <michael@smethur.st>
2024-12-14 19:16:25 +0000

    placeholder query and getter for listing work packages for a calculation style

---
+++ lib/sparql/queries/calculation_style_work_packages.rb
@@ -0,0 +1,9 @@
+module Sparql::Queries::CalculationStyleWorkPackages
+
+  # A SPARQL query to get work packages for a calculation style.
+  def calculation_style_work_packages_query( calculation_style_id )
+    "
+      
+    "
+  end
+end
