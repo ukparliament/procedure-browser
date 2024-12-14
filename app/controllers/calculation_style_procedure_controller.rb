6d3573ba634767e4055965a8255bf47dbf67cdb2
fantasticlife <michael@smethur.st>
2024-12-14 18:51:03 +0000

    basic controller for listing procedures for a calculation style

---
+++ app/controllers/calculation_style_procedure_controller.rb
@@ -0,0 +1,25 @@
+class CalculationStyleProcedureController < ApplicationController
+
+  # We include modules required to get all calculation styles and a calculation style.
+  include Sparql::Get::CalculationStyles
+  include Sparql::Queries::CalculationStyles
+  include Sparql::Get::CalculationStyle
+  include Sparql::Queries::CalculationStyle
+  include Sparql::Get::CalculationStyleProcedures
+  include Sparql::Queries::CalculationStyleProcedures
+  include Sparql::Get::Response
+
+  def index
+    calculation_style_id = params[:calculation_style]
+    @calculation_style = get_calculation_style( calculation_style_id )
+    
+    @calculation_style_procedures = get_calculation_style_procedures( calculation_style_id )
+    
+    #@page_title = @calculation_style.label
+    #@description = "#{@calculation_style.label}."
+    @crumb << { label: 'Calculation styles', url: calculation_style_list_url }
+    #@crumb << { label: @calculation_style.label, url: calculation_style_show_url }
+    @crumb << { label: 'Procedures', url: nil }
+    @section = 'calculation-styles'
+  end
+end
---
+++ test/controllers/calculation_style_procedure_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CalculationStyleProcedureControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
