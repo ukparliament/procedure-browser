620a463cbcb023771bd2c96a954a977412834708
fantasticlife <michael@smethur.st>
2024-12-14 18:52:51 +0000

    placeholder query and getter for listing procedures for a calculation style

---
+++ lib/sparql/get/calculation_style_procedures.rb
@@ -0,0 +1,27 @@
+module Sparql::Get::CalculationStyleProcedures
+  
+  def get_calculation_style_procedures( calculation_style )
+    
+    # We get the calculation style procedures query.
+    request_body = calculation_style_procedures_query( calculation_style )
+    
+    # We get the SPARQL response as a CSV.
+    #csv = get_sparql_response_as_csv( request_body )
+    
+    # We construct an array to hold the procedures.
+    procedures = []
+  
+    # For each row in the CSV ...
+    #csv.each do |row|
+  
+      # ... we create a new procedure object ...
+      procedure = Procedure.new
+      																	
+      # ... and add it to the array of procedures.
+      #procedures << procedure
+    #end
+  
+    # We return the array of procedures.
+    procedures
+  end
+end
---
+++ lib/sparql/queries/calculation_style_procedures.rb
@@ -0,0 +1,9 @@
+module Sparql::Queries::CalculationStyleProcedures
+
+  # A SPARQL query to get procedures for a calculation style.
+  def calculation_style_procedures_query( procedure_id )
+    "
+      
+    "
+  end
+end
