619555d7fce560f9fb13c46346fc87b197d3e4e2
fantasticlife <michael@smethur.st>
2024-12-15 12:19:13 +0000

    filled out routes controller with calls to queries and getters

--- app/controllers/route_controller.rb
+++ app/controllers/route_controller.rb
@@ -1,7 +1,29 @@
 class RouteController < ApplicationController
 
+  # We include modules required to get all routes and a routes with its steps.
+  include Sparql::Get::Routes
+  include Sparql::Queries::Routes
+  include Sparql::Get::Route
+  include Sparql::Queries::Route
+  include Sparql::Get::Response
+
+  def index
+    @routes = get_routes
+    
+    @page_title = 'Routes'
+    @description = 'Routes in procedures.'
+    @crumb << { label: 'Routes', url: nil }
+    @section = 'routes'
+  end
+
   def show
-    @page_title = 'Coming soon'
-    render :template => 'under_construction/notice'
+    route_id = params[:route]
+    @route = get_route( route_id )
+    
+    #@page_title = 
+    #@description = 
+    @crumb << { label: 'Routes', url: route_list_url }
+    #@crumb << { label: , url: nil }
+    @section = 'routes'
   end
 end
