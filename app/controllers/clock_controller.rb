88adcb20c663205257db62ac064e53f548b919fe
fantasticlife <michael@smethur.st>
2024-12-15 13:16:14 +0000

    filled out the clock controller with calls to queries and getters

--- app/controllers/clock_controller.rb
+++ app/controllers/clock_controller.rb
@@ -1,7 +1,29 @@
 class ClockController < ApplicationController
 
+  # We include modules required to get all clocks and a clock with its steps.
+  include Sparql::Get::Clocks
+  include Sparql::Queries::Clocks
+  include Sparql::Get::Clock
+  include Sparql::Queries::Clock
+  include Sparql::Get::Response
+
+  def index
+    @clock = get_clocks
+    
+    @page_title = 'Clocks'
+    @description = 'Clocks in procedures.'
+    @crumb << { label: 'Clocks', url: nil }
+    @section = 'clocks'
+  end
+
   def show
-    @page_title = 'Coming soon'
-    render :template => 'under_construction/notice'
+    clock_id = params[:clock]
+    @clock = get_clock( clock_id )
+    
+    #@page_title = 
+    #@description = 
+    @crumb << { label: 'Clocks', url: route_list_url }
+    #@crumb << { label: , url: nil }
+    @section = 'clocks'
   end
 end
