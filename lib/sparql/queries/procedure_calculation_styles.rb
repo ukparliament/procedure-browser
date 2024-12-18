dd3dd53fc1342fcfb6aa97538cb0c7c2be3fe1c4
fantasticlife <michael@smethur.st>
2024-12-18 20:17:22 +0000

    populated query and getter for calculation styles for a procedure

--- lib/sparql/queries/procedure_calculation_styles.rb
+++ lib/sparql/queries/procedure_calculation_styles.rb
@@ -3,7 +3,17 @@ module Sparql::Queries::ProcedureCalculationStyles
   # A SPARQL query to get calculation styles for a procedure.
   def procedure_calculation_styles_query( procedure_id )
     "
-      
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX id: <https://id.parliament.uk/>
+      SELECT ?procedureCalculationStyle ?procedureCalculationStyleName WHERE {
+        ?Procedure a :Procedure;
+                   :name ?ProcedureName.
+                optional {?Procedure :procedureHasCalculationStyle ?procedureCalculationStyle.
+        ?procedureCalculationStyle :name ?procedureCalculationStyleName. }
+        filter (?Procedure in (id:#{procedure_id}))
+              } 
     "
   end
 end
