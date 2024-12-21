7f770f665a1244ee60453eb0fcf5bedd833bf945
fantasticlife <michael@smethur.st>
2024-12-21 19:20:30 +0000

    populated query and getter for procedures under a calculation style. broken query

--- lib/sparql/get/calculation_style_procedures.rb
+++ lib/sparql/get/calculation_style_procedures.rb
@@ -6,20 +6,23 @@ module Sparql::Get::CalculationStyleProcedures
     request_body = calculation_style_procedures_query( calculation_style )
     
     # We get the SPARQL response as a CSV.
-    #csv = get_sparql_response_as_csv( request_body )
+    csv = get_sparql_response_as_csv( request_body )
     
     # We construct an array to hold the procedures.
     procedures = []
   
     # For each row in the CSV ...
-    #csv.each do |row|
+    csv.each do |row|
   
       # ... we create a new procedure object ...
       procedure = Procedure.new
-      																	
+      procedure.identifier = row['procedure']
+      procedure.label = row['procedureName']
+      procedure.description = row['procedureDescription']
+      
       # ... and add it to the array of procedures.
-      #procedures << procedure
-    #end
+      procedures << procedure
+    end
   
     # We return the array of procedures.
     procedures
--- lib/sparql/queries/calculation_style_procedures.rb
+++ lib/sparql/queries/calculation_style_procedures.rb
@@ -3,7 +3,18 @@ module Sparql::Queries::CalculationStyleProcedures
   # A SPARQL query to get procedures for a calculation style.
   def calculation_style_procedures_query( calculation_style_id )
     "
-      
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX id: <https://id.parliament.uk/>
+      SELECT * WHERE {
+        ?CalculationStyle a :CalculationStyle;
+                   :name ?CalculationStyleName.
+  filter (?CalculationStyle in (id:#{calculation_style_id}))
+     optional {?CalculationStyle :calculationStyleHasProcedure ?procedure.
+   			   ?procedure :name ?procedureName.
+  			   ?procedure :procedureDescription ?procedureDescription}
+              } Order by ?CalculationStyleName
     "
   end
 end
