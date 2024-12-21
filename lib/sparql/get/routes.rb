bc70c3e6cd41803cadd37c7a53a6d416d66b9f41
fantasticlife <michael@smethur.st>
2024-12-21 19:01:51 +0000

    populated query and getter for route listing. still missing start and end dates

--- lib/sparql/get/routes.rb
+++ lib/sparql/get/routes.rb
@@ -7,20 +7,39 @@ module Sparql::Get::Routes
     request_body = routes_query
   
     # We get the SPARQL response as a CSV.
-    #csv = get_sparql_response_as_csv( request_body )
+    csv = get_sparql_response_as_csv( request_body )
   
     # We construct an array to hold the routes.
     routes = []
   
     # For each row in the CSV ...
-    #csv.each do |row|
+    csv.each do |row|
   
       # ... we create a new route object ...
       route = Route.new
+      route.identifier = row['Route']
+      route.procedure_identifier = row['Procedure']
+      route.procedure_label = row['procedureName']
+      route.from_step_identifier = row['FromStep']
+      route.from_step_label = row['FromStepName']
+      route.from_step_type_identifier = row['FromStepType']
+      route.from_step_type_label = row['FromStepTypeName']
+      route.from_step_legislature_identifier = row['FromSteplegislature']
+      route.from_step_legislature_label = row['FromSteplegislatureName']
+      route.from_step_commons_identifier = row['FromStepCommonsId']
+      route.from_step_lords_identifier = row['FromStepLordsId']
+      route.to_step_identifier = row['ToStep']
+      route.to_step_label = row['ToStepName']
+      route.to_step_type_identifier = row['ToStepType']
+      route.to_step_type_label = row['ToStepTypeName']
+      route.to_step_legislature_identifier = row['ToSteplegislature']
+      route.to_step_legislature_label = row['ToSteplegislatureName']
+      route.to_step_commons_identifier = row['ToStepCommonsId']
+      route.to_step_lords_identifier = row['ToStepLordsId']
       
       # ... and add it to the array of routes.
       routes << route
-    #end
+    end
   
     # We return the array of routes.
     routes
--- lib/sparql/queries/routes.rb
+++ lib/sparql/queries/routes.rb
@@ -3,6 +3,36 @@ module Sparql::Queries::Routes
   # A SPARQL query to get all routes.
   def routes_query
     "
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX id: <https://id.parliament.uk/>
+      select * where {
+       ?Route a :ProcedureRoute;
+              :procedureRouteHasProcedure ?Procedure.
+        ?Procedure :name ?procedureName. 
+        ?Route :procedureRouteIsFromProcedureStep ?FromStep. 
+        ?FromStep :name ?FromStepName;
+                  :procedureStepHasProcedureStepType ?FromStepType. 
+        ?FromStepType :name ?FromStepTypeName. 
+        Optional {?FromStep :procedureStepInLegislature ?FromSteplegislature.
+                ?FromSteplegislature :name ?FromSteplegislatureName.}
+                Optional {?FromStep:procedureStepHasHouse ?FromStepCommonsId
+                filter (?FromStepCommonsId IN (id:1AFu55Hs))}
+                Optional {?FromStep :procedureStepHasHouse ?FromStepLordsId.
+                  filter (?FromStepLordsId IN (id:WkUWUBMx))}
+                ?Route :procedureRouteIsToProcedureStep ?ToStep. 
+              ?ToStep :name ?ToStepName;
+                        :procedureStepHasProcedureStepType ?ToStepType.
+              ?ToStepType :name ?ToStepTypeName. 
+              Optional {?ToStep :procedureStepInLegislature ?ToSteplegislature.
+                ?ToSteplegislature :name ?ToSteplegislatureName.}
+                Optional {?ToStep:procedureStepHasHouse ?ToStepCommonsId
+                filter (?ToStepCommonsId IN (id:1AFu55Hs))}
+                Optional {?ToStep :procedureStepHasHouse ?ToStepLordsId.
+                  filter (?ToStepLordsId IN (id:WkUWUBMx))}
+  
+      } order by ?procedureName ?FromStepName ?ToStepName
     "
   end
 end
