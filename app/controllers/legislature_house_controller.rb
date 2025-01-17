abe9be5062e82a874021b9bc36b557850f19cc09
fantasticlife <michael@smethur.st>
2025-01-17 14:06:52 +0000

    added controller for houses in legislatures

---
+++ app/controllers/legislature_house_controller.rb
@@ -0,0 +1,23 @@
+class LegislatureHouseController < ApplicationController
+
+  # We include modules required to get all legislatures and a legislature.
+  include Sparql::Get::Legislature
+  include Sparql::Queries::Legislature
+  include Sparql::Get::LegislatureHouses
+  include Sparql::Queries::LegislatureHouses
+  include Sparql::Get::Response
+
+  def index
+    legislature_id = params[:legislature]
+    @legislature = get_legislature( legislature_id )
+    @legislature_houses = get_legislature_houses( legislature_id )
+    
+    @page_title = "Houses in the #{@legislature.label}"
+    @multiline_page_title = "#{@legislature.label} <span class='subhead'>Houses</span>".html_safe
+    @description = "Houses in the #{@legislature.label}."
+    @crumb << { label: 'Legislatures', url: legislature_list_url }
+    @crumb << { label: @legislature.label, url: legislature_show_url }
+    @crumb << { label: 'Houses', url: nil }
+    @section = 'legislatures'
+  end
+end
