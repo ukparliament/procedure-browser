27fa880193c0697777c35cfa07cf6ee2f8937e8f
fantasticlife <michael@smethur.st>
2025-01-17 11:15:40 +0000

    filled out controller to get a list of legislatures

--- app/controllers/legislature_controller.rb
+++ app/controllers/legislature_controller.rb
@@ -1,5 +1,21 @@
 class LegislatureController < ApplicationController
 
+  # We include modules required to get all legislatures and a legislature.
+  include Sparql::Get::Legislatures
+  include Sparql::Queries::Legislatures
+  #include Sparql::Get::Legislature
+  #include Sparql::Queries::Legislature
+  include Sparql::Get::Response
+
+  def index
+    @legislatures = get_legislatures
+    
+    @page_title = 'Legislatures'
+    @description = 'Legislatures.'
+    @crumb << { label: 'Legislatures', url: nil }
+    @section = 'legislatures'
+  end
+
   def show
     @page_title = 'Coming soon'
     render :template => 'under_construction/notice'
