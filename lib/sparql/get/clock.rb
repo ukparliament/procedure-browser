c2738d3bdec581f9e5f1e2657e35193d62f5d332
fantasticlife <michael@smethur.st>
2025-01-17 00:02:34 +0000

    populated query and getter for a clock

--- lib/sparql/get/clock.rb
+++ lib/sparql/get/clock.rb
@@ -7,16 +7,37 @@ module Sparql::Get::Clock
     request_body = clock_query( clock_id )
   
     # We get the SPARQL response as a CSV.
-    #csv = get_sparql_response_as_csv( request_body )
+    csv = get_sparql_response_as_csv( request_body )
     
     # We take the one and only row from the CSV ...
-    #csv.take( 1 ).each do |row|
+    csv.take( 1 ).each do |row|
     
       # ... and create a new clock object.
       clock = Clock.new
+      clock.identifier = row['Clock']
+      clock.label = row['ClockName']
+      clock.day_count = row['DayCount']
+      clock.procedure_identifier = row['procedure']
+      clock.procedure_label = row['procedureName']
+      clock.from_step_identifier = row['StartStep']
+      clock.from_step_label = row['StartStepName']
+      clock.from_step_type_identifier = row['StartStepType']
+      clock.from_step_type_label = row['StartStepTypeName']
+      clock.from_step_legislature_identifier = row['StartSteplegislature']
+      clock.from_step_legislature_label = row['StartSteplegislatureName']
+      clock.from_step_commons_identifier = row['StartStepCommonsId']
+      clock.from_step_lords_identifier = row['StartStepLordsId']
+      clock.to_step_identifier = row['EndStep']
+      clock.to_step_label = row['EndStepName']
+      clock.to_step_type_identifier = row['EndStepType']
+      clock.to_step_type_label = row['EndStepTypeName']
+      clock.to_step_legislature_identifier = row['EndSteplegislature']
+      clock.to_step_legislature_label = row['EndSteplegislatureName']
+      clock.to_step_commons_identifier = row['EndStepCommonsId']
+      clock.to_step_lords_identifier = row['EndStepLordsId']
       
       # We return the clock object.
       return clock
-    #end
+    end
   end
 end
--- lib/sparql/queries/clock.rb
+++ lib/sparql/queries/clock.rb
@@ -3,7 +3,38 @@ module Sparql::Queries::Clock
   # A SPARQL query to get a clock.
   def clock_query( clock_id )
     "
-      
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX id: <https://id.parliament.uk/>
+      SELECT ?Clock ?ClockName ?DayCount ?procedure ?procedureName ?StartStep ?StartStepName ?StartStepType ?StartStepTypeName ?StartSteplegislature ?StartSteplegislatureName ?StartStepCommonsId ?StartStepLordsId ?EndStep ?EndStepName ?EndStepType ?EndStepTypeName ?EndSteplegislature ?EndSteplegislatureName ?EndStepCommonsId ?EndStepLordsId WHERE {
+        ?Clock a :Clock;
+                   :name ?ClockName;
+                   :clockFormsPartOfProcedure ?procedure;
+                   :clockHasStartProcedureStep ?StartStep;
+                   :clockHasEndProcedureStep ?EndStep. 
+  optional {?Clock :clockDayCount ?DayCount.}
+  filter (?Clock in (id:#{clock_id}))
+                   ?procedure :name ?procedureName.
+  			    ?StartStep :name ?StartStepName;
+            :procedureStepHasProcedureStepType ?StartStepType.
+  ?StartStepType :name ?StartStepTypeName. 
+    Optional {?StartStep :procedureStepInLegislature ?StartSteplegislature.
+    ?StartSteplegislature :name ?StartSteplegislatureName.}
+    Optional {?StartStep:procedureStepHasHouse ?StartStepCommonsId
+    filter (?StartStepCommonsId IN (id:1AFu55Hs))}
+    Optional {?StartStep :procedureStepHasHouse ?StartStepLordsId.
+      filter (?StartStepLordsId IN (id:WkUWUBMx))}
+     ?EndStep :name ?EndStepName;
+            :procedureStepHasProcedureStepType ?EndStepType.
+  ?EndStepType :name ?EndStepTypeName. 
+  Optional {?EndStep :procedureStepInLegislature ?EndSteplegislature.
+    ?EndSteplegislature :name ?EndSteplegislatureName.}
+    Optional {?EndStep:procedureStepHasHouse ?EndStepCommonsId
+    filter (?EndStepCommonsId IN (id:1AFu55Hs))}
+    Optional {?EndStep :procedureStepHasHouse ?EndStepLordsId.
+      filter (?EndStepLordsId IN (id:WkUWUBMx))}
+              } Order by ?ClockName ?procedureName
     "
   end
 end
