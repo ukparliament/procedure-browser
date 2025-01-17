fd3c0cf3eeedcd6a9f510768aeecf3cb4ec58a58
fantasticlife <michael@smethur.st>
2025-01-17 15:10:00 +0000

    populated house list action

--- app/controllers/house_controller.rb
+++ app/controllers/house_controller.rb
@@ -1,5 +1,19 @@
 class HouseController < ApplicationController
 
+  # We include modules required to get all legislatures and a legislature.
+  include Sparql::Get::Houses
+  include Sparql::Queries::Houses
+  include Sparql::Get::Response
+
+  def index
+    @houses = get_houses
+    
+    @page_title = 'Houses'
+    @description = 'Houses.'
+    @crumb << { label: 'Houses', url: nil }
+    @section = 'houses'
+  end
+
   def show
     @page_title = 'Coming soon'
     render :template => 'under_construction/notice'
