77a6c60ec628352ba7d973748db74ce2a2a92f6f
fantasticlife <michael@smethur.st>
2025-01-17 19:31:08 +0000

    added legislature bits to house model

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,8 +2,14 @@ class House
   
   attr_accessor :identifier
   attr_accessor :label
+  attr_accessor :legislature_identifier
+  attr_accessor :legislature_label
   
   def id
     self.identifier.split( '/' ).last
+  end
+  
+  def legislature_id
+    self.legislature_identifier.split( '/' ).last
   end
 end
