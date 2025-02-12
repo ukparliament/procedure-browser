08d44c44246428f168f1cf5e6123468e96e4c15c
fantasticlife <michael@smethur.st>
2025-02-12 12:57:01 +0000

    stubbed out query and getter for a list of work packageable things

---
+++ lib/sparql/get/work_packageable_things.rb
@@ -0,0 +1,33 @@
+module Sparql::Get::WorkPackageableThings
+
+  # A method to get an array of all work packageable things on this page with this many results per page.
+  def get_work_packageable_things( page_number, results_per_page )
+  
+    # We calculate the result offset.
+    result_offset = ( ( page_number - 1) * results_per_page )
+
+    # We get the all work packageable things query passing parameters for the number of results per page and the result offset.
+    request_body = work_packageable_things_query( results_per_page, result_offset )
+  
+    # We get the SPARQL response as a CSV.
+    #csv = get_sparql_response_as_csv( request_body )
+  
+    # We construct an array to hold the work packageable things.
+    work_packageable_things = []
+  
+    # For each row in the CSV ...
+    #csv.each do |row|
+  
+      # ... we create a new work packageable thing object ...
+      #work_packageable_thing = WorkPackageableThing.new
+      #work_packageable_thing.identifier = row['workPackageableThing']
+      #work_packageable_thing.label = row['workPackageableThingLabel']
+      
+      # ... and add it to the array of work packageable things.
+      #work_packageable_things << work_packageable_thing
+    #end
+  
+    # We return the array of work packageable things.
+    work_packageable_things
+  end
+end
---
+++ lib/sparql/queries/work_packageable_things.rb
@@ -0,0 +1,9 @@
+module Sparql::Queries::WorkPackageableThings
+
+  # A SPARQL query to get all work packageable things taking parameters of limit and offset.
+  def work_packageable_things_query( limit, offset )
+    "
+        
+    "
+  end
+end
