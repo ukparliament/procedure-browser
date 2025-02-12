823e4831ce9f00b5b9958b7ed7b0b56c549d9990
fantasticlife <michael@smethur.st>
2025-02-12 16:26:48 +0000

    stubbed query and getter for an item ofenabling legislation

---
+++ lib/sparql/queries/enabling_legislation.rb
@@ -0,0 +1,9 @@
+module Sparql::Queries::EnablingLegislation
+
+  # A SPARQL query to get an item of enabling legislation.
+  def enabling_legislation_query( enabling_legislation_id )
+    "
+      
+    "
+  end
+end
