cec0b63d0366964b95f3449aa607a890919bb65b
fantasticlife <michael@smethur.st>
2025-02-12 16:25:50 +0000

    stubbed query and getter for enabling legislation count

---
+++ lib/sparql/queries/enabling_legislation_count.rb
@@ -0,0 +1,9 @@
+module Sparql::Queries::EnablingLegislationCount
+
+  # A SPARQL query to get a count of all enabling legislation.
+  def enabling_legislation_count_query
+    "
+      
+    "
+  end
+end
