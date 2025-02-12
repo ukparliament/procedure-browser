7855f1a7a6430d9281028e007a107fcefbf3daaf
fantasticlife <michael@smethur.st>
2025-02-12 12:56:33 +0000

    stubbed out query and getter for a work packageable thing count

---
+++ lib/sparql/get/work_packageable_thing_count.rb
@@ -0,0 +1,19 @@
+module Sparql::Get::WorkPackageableThingCount
+
+  # A method to get a count of all work packageable things.
+  def get_work_packageable_thing_count
+  
+    # We get the work packageable_thing count query.
+    request_body = work_packageable_thing_count_query
+  
+    # We get the SPARQL response as a CSV.
+    #csv = get_sparql_response_as_csv( request_body )
+    
+    # We take the one and only row from the CSV ...
+    #csv.take( 1 ).each do |row|
+      
+      # We return the count.
+      #return row[0].to_i
+    #end
+  end
+end
---
+++ lib/sparql/queries/work_packageable_thing_count.rb
@@ -0,0 +1,9 @@
+module Sparql::Queries::WorkPackageableThingCount
+
+  # A SPARQL query to get a count of all work packageable things.
+  def work_packageable_thing_count_query
+    "
+      
+    "
+  end
+end
