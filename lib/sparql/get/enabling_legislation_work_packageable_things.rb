04bb08e0bb4ff7a89de771818ae04848fdf503c5
fantasticlife <michael@smethur.st>
2025-02-12 23:37:12 +0000

    stubbed out query and getter for work packageable things enabled by an item of legislation

---
+++ lib/sparql/get/enabling_legislation_work_packageable_things.rb
@@ -0,0 +1,29 @@
+module Sparql::Get::EnablingLegislationWorkPackageableThings
+  
+  def get_enabling_legislation_work_packageable_things( enabling_thing_id )
+    
+    # We get the enabling legislation work packageable things query.
+    request_body = enabling_legislation_work_packageable_things_query( enabling_thing_id )
+    
+    # We get the SPARQL response as a CSV.
+    #csv = get_sparql_response_as_csv( request_body )
+    
+    # We construct an array to hold the work packageable things.
+    #work_packageable_things = []
+  
+    # For each row in the CSV ...
+    #csv.each do |row|
+  
+      # ... we create a new work packageable thing object ...
+      #work_packageable_thing = WorkPackageableThing.new
+      #work_packageable_thing.identifier = row['workPackageableThing']
+      #work_packageable_thing.label = row['workPackageableThingName']
+      
+      # ... and add it to the array of work packageable things.
+      #work_packageable_things << work_packageable_thing
+    #end
+  
+    # We return the array of work packageable things.
+    #work_packageable_things
+  end
+end
---
+++ lib/sparql/queries/enabling_legislation_work_packageable_things.rb
@@ -0,0 +1,9 @@
+module Sparql::Queries::EnablingLegislationWorkPackageableThings
+
+  # A SPARQL query to get work packageable things enabled by an item of legislation.
+  def enabling_legislation_work_packageable_things_query( enabling_thing_id )
+    "
+      
+    "
+  end
+end
