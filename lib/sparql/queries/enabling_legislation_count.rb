1bb899904bbf556581d1fe8764374d557d7c7603
fantasticlife <michael@smethur.st>
2025-03-06 12:06:07 +0000

    populated query and getter for a count of enabling legislation

--- lib/sparql/queries/enabling_legislation_count.rb
+++ lib/sparql/queries/enabling_legislation_count.rb
@@ -3,7 +3,18 @@ module Sparql::Queries::EnablingLegislationCount
   # A SPARQL query to get a count of all enabling legislation.
   def enabling_legislation_count_query
     "
-      
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      select (COUNT(DISTINCT ?Act) AS ?count)  WHERE {
+       ?Act a :ActOfParliament.
+        ?Act :actOfParliamentName ?Name.
+         OPTIONAL {?Act :actOfParliamentNumber ?Number}
+        OPTIONAL {?Act :actOfParliamentYear ?Year}
+         OPTIONAL{  ?Act :actOfParliamentRoyalAssentDate ?Date}
+        OPTIONAL {?Act :actOfParliamentUrl ?URL}
+  
+      } 
     "
   end
 end
