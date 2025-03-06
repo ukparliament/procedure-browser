ac821c4ec2129d2b3a30ac09996d9931bc074596
fantasticlife <michael@smethur.st>
2025-03-06 12:06:46 +0000

    populated query and getter for a paginated list of enabling legislation

--- lib/sparql/get/enabling_legislations.rb
+++ lib/sparql/get/enabling_legislations.rb
@@ -10,22 +10,28 @@ module Sparql::Get::EnablingLegislations
     request_body = enabling_legislations_query( results_per_page, result_offset )
   
     # We get the SPARQL response as a CSV.
-    #csv = get_sparql_response_as_csv( request_body )
+    csv = get_sparql_response_as_csv( request_body )
   
     # We construct an array to hold the enabling legislation.
-    #enabling_legislations = []
+    enabling_legislations = []
   
     # For each row in the CSV ...
-    #csv.each do |row|
+    csv.each do |row|
   
       # ... we create a new enabling legislation object ...
-      #enabling_legislation = EnablingLegislation.new
+      enabling_legislation = EnablingLegislation.new
+      enabling_legislation.identifier = row['Act']
+      enabling_legislation.label = row['Name']
+      enabling_legislation.date = row['Date'].to_date if row['Date']
+      enabling_legislation.year = row['Year']
+      enabling_legislation.act_number = row['Number']
+      enabling_legislation.uri = row['URL']
       
       # ... and add it to the array of enabling legislation.
-      #enabling_legislations << enabling_legislation
-    #end
+      enabling_legislations << enabling_legislation
+    end
   
     # We return the array of enabling legislation.
-    #enabling_legislations
+    enabling_legislations
   end
 end
--- lib/sparql/queries/enabling_legislations.rb
+++ lib/sparql/queries/enabling_legislations.rb
@@ -3,7 +3,19 @@ module Sparql::Queries::EnablingLegislations
   # A SPARQL query to get all enabling legislation taking parameters of limit and offset.
   def enabling_legislations_query( limit, offset )
     "
-        
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      SELECT * WHERE {
+       ?Act a :ActOfParliament.
+        ?Act :actOfParliamentName ?Name.
+         OPTIONAL {?Act :actOfParliamentNumber ?Number}
+        OPTIONAL {?Act :actOfParliamentYear ?Year}
+         OPTIONAL{  ?Act :actOfParliamentRoyalAssentDate ?Date}
+        OPTIONAL {?Act :actOfParliamentUrl ?URL}
+  
+      } Order by desc(?Date)
+      LIMIT #{limit} OFFSET #{offset}
     "
   end
 end
