910a9f35a46a375ab514a979e5029c57fb3acbbf
fantasticlife <michael@smethur.st>
2025-03-07 14:16:38 +0000

    added enabling_legislation_work_packageable_thing_results_per_page_link method

--- app/helpers/enabling_legislation_work_packageable_thing_helper.rb
+++ app/helpers/enabling_legislation_work_packageable_thing_helper.rb
@@ -1,2 +1,10 @@
 module EnablingLegislationWorkPackageableThingHelper
+
+  def enabling_legislation_work_packageable_thing_results_per_page_link( number, results_per_page )
+    if number == results_per_page
+      number.to_s
+    else
+      link_to( number.to_s, enabling_legislation_work_packageable_thing_list_url( 'results-per-page' => number ) )
+    end
+  end
 end
