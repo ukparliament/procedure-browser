1878b55d050fb9b6a44a7d6f3fc798c200450f97
fantasticlife <michael@smethur.st>
2025-03-07 14:11:28 +0000

    populated query and getter for a count of work packageable things for an item of enabling legislation

--- lib/sparql/get/enabling_legislation_work_packageable_thing_count.rb
+++ lib/sparql/get/enabling_legislation_work_packageable_thing_count.rb
@@ -7,13 +7,13 @@ module Sparql::Get::EnablingLegislationWorkPackageableThingCount
     request_body = enabling_legislation_work_packageable_thing_count_query( enabling_legislation_id )
   
     # We get the SPARQL response as a CSV.
-    #csv = get_sparql_response_as_csv( request_body )
+    csv = get_sparql_response_as_csv( request_body )
     
     # We take the one and only row from the CSV ...
-    #csv.take( 1 ).each do |row|
+    csv.take( 1 ).each do |row|
       
       # We return the count.
-      #return row[0].to_i
-    #end
+      return row[0].to_i
+    end
   end
 end
--- lib/sparql/queries/enabling_legislation_work_packageable_thing_count.rb
+++ lib/sparql/queries/enabling_legislation_work_packageable_thing_count.rb
@@ -3,7 +3,22 @@ module Sparql::Queries::EnablingLegislationWorkPackageableThingCount
   # A SPARQL query to get a count of all work packageable things for an item of enabling legislation.
   def enabling_legislation_work_packageable_thing_count_query( enabling_thing_id )
     "
-      
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX id: <https://id.parliament.uk/>
+      SELECT (COUNT(DISTINCT ?workPackagedThing) AS ?count) WHERE {
+       ?Act a :ActOfParliament.
+        ?Act :actOfParliamentName ?Name.
+         OPTIONAL {?Act :actOfParliamentNumber ?Number}
+        OPTIONAL {?Act :actOfParliamentYear ?Year}
+         OPTIONAL{  ?Act :actOfParliamentRoyalAssentDate ?Date}
+        OPTIONAL {?Act :actOfParliamentUrl ?URL}
+        filter (?Act in (id:#{enabling_thing_id}))
+        ?Act :enabling ?workPackagedThing. 
+        ?workPackagedThing :name ?workPackagedThingName. 
+      }                      
+       
     "
   end
 end
