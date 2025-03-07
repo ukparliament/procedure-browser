3a5eb902c59c888de3f43da68dbbe0f58ee386d4
fantasticlife <michael@smethur.st>
2025-03-07 14:12:51 +0000

    enabling legislation show action now includes work packageable things

--- app/controllers/enabling_legislation_controller.rb
+++ app/controllers/enabling_legislation_controller.rb
@@ -7,6 +7,10 @@ class EnablingLegislationController < ApplicationController
   include Sparql::Queries::EnablingLegislations
   include Sparql::Get::EnablingLegislation
   include Sparql::Queries::EnablingLegislation
+  include Sparql::Get::EnablingLegislationWorkPackageableThingCount
+  include Sparql::Queries::EnablingLegislationWorkPackageableThingCount
+  include Sparql::Get::EnablingLegislationWorkPackageableThings
+  include Sparql::Queries::EnablingLegislationWorkPackageableThings
   include Sparql::Get::Response
 
   def index
@@ -38,13 +42,36 @@ class EnablingLegislationController < ApplicationController
   end
 
   def show
-    enabling_legislation_id = params[:enabling_legislation]
-    @enabling_legislation = get_enabling_legislation( enabling_legislation_id )
+  
+    # We get the enabling legislation.
+    enabling_legislation = params[:enabling_legislation]
+    @enabling_legislation = get_enabling_legislation( enabling_legislation )
+  
+    # We get the page number passed as a parameter.
+    page = params['page']
+    @page = ( page || "1" ).to_i
     
-    #@page_title = @enabling_legislation.label
-    #@description = "#{@enabling_legislation.label}."
+    # We get the number of results per page passed as a parameter.
+    results_per_page = params['results-per-page']
+    @results_per_page = ( results_per_page || $DEFAULT_RESULTS_PER_PAGE ).to_i
+    
+    # We get the count of all work packageable things for the item of enabling legislation.
+    @result_count = get_enabling_legislation_work_packageable_thing_count( enabling_legislation )
+    
+    # If this is not the first page and the number of the first work package on this page exceeds the total number of work packages ...
+    if @page != 1 && ( ( ( @page - 1 ) * @results_per_page ) + 1 > @result_count )
+      raise ActionController::RoutingError.new("Not Found")
+    end
+    
+    # We get the set of work packageable things for an item of legislation on this page with this many results per page.
+    @enabling_legislation_work_packageable_things = get_enabling_legislation_work_packageable_things( enabling_legislation, @page, @results_per_page ) 
+    
+    @page_title = @enabling_legislation.label
+    @description = "#{@enabling_legislation.label}."
     @crumb << { label: 'Enabling legislation', url: enabling_legislation_list_url }
-    #@crumb << { label: @enabling_legislation.label, url: nil }
+    @crumb << { label: @enabling_legislation.label, url: nil }
     @section = 'enabling-legislation'
+    
+    render :template => 'enabling_legislation_work_packageable_thing/index'
   end
 end
