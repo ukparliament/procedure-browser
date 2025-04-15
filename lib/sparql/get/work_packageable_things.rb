e00349a36ef350c340318ebf5d7ea04bd9689526
fantasticlife <michael@smethur.st>
2025-04-15 13:04:23 +0100

    populated getter and query for a list of work packageable things

--- lib/sparql/get/work_packageable_things.rb
+++ lib/sparql/get/work_packageable_things.rb
@@ -10,22 +10,22 @@ module Sparql::Get::WorkPackageableThings
     request_body = work_packageable_things_query( results_per_page, result_offset )
   
     # We get the SPARQL response as a CSV.
-    #csv = get_sparql_response_as_csv( request_body )
+    csv = get_sparql_response_as_csv( request_body )
   
     # We construct an array to hold the work packageable things.
     work_packageable_things = []
   
     # For each row in the CSV ...
-    #csv.each do |row|
+    csv.each do |row|
   
       # ... we create a new work packageable thing object ...
-      #work_packageable_thing = WorkPackageableThing.new
-      #work_packageable_thing.identifier = row['workPackageableThing']
-      #work_packageable_thing.label = row['workPackageableThingLabel']
+      work_packageable_thing = WorkPackageableThing.new
+      work_packageable_thing.identifier = row['WorkPackagedThing']
+      work_packageable_thing.label = row['workPackagedThingName']
       
       # ... and add it to the array of work packageable things.
-      #work_packageable_things << work_packageable_thing
-    #end
+      work_packageable_things << work_packageable_thing
+    end
   
     # We return the array of work packageable things.
     work_packageable_things
--- lib/sparql/queries/work_packageable_things.rb
+++ lib/sparql/queries/work_packageable_things.rb
@@ -3,7 +3,14 @@ module Sparql::Queries::WorkPackageableThings
   # A SPARQL query to get all work packageable things taking parameters of limit and offset.
   def work_packageable_things_query( limit, offset )
     "
-        
+      PREFIX id: <https://id.parliament.uk/>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      select * where {
+        ?WorkPackagedThing a :WorkPackagedThing;
+                           :name ?workPackagedThingName.
+      } LIMIT #{limit} OFFSET #{offset}
     "
   end
 end
