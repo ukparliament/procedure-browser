3e3b4095984515818da4fca41f331904a7075031
fantasticlife <michael@smethur.st>
2025-04-15 13:03:45 +0100

    populated getter and query for a count of work packageable things

--- lib/sparql/queries/work_packageable_thing_count.rb
+++ lib/sparql/queries/work_packageable_thing_count.rb
@@ -3,7 +3,14 @@ module Sparql::Queries::WorkPackageableThingCount
   # A SPARQL query to get a count of all work packageable things.
   def work_packageable_thing_count_query
     "
-      
+      PREFIX id: <https://id.parliament.uk/>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      select (COUNT(DISTINCT ?WorkPackagedThing) AS ?count)  where {
+        ?WorkPackagedThing a :WorkPackagedThing;
+                           :name ?workPackagedThingName.
+      } 
     "
   end
 end
