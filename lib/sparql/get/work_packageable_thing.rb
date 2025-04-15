6499425d1fa92c12ff6db863be519cc34b54057a
fantasticlife <michael@smethur.st>
2025-04-15 15:36:49 +0100

    poplated getter and query for a work packaged thing

--- lib/sparql/get/work_packageable_thing.rb
+++ lib/sparql/get/work_packageable_thing.rb
@@ -7,18 +7,18 @@ module Sparql::Get::WorkPackageableThing
     request_body = work_packageable_thing_query( work_packageable_thing_id )
   
     # We get the SPARQL response as a CSV.
-    #csv = get_sparql_response_as_csv( request_body )
+    csv = get_sparql_response_as_csv( request_body )
     
     # We take the one and only row from the CSV ...
-    #csv.take( 1 ).each do |row|
+    csv.take( 1 ).each do |row|
     
       # ... and create a new work packageable thing object.
-      #work_packageable_thing = WorkPackageableThing.new
-      #work_packageable_thing.identifier = row['workPackageableThing']
-      #work_packageable_thing.label = row['workPackageableThingLabel']
+      work_packageable_thing = WorkPackageableThing.new
+      work_packageable_thing.identifier = row['WorkPackagedThing']
+      work_packageable_thing.label = row['workPackagedThingName']
       
       # We return the work packageable thing object.
-      #return work_packageable_thing
-    #end
+      return work_packageable_thing
+    end
   end
 end
--- lib/sparql/queries/work_packageable_thing.rb
+++ lib/sparql/queries/work_packageable_thing.rb
@@ -3,7 +3,15 @@ module Sparql::Queries::WorkPackageableThing
   # A SPARQL query to get a work packageable thing.
   def work_packageable_thing_query( work_packageable_thing_id )
     "
-      
+      PREFIX id: <https://id.parliament.uk/>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      select * where {
+        ?WorkPackagedThing a :WorkPackagedThing;
+                           :name ?workPackagedThingName.
+        filter (?WorkPackagedThing in (id:#{work_packageable_thing_id}))
+      } 
     "
   end
 end
