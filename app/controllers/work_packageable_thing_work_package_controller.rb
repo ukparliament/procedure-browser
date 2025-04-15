70a30ed48e82080ea9508dacf35589c116b3e609
fantasticlife <michael@smethur.st>
2025-04-15 15:38:59 +0100

    new controller to list work packages for a work packaged thing

---
+++ app/controllers/work_packageable_thing_work_package_controller.rb
@@ -0,0 +1,25 @@
+class WorkPackageableThingWorkPackageController < ApplicationController
+
+  # We include modules required to get work packageable thing.
+  include Sparql::Get::WorkPackageableThing
+  include Sparql::Queries::WorkPackageableThing
+  include Sparql::Get::WorkPackageableThingWorkPackages
+  include Sparql::Queries::WorkPackageableThingWorkPackages
+  include Sparql::Get::Response
+
+  def index
+    work_packageable_thing_id = params[:work_packageable_thing]
+    @work_packageable_thing = get_work_packageable_thing( work_packageable_thing_id )
+    
+    # We get all work packages for this work packageable thing.
+    @work_packageable_thing_work_packages = get_work_packageable_thing_work_packages( work_packageable_thing_id )
+    
+    @page_title = "#{@work_packageable_thing.label} - work packages"
+    @multiline_page_title = "#{@work_packageable_thing.label} <span class='subhead'>Work packages</span>".html_safe
+    @description = "Work packages for #{@work_packageable_thing.label}."
+    @crumb << { label: 'Work packageable things', url: work_packageable_thing_list_url }
+    @crumb << { label: @work_packageable_thing.label, url: work_packageable_thing_show_url }
+    @crumb << { label: 'Work packages', url: nil }
+    @section = 'work-packageable-things'
+  end
+end
