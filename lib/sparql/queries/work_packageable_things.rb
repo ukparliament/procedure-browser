745c2f547475b99646a07e4b07ba944f0fbff9c5
fantasticlife <michael@smethur.st>
2025-04-25 15:17:56 +0100

    query for list of work packageable things now has date of last work package being made available

--- lib/sparql/queries/work_packageable_things.rb
+++ lib/sparql/queries/work_packageable_things.rb
@@ -7,10 +7,17 @@ module Sparql::Queries::WorkPackageableThings
       PREFIX : <https://id.parliament.uk/schema/>
       PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
       PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-      select * where {
+      select ?WorkPackagedThing ?workPackagedThingName where {
         ?WorkPackagedThing a :WorkPackagedThing;
                            :name ?workPackagedThingName.
-      } LIMIT #{limit} OFFSET #{offset}
+        ?WorkPackagedThing :workPackagedThingHasWorkPackage ?workPackage.
+        optional {?workPackage :workPackageHasBusinessItem ?businessItem .
+                  ?businessItem :businessItemHasProcedureStep ?step;
+                         :businessItemDate ?businessItemDate.
+                  filter (?step in (id:isWn7s3K, id:cspzmb6w, id:ITNO9JWr, id:otscOTzB))}
+            
+              } Order by desc (?businessItemDate)
+      LIMIT #{limit} OFFSET #{offset}
     "
   end
 end
