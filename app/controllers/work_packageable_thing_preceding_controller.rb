666e731f445b433caeeb0a0f28c5033620764f45
fantasticlife <michael@smethur.st>
2025-04-25 15:23:12 +0100

    basic controller and template for preceding work packageable things for a work packageable thing

---
+++ app/controllers/work_packageable_thing_preceding_controller.rb
@@ -0,0 +1,25 @@
+class WorkPackageableThingPrecedingController < ApplicationController
+
+  # We include modules required to get work packageable thing.
+  include Sparql::Get::WorkPackageableThing
+  include Sparql::Queries::WorkPackageableThing
+  include Sparql::Get::WorkPackageableThingPreceding
+  include Sparql::Queries::WorkPackageableThingPreceding
+  include Sparql::Get::Response
+
+  def index
+    work_packageable_thing_id = params[:work_packageable_thing]
+    @work_packageable_thing = get_work_packageable_thing( work_packageable_thing_id )
+    
+    @work_packageable_things = get_work_packageable_thing_preceding( work_packageable_thing_id )
+    
+    @page_title = "#{@work_packageable_thing.label} - preceding work packageable things"
+    @multiline_page_title = "#{@work_packageable_thing.label} <span class='subhead'>Preceding work packageable things</span>".html_safe
+    @description = "Preceding work packageable things for #{@work_packageable_thing.label}."
+    @crumb << { label: 'Work packageable things', url: work_packageable_thing_list_url }
+    @crumb << { label: @work_packageable_thing.label, url: work_packageable_thing_show_url }
+    @crumb << { label: 'Preceding work packageable things', url: nil }
+    @section = 'work-packageable-things'
+    @subsection = 'preceding'
+  end
+end
