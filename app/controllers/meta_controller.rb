74f133d62098d89e0006161d6d6ef0f5bb77f216
fantasticlife <michael@smethur.st>
2025-04-28 21:14:04 +0100

    added controller action for meta listing. Added title, description etc for meta sitemap

--- app/controllers/meta_controller.rb
+++ app/controllers/meta_controller.rb
@@ -1,5 +1,15 @@
 class MetaController < ApplicationController
 
+  def index
+    @page_title = 'Meta'
+    @description = 'Meta.'
+    @crumb << { label: 'Meta', url: nil }
+  end
+
   def sitemap
+    @page_title = 'Sitemap'
+    @description = 'Sitemap.'
+    @crumb << { label: 'Meta', url: meta_list_url }
+    @crumb << { label: 'Sitemap', url: nil }
   end
 end
