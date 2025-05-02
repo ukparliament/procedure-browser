247dc236dcd7a2d6734863dc65b86e69d137c932
fantasticlife <michael@smethur.st>
2025-05-02 15:50:06 +0100

    basic model for organisation accountable to parliament

---
+++ app/models/organisation_accountable_to_parliament.rb
@@ -0,0 +1,13 @@
+class OrganisationAccountableToParliament
+  
+  attr_accessor :identifier
+  attr_accessor :label
+  attr_accessor :date
+  attr_accessor :year
+  attr_accessor :act_number
+  attr_accessor :uri
+  
+  def id
+    self.identifier.split( '/' ).last
+  end
+end
