68c82964e6e04a94b87bab24fda86b783eb97a04
fantasticlife <michael@smethur.st>
2025-05-02 15:49:29 +0100

    added query and getter for a list of organisations accountable to parliament

---
+++ lib/sparql/get/organisations_accountable_to_parliament.rb
@@ -0,0 +1,33 @@
+module Sparql::Get::OrganisationsAccountableToParliament
+
+  # A method to get an array of all organisations accountable to Parliament on this page with this many results per page.
+  def get_organisations_accountable_to_parliament( page_number, results_per_page )
+  
+    # We calculate the result offset.
+    result_offset = ( ( page_number - 1) * results_per_page )
+
+    # We get the all organisations accountable to Parliament query passing parameters for the number of results per page and the result offset.
+    request_body = organisations_accountable_to_parliament_query( results_per_page, result_offset )
+    
+    # We get the SPARQL response as a CSV.
+    csv = get_sparql_response_as_csv( request_body )
+  
+    # We construct an array to hold the organisations accountable to Parliament.
+    organisations_accountable_to_parliament = []
+  
+    # For each row in the CSV ...
+    csv.each do |row|
+  
+      # ... we create a new organisation accountable to Parliament object ...
+      organisation_accountable_to_parliament = OrganisationAccountableToParliament.new
+      organisation_accountable_to_parliament.identifier = row['Organisations']
+      organisation_accountable_to_parliament.label = row['Name']
+      
+      # ... and add it to the array of organisations accountable to Parliament.
+      organisations_accountable_to_parliament << organisation_accountable_to_parliament
+    end
+  
+    # We return the array of organisations accountable to Parliament.
+    organisations_accountable_to_parliament
+  end
+end
---
+++ lib/sparql/queries/organisations_accountable_to_parliament.rb
@@ -0,0 +1,24 @@
+module Sparql::Queries::OrganisationsAccountableToParliament
+
+  # A SPARQL query to get all organisations accountable to Parliament taking parameters of limit and offset.
+  def organisations_accountable_to_parliament_query( limit, offset )
+    "
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX id: <https://id.parliament.uk/>
+      SELECT * WHERE {
+       { ?Organisations a :GovernmentOrganisation.}
+       UNION {
+      ?Organisations a :ArmsLengthBody.
+                      }
+        ?Organisations :name ?Name.
+  
+      } order by ?Name
+
+      Offset #{offset} Limit #{limit}
+
+      
+    "
+  end
+end
