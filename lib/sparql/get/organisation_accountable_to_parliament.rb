02ff23513473357216d4c26d4cdf4b31a9fbf7a4
fantasticlife <michael@smethur.st>
2025-05-06 13:46:11 +0100

    added query and getter for an organisation accountable to parliament

---
+++ lib/sparql/get/organisation_accountable_to_parliament.rb
@@ -0,0 +1,24 @@
+module Sparql::Get::OrganisationAccountableToParliament
+
+  # A method to get an organisation accountable to Parliament.
+  def get_organisation_accountable_to_parliament( organisation_accountable_to_parliament_id )
+  
+    # We get the organisation accountable to Parliament query.
+    request_body = organisation_accountable_to_parliament_query( organisation_accountable_to_parliament_id )
+  
+    # We get the SPARQL response as a CSV.
+    csv = get_sparql_response_as_csv( request_body )
+    
+    # We take the one and only row from the CSV ...
+    csv.take( 1 ).each do |row|
+    
+      # ... and create a new organisation accountable to Parliament object.
+      organisation_accountable_to_parliament = OrganisationAccountableToParliament.new
+      organisation_accountable_to_parliament.identifier = row['Organisations']
+      organisation_accountable_to_parliament.label = row['Name']
+      
+      # We return the organisation accountable to Parliament object object.
+      return organisation_accountable_to_parliament
+    end
+  end
+end
---
+++ lib/sparql/queries/organisation_accountable_to_parliament.rb
@@ -0,0 +1,20 @@
+module Sparql::Queries::OrganisationAccountableToParliament
+
+  # A SPARQL query to get an organisation accountable to parliament.
+  def organisation_accountable_to_parliament_query( organisation_accountable_to_parliament_id )
+    "
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX id: <https://id.parliament.uk/>
+      SELECT * WHERE {
+       { ?Organisations a :GovernmentOrganisation.}
+       UNION {
+      ?Organisations a :ArmsLengthBody.
+                      }
+        ?Organisations :name ?Name.
+        filter (?Organisations in (id:#{organisation_accountable_to_parliament_id}))
+      } 
+    "
+  end
+end
