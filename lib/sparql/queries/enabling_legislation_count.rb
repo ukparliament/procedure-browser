7aea716e47f94927a31b22d554c95fc2c87c15ee
fantasticlife <michael@smethur.st>
2025-05-06 16:48:14 +0100

    new query to get a count of enabling legislation

--- lib/sparql/queries/enabling_legislation_count.rb
+++ lib/sparql/queries/enabling_legislation_count.rb
@@ -6,15 +6,23 @@ module Sparql::Queries::EnablingLegislationCount
       PREFIX : <https://id.parliament.uk/schema/>
       PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
       PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-      select (COUNT(DISTINCT ?Act) AS ?count)  WHERE {
-       ?Act a :ActOfParliament.
-        ?Act :actOfParliamentName ?Name.
-         OPTIONAL {?Act :actOfParliamentNumber ?Number}
-        OPTIONAL {?Act :actOfParliamentYear ?Year}
-         OPTIONAL{  ?Act :actOfParliamentRoyalAssentDate ?Date}
-        OPTIONAL {?Act :actOfParliamentUrl ?URL}
-  
-      } 
+      PREFIX id: <https://id.parliament.uk/>
+      SELECT distinct (COUNT(DISTINCT ?EnablingThing) AS ?count) WHERE {
+        ?EnablingThing a :EnablingThing;  
+             :enabling ?EnabledThing.
+        Optional { ?EnablingThing  :actOfParliamentName ?Name;}
+        OPTIONAL {?EnablingThing :actOfParliamentNumber ?Number}
+        OPTIONAL {?EnablingThing:actOfParliamentYear ?Year}
+        OPTIONAL{  ?EnablingThing :actOfParliamentRoyalAssentDate ?Date}
+        OPTIONAL {?EnablingThing :actOfParliamentUrl ?URL}
+        ?EnabledThing :name ?EnabledThingName;
+        :workPackagedThingHasWorkPackage ?workPackage.
+                optional {?workPackage :workPackageHasBusinessItem ?businessItem .
+                  ?businessItem :businessItemHasProcedureStep ?step;
+                         :businessItemDate ?businessItemDate.
+                  filter (?step in (id:isWn7s3K, id:cspzmb6w, id:ITNO9JWr, id:otscOTzB))}
+          
+      }
     "
   end
 end
