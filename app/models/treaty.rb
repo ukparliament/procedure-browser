4936e288c2568729da07a01e2a809176ee7e5715
fantasticlife <michael@smethur.st>
2025-05-20 17:38:53 +0100

    updated treaty model to add attribute accessors for series membership and remove making available stuff

--- app/models/treaty.rb
+++ app/models/treaty.rb
@@ -2,8 +2,12 @@ class Treaty
   
   attr_accessor :identifier
   attr_accessor :label
-  attr_accessor :laid_on
-  attr_accessor :series_citation
+  attr_accessor :country_series_membership
+  attr_accessor :country_series_membership_citation
+  attr_accessor :european_union_series_membership
+  attr_accessor :european_union_series_membership_citation
+  attr_accessor :miscellaneous_series_membership
+  attr_accessor :miscellaneous_series_membership_citation
   
   def id
     self.identifier.split( '/' ).last
