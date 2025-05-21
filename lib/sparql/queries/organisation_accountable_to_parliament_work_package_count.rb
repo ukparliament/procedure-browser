e15e5815b846a89a85ad7a9830e5563b1896902d
fantasticlife <michael@smethur.st>
2025-05-21 19:29:29 +0100

    organisations accountable to parliament now list work packages, not making availables

--- lib/sparql/queries/organisation_accountable_to_parliament_work_package_count.rb
+++ lib/sparql/queries/organisation_accountable_to_parliament_work_package_count.rb
@@ -1,13 +1,13 @@
-module Sparql::Queries::OrganisationAccountableToParliamentMakingAvailableCount
+module Sparql::Queries::OrganisationAccountableToParliamentWorkPackageCount
 
-  # A SPARQL query to get a count of all making avilabless for an organisation accountable to Parliament.
-  def organisation_accountable_to_parliament_making_available_count_query( organisation_accountable_to_parliament_id )
+  # A SPARQL query to get a count of all work packages for an organisation accountable to Parliament.
+  def organisation_accountable_to_parliament_work_package_count_query( organisation_accountable_to_parliament_id )
     "
       PREFIX : <https://id.parliament.uk/schema/>
       PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
       PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
       PREFIX id: <https://id.parliament.uk/>
-      SELECT distinct (count(distinct ?LaidThing) as ?count) WHERE {
+      SELECT distinct  (count(distinct ?LaidThing) as ?count) WHERE {
        { ?Organisations a :GovernmentOrganisation.}
        UNION {
       ?Organisations a :ArmsLengthBody.
@@ -19,9 +19,12 @@ module Sparql::Queries::OrganisationAccountableToParliamentMakingAvailableCount
                 :layingDate ?LaidDate.
         filter (?Step in (id:cspzmb6w, id:isWn7s3K))
         ?LaidThing :name ?LaidThingName. 
+        ?LaidThing :workPackagedThingHasWorkPackage ?workPackage.
+                           ?workPackage :workPackageHasProcedure ?procedure.
+                ?procedure :name ?procedureName.
+               optional { ?workPackage :workPackageHasCalculationStyle ?calculationStyle. 
+                  ?calculationStyle :name ?calculationStyleName. }
         filter (?Organisations in (id:#{organisation_accountable_to_parliament_id}))
-  
-  
       }
     "
   end
