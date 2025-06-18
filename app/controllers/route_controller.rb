a707c4091c47d65f5735d59a033b4ef1084719e7
fantasticlife <michael@smethur.st>
2025-06-18 09:14:04 +0100

    included procedures in route show controller and made it render the route procedures list

--- app/controllers/route_controller.rb
+++ app/controllers/route_controller.rb
@@ -1,10 +1,12 @@
 class RouteController < ApplicationController
 
-  # We include modules required to get all routes and a routes with its steps.
+  # We include modules required to get all routes and a routes with its steps and procedures.
   include Sparql::Get::Routes
   include Sparql::Queries::Routes
   include Sparql::Get::Route
   include Sparql::Queries::Route
+  include Sparql::Get::RouteProcedures
+  include Sparql::Queries::RouteProcedures
   include Sparql::Get::Response
 
   def index
@@ -20,10 +22,14 @@ class RouteController < ApplicationController
     route_id = params[:route]
     @route = get_route( route_id )
     
+    @procedures = get_route_procedures( route_id )
+    
     @page_title = @route.title
     @description = @route.description
     @crumb << { label: 'Routes', url: route_list_url }
     @crumb << { label: @route.crumb_label, url: nil }
     @section = 'routes'
+    
+    render :template => 'route_procedure/index'
   end
 end
