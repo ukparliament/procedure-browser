b4cdf55123213c81796601677983e9248036d2ed
fantasticlife <michael@smethur.st>
2025-06-23 14:48:54 +0100

    revised query for current work packages from jayne

--- lib/sparql/queries/work_packages_current.rb
+++ lib/sparql/queries/work_packages_current.rb
@@ -4,27 +4,27 @@ module Sparql::Queries::WorkPackagesCurrent
   def work_packages_current_query( limit, offset )
     "
       PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-             PREFIX : <https://id.parliament.uk/schema/>
-             PREFIX id: <https://id.parliament.uk/>
-             SELECT * where {
-  
-              ?Paper a :WorkPackagedThing .  
-                  ?Paper :name ?Papername ;
-                  :workPackagedThingHasWorkPackage ?workPackage.
-               optional {?workPackage :workPackageHasBusinessItem ?businessItem .
-                 ?businessItem :businessItemHasProcedureStep ?step;
-                        :businessItemDate ?businessItemDate.
-                 filter (?step in (id:isWn7s3K, id:cspzmb6w, id:ITNO9JWr, id:otscOTzB))}
-                 ?workPackage :workPackageHasProcedure ?procedure.
-               ?procedure :name ?procedureName.
-              optional { ?workPackage :workPackageHasCalculationStyle ?calculationStyle. 
-                 ?calculationStyle :name ?calculationStyleName. }
-        MINUS {  ?workPackage   :workPackageHasBusinessItem ?bi2.
-       ?bi2 :businessItemHasProcedureStep ?stepId2.
-          ?stepId2 :procedureStepHasProcedureStepCollectionMembership/:procedureStepCollectionMembershipHasProcedureStepCollection id:TRohjSuI}
-             } Order by desc (?businessItemDate)
+       PREFIX : <https://id.parliament.uk/schema/>
+       PREFIX id: <https://id.parliament.uk/>
+       SELECT ?Paper ?Papername ?workPackage ?businessItem ?step ?businessItemDate ?procedure ?procedureName ?calculationStyle ?calculationStyleName where {
 
-             LIMIT #{limit} OFFSET #{offset}
+        ?Paper a :WorkPackagedThing .  
+            ?Paper :name ?Papername ;
+            :workPackagedThingHasWorkPackage ?workPackage.
+         optional {?workPackage :workPackageHasBusinessItem ?businessItem .
+           ?businessItem :businessItemHasProcedureStep ?step;
+                  :businessItemDate ?businessItemDate.
+           filter (?step in (id:isWn7s3K, id:cspzmb6w, id:ITNO9JWr, id:otscOTzB))}
+           ?workPackage :workPackageHasProcedure ?procedure.
+         ?procedure :name ?procedureName.
+        optional { ?workPackage :workPackageHasCalculationStyle ?calculationStyle. 
+           ?calculationStyle :name ?calculationStyleName. }
+  MINUS {  ?workPackage   :workPackageHasBusinessItem ?bi2.
+  ?bi2 :businessItemHasProcedureStep ?stepId2.
+    ?stepId2 :procedureStepHasProcedureStepCollectionMembership/:procedureStepCollectionMembershipHasProcedureStepCollection id:TRohjSuI}
+       } Order by desc (?businessItemDate)
+
+       LIMIT #{limit} OFFSET #{offset}
     "
   end
 end
