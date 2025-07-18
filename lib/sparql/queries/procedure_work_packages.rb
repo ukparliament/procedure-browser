506ae53bca1b34726116bea450ccd8a74adc3f7e
fantasticlife <michael@smethur.st>
2025-07-18 15:08:38 +0100

    included brought to attention of parliament date for procedure work packages

--- lib/sparql/queries/procedure_work_packages.rb
+++ lib/sparql/queries/procedure_work_packages.rb
@@ -4,25 +4,30 @@ module Sparql::Queries::ProcedureWorkPackages
   def procedure_work_packages_query( procedure_id, limit, offset )
     "
       PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-      PREFIX : <https://id.parliament.uk/schema/>
-      PREFIX id: <https://id.parliament.uk/>
-      SELECT distinct * WHERE {
-        ?Procedure a :Procedure;
-                   :name ?ProcedureName.
-           filter (?Procedure in (id:#{procedure_id}))
-        ?Procedure :procedureHasWorkPackage ?workPackage.
-        ?workPackage :workPackageHasWorkPackagedThing ?workPackagedThing.
-        ?workPackagedThing :name ?workPackagedThingName. 
-        optional {?workPackage :workPackageHasBusinessItem ?businessItem .
-                  ?businessItem :businessItemHasProcedureStep ?step;
-                         :businessItemDate ?businessItemDate.
-                  filter (?step in (id:isWn7s3K, id:cspzmb6w, id:ITNO9JWr, id:otscOTzB))}
-             optional { ?workPackage :workPackageHasCalculationStyle ?calculationStyle. 
-                ?calculationStyle :name ?calculationStyleName. }
-            }     Order by desc (?businessItemDate)
+           PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+           PREFIX : <https://id.parliament.uk/schema/>
+           PREFIX id: <https://id.parliament.uk/>
+           SELECT distinct ?Procedure ?ProcedureName ?workPackage ?workPackagedThing ?workPackagedThingName ?combinedDate ?calculationStyle ?calculationStyleName  WHERE {
+             ?Procedure a :Procedure;
+                        :name ?ProcedureName.
+                filter (?Procedure in (id:#{procedure_id}))
+             ?Procedure :procedureHasWorkPackage ?workPackage.
+             ?workPackage :workPackageHasWorkPackagedThing ?workPackagedThing.
+             ?workPackagedThing :name ?workPackagedThingName. 
+            optional {   ?workPackage :workPackageHasBusinessItem ?businessItem .
+                ?businessItem :businessItemHasProcedureStep ?step;
+                       :businessItemDate ?businessItemDate.
+         filter (?step in (id:isWn7s3K, id:cspzmb6w, id:ITNO9JWr, id:otscOTzB))}
+         optional {    ?workPackage :workPackageHasBusinessItem ?businessItem2 .
+                ?businessItem2 :businessItemHasProcedureStep ?step2;
+                               :businessItemDate ?businessItemDate2.
+         filter (?step2 in (id:AmYrFxwO))}
+                  optional { ?workPackage :workPackageHasCalculationStyle ?calculationStyle. 
+                     ?calculationStyle :name ?calculationStyleName. }
+         BIND(COALESCE(?businessItemDate, ?businessItemDate2) AS ?combinedDate)
+     } Order by desc(?combinedDate) ?workPackagedThingName
 
-              LIMIT #{limit} OFFSET #{offset}
+                   LIMIT #{limit} OFFSET #{offset}
     "
   end
 end
