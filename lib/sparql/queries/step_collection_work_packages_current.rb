c8437dd0b4be13248553148d4dd5618064b44073
fantasticlife <michael@smethur.st>
2025-07-18 15:39:24 +0100

    included brought to attention of parliament date for step collection current work packages

--- lib/sparql/queries/step_collection_work_packages_current.rb
+++ lib/sparql/queries/step_collection_work_packages_current.rb
@@ -4,33 +4,41 @@ module Sparql::Queries::StepCollectionWorkPackagesCurrent
   def step_collection_work_packages_current_query( step_collection_id, limit, offset )
     "
       PREFIX id: <https://id.parliament.uk/>
-                 PREFIX : <https://id.parliament.uk/schema/>
-                 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-                 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                 SELECT distinct ?workPackage ?workPackagedThing ?workPackagedThingName ?procedure ?procedureName ?MadeAvailableDate  WHERE {
+                       PREFIX : <https://id.parliament.uk/schema/>
+                       PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                       PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                       SELECT distinct ?workPackage ?workPackagedThing ?workPackagedThingName ?procedure ?procedureName ?combinedDate  WHERE {
 
-                   ?StepCollection a :ProcedureStepCollection;
-                             :name ?stepCollectionName.
-                        filter (?StepCollection IN (id:#{step_collection_id}))
-                  ?StepCollection :procedureStepCollectionHasProcedureStepCollectionMembership ?StepCollectionMembership.
-                     ?StepCollectionMembership :procedureStepCollectionMembershipHasProcedureStep ?procedureStep.
+                         ?StepCollection a :ProcedureStepCollection;
+                                   :name ?stepCollectionName.
+                              filter (?StepCollection IN (id:#{step_collection_id}))
+                        ?StepCollection :procedureStepCollectionHasProcedureStepCollectionMembership ?StepCollectionMembership.
+                           ?StepCollectionMembership :procedureStepCollectionMembershipHasProcedureStep ?procedureStep.
     
-             ?procedureStep :procedureStepHasBusinessItem ?businessItem. 
-             ?businessItem :businessItemHasWorkPackage ?workPackage. 
-               ?workPackage :workPackageHasWorkPackagedThing ?workPackagedThing;
-                            :workPackageHasProcedure ?procedure.
-               ?workPackagedThing :name ?workPackagedThingName. 
-             	?procedure :name ?procedureName.  MINUS {  ?workPackage   :workPackageHasBusinessItem ?bi2.
-         ?bi2 :businessItemHasProcedureStep ?stepId2.
-            ?stepId2 :procedureStepHasProcedureStepCollectionMembership/:procedureStepCollectionMembershipHasProcedureStepCollection id:TRohjSuI}
-             optional {?workPackage :workPackageHasBusinessItem ?businessItem2 .
-                       ?businessItem2 :businessItemHasProcedureStep ?step2;
-                              :businessItemDate ?MadeAvailableDate.
-                       filter (?step2 in (id:isWn7s3K, id:cspzmb6w, id:ITNO9JWr, id:otscOTzB))}
-    
-           } Order by desc(?MadeAvailableDate) 
+                   ?procedureStep :procedureStepHasBusinessItem ?businessItem. 
+                   ?businessItem :businessItemHasWorkPackage ?workPackage. 
+                     ?workPackage :workPackageHasWorkPackagedThing ?workPackagedThing;
+                                  :workPackageHasProcedure ?procedure.
+                     ?workPackagedThing :name ?workPackagedThingName. 
+                   	?procedure :name ?procedureName.  MINUS {  ?workPackage   :workPackageHasBusinessItem ?bi2.
+               ?bi2 :businessItemHasProcedureStep ?stepId2.
+                  ?stepId2 :procedureStepHasProcedureStepCollectionMembership/:procedureStepCollectionMembershipHasProcedureStepCollection id:TRohjSuI}
+                      optional {   ?workPackage :workPackageHasBusinessItem ?businessItem1 .
+                 ?businessItem1 :businessItemHasProcedureStep ?step;
+                        :businessItemDate ?businessItemDate.
+          filter (?step in (id:isWn7s3K, id:cspzmb6w, id:ITNO9JWr, id:otscOTzB))}
+        optional {    ?workPackage :workPackageHasBusinessItem ?businessItem2 .
+                 ?businessItem2 :businessItemHasProcedureStep ?step2;
+                                :businessItemDate ?businessItemDate2.
+          filter (?step2 in (id:AmYrFxwO))}
+        			?workPackage :workPackageHasWorkPackagedThing ?workPackagedThing.
+        			?workPackagedThing :name ?workPackagedThingName. 
+                  ?workPackage :workPackageHasProcedure ?procedure.
+                ?procedure :name ?procedureName.
+         BIND(COALESCE(?businessItemDate, ?businessItemDate2) AS ?combinedDate)
+       } Order by desc(?combinedDate) ?workPackagedThingName
+                           offset #{offset} limit #{limit}
                      
-                      LIMIT #{limit} OFFSET #{offset}
     
     "
   end
