e458b10d4ef853d74b486596ed7d9d9848d0982f
fantasticlife <michael@smethur.st>
2025-07-18 15:29:54 +0100

    included brought to attention of parliament date for calculation style current work packages

--- lib/sparql/queries/calculation_style_work_packages_current.rb
+++ lib/sparql/queries/calculation_style_work_packages_current.rb
@@ -4,27 +4,32 @@ module Sparql::Queries::CalculationStyleWorkPackagesCurrent
   def calculation_style_work_packages_current_query( calculation_style_id, limit, offset )
     "
       PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-      PREFIX : <https://id.parliament.uk/schema/>
-      PREFIX id: <https://id.parliament.uk/>
-      SELECT ?CalculationStyle ?CalculationStyleName ?workPackage ?MadeAvailableDate ?workPackagedThing ?workPackagedThingName ?procedure ?procedureName WHERE {
-        ?CalculationStyle a :CalculationStyle;
-                   :name ?CalculationStyleName;
-                   :calculationStyleHasWorkPackage ?workPackage.
-  filter (?CalculationStyle in (id:#{calculation_style_id}))
-                optional {?workPackage :workPackageHasBusinessItem ?businessItem .
-            ?businessItem :businessItemHasProcedureStep ?step;
-                   :businessItemDate ?MadeAvailableDate.
-            filter (?step in (id:isWn7s3K, id:cspzmb6w, id:ITNO9JWr, id:otscOTzB))}
-  			?workPackage :workPackageHasWorkPackagedThing ?workPackagedThing.
-  			?workPackagedThing :name ?workPackagedThingName. 
-            ?workPackage :workPackageHasProcedure ?procedure.
-          ?procedure :name ?procedureName.
-      MINUS {  ?workPackage   :workPackageHasBusinessItem ?bi2.
-  ?bi2 :businessItemHasProcedureStep ?stepId2.
-     ?stepId2 :procedureStepHasProcedureStepCollectionMembership/:procedureStepCollectionMembershipHasProcedureStepCollection id:TRohjSuI}
-        } Order by desc (?MadeAvailableDate)
-      LIMIT #{limit} OFFSET #{offset}
+           PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+           PREFIX : <https://id.parliament.uk/schema/>
+           PREFIX id: <https://id.parliament.uk/>
+             SELECT ?CalculationStyle ?CalculationStyleName ?workPackage ?combinedDate ?workPackagedThing ?workPackagedThingName ?procedure ?procedureName WHERE {
+             ?CalculationStyle a :CalculationStyle;
+                        :name ?CalculationStyleName;
+                        :calculationStyleHasWorkPackage ?workPackage.
+       filter (?CalculationStyle in (id:#{calculation_style_id}))
+                     optional {   ?workPackage :workPackageHasBusinessItem ?businessItem .
+                ?businessItem :businessItemHasProcedureStep ?step;
+                       :businessItemDate ?businessItemDate.
+         filter (?step in (id:isWn7s3K, id:cspzmb6w, id:ITNO9JWr, id:otscOTzB))}
+         optional {    ?workPackage :workPackageHasBusinessItem ?businessItem2 .
+                ?businessItem2 :businessItemHasProcedureStep ?step2;
+                               :businessItemDate ?businessItemDate2.
+         filter (?step2 in (id:AmYrFxwO))}
+       			?workPackage :workPackageHasWorkPackagedThing ?workPackagedThing.
+       			?workPackagedThing :name ?workPackagedThingName. 
+                 ?workPackage :workPackageHasProcedure ?procedure.
+               ?procedure :name ?procedureName.
+            MINUS {  ?workPackage   :workPackageHasBusinessItem ?bi2.
+       ?bi2 :businessItemHasProcedureStep ?stepId2.
+          ?stepId2 :procedureStepHasProcedureStepCollectionMembership/:procedureStepCollectionMembershipHasProcedureStepCollection id:TRohjSuI}
+     BIND(COALESCE(?businessItemDate, ?businessItemDate2) AS ?combinedDate)
+      } Order by desc(?combinedDate) ?workPackagedThingName
+     limit #{limit} offset #{offset}
     
     "
   end
