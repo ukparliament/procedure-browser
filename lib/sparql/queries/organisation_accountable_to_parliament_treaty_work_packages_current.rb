895c8698da1733ceb0cb77ed1388419ede937d98
fantasticlife <michael@smethur.st>
2025-07-18 14:43:14 +0100

    included brought to attention of parliament date for organisation accountable to parliament treaty current work packages

--- lib/sparql/queries/organisation_accountable_to_parliament_treaty_work_packages_current.rb
+++ lib/sparql/queries/organisation_accountable_to_parliament_treaty_work_packages_current.rb
@@ -4,51 +4,57 @@ module Sparql::Queries::OrganisationAccountableToParliamentTreatyWorkPackagesCur
   def organisation_accountable_to_parliament_treaty_work_packages_current_query( organisation_accountable_to_parliament_id, limit, offset )
     "
       PREFIX : <https://id.parliament.uk/schema/>
-          PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-          PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-          PREFIX id: <https://id.parliament.uk/>
-          SELECT distinct ?WorkPackage ?procedure ?procedureName ?treaty ?TreatyName ?treatyHasCountrySeriesMembership ?countrySeriesItemCitation ?treatyHasEuropeanUnionSeriesMembership ?europeanSeriesItemCitation ?treatyHasMiscellaneousSeriesMembership ?miscSeriesItemCitation ?businessItemDate ?calculationStyle ?calculationStyleName  WHERE {
-           { ?Organisations a :GovernmentOrganisation.}
-           UNION {
-          ?Organisations a :ArmsLengthBody.
-                          }
-            ?Organisations :name ?Name;
-                           :leadGovernmentOrganisationHasTreaty ?treaty.
-            ?treaty :name ?TreatyName;
-                    :workPackagedThingHasWorkPackage ?WorkPackage.
-      ?WorkPackage :workPackageHasProcedure ?procedure.
-      ?procedure :name ?procedureName.
-              OPTIONAL
-            {
-              ?treaty :treatyHasCountrySeriesMembership ?treatyHasCountrySeriesMembership .
-              ?treatyHasCountrySeriesMembership :seriesItemCitation ?countrySeriesItemCitation .
-            }
-            OPTIONAL
-            {
-              ?treaty :treatyHasEuropeanUnionSeriesMembership ?treatyHasEuropeanUnionSeriesMembership .
-              ?treatyHasEuropeanUnionSeriesMembership :seriesItemCitation ?europeanSeriesItemCitation .
-            }
-            OPTIONAL
-            {
-              ?treaty :treatyHasMiscellaneousSeriesMembership ?treatyHasMiscellaneousSeriesMembership .
-              ?treatyHasMiscellaneousSeriesMembership :seriesItemCitation ?miscSeriesItemCitation .
-            }
+                PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                PREFIX id: <https://id.parliament.uk/>
+                SELECT distinct ?workPackage ?procedure ?procedureName ?treaty ?TreatyName ?treatyHasCountrySeriesMembership ?countrySeriesItemCitation ?treatyHasEuropeanUnionSeriesMembership ?europeanSeriesItemCitation ?treatyHasMiscellaneousSeriesMembership ?miscSeriesItemCitation ?combinedDate ?calculationStyle ?calculationStyleName  WHERE {
+                 { ?Organisations a :GovernmentOrganisation.}
+                 UNION {
+                ?Organisations a :ArmsLengthBody.
+                                }
+                  ?Organisations :name ?Name;
+                                 :leadGovernmentOrganisationHasTreaty ?treaty.
+                  ?treaty :name ?TreatyName;
+                          :workPackagedThingHasWorkPackage ?workPackage.
+            ?workPackage :workPackageHasProcedure ?procedure.
+            ?procedure :name ?procedureName.
+                    OPTIONAL
+                  {
+                    ?treaty :treatyHasCountrySeriesMembership ?treatyHasCountrySeriesMembership .
+                    ?treatyHasCountrySeriesMembership :seriesItemCitation ?countrySeriesItemCitation .
+                  }
+                  OPTIONAL
+                  {
+                    ?treaty :treatyHasEuropeanUnionSeriesMembership ?treatyHasEuropeanUnionSeriesMembership .
+                    ?treatyHasEuropeanUnionSeriesMembership :seriesItemCitation ?europeanSeriesItemCitation .
+                  }
+                  OPTIONAL
+                  {
+                    ?treaty :treatyHasMiscellaneousSeriesMembership ?treatyHasMiscellaneousSeriesMembership .
+                    ?treatyHasMiscellaneousSeriesMembership :seriesItemCitation ?miscSeriesItemCitation .
+                  }
  
-             filter (?Organisations in (id:#{organisation_accountable_to_parliament_id}))
-         optional {?WorkPackage :workPackageHasBusinessItem ?businessItem .
-                      ?businessItem :businessItemHasProcedureStep ?step;
-                             :businessItemDate ?businessItemDate.
-                      filter (?step in (id:isWn7s3K, id:cspzmb6w, id:ITNO9JWr, id:otscOTzB))}
-                      ?WorkPackage :workPackageHasProcedure ?procedure.
-                    ?procedure :name ?procedureName.
-                         optional { ?WorkPackage :workPackageHasCalculationStyle ?calculationStyle. 
-               ?calculationStyle :name ?calculationStyleName. }
-        MINUS {  ?WorkPackage   :workPackageHasBusinessItem ?bi2.
-      ?bi2 :businessItemHasProcedureStep ?stepId2.
-        ?stepId2 :procedureStepHasProcedureStepCollectionMembership/:procedureStepCollectionMembershipHasProcedureStepCollection id:TRohjSuI}
-          } Order by desc(?businessItemDate) ?TreatyName                   
+                   filter (?Organisations in (id:#{organisation_accountable_to_parliament_id}))
+               optional {   ?workPackage :workPackageHasBusinessItem ?businessItem .
+                 ?businessItem :businessItemHasProcedureStep ?step;
+                        :businessItemDate ?businessItemDate.
+          filter (?step in (id:isWn7s3K, id:cspzmb6w, id:ITNO9JWr, id:otscOTzB))}
+          optional {    ?workPackage :workPackageHasBusinessItem ?businessItem2 .
+                 ?businessItem2 :businessItemHasProcedureStep ?step2;
+                                :businessItemDate ?businessItemDate2.
+          filter (?step2 in (id:AmYrFxwO))}
+                            ?workPackage :workPackageHasProcedure ?procedure.
+                          ?procedure :name ?procedureName.
+                               optional { ?workPackage :workPackageHasCalculationStyle ?calculationStyle. 
+                     ?calculationStyle :name ?calculationStyleName. }
+        MINUS {  ?workPackage   :workPackageHasBusinessItem ?bi2.
+            ?bi2 :businessItemHasProcedureStep ?stepId2.
+              ?stepId2 :procedureStepHasProcedureStepCollectionMembership/:procedureStepCollectionMembershipHasProcedureStepCollection id:TRohjSuI}
+      BIND(COALESCE(?businessItemDate, ?businessItemDate2) AS ?combinedDate)
+      } Order by desc(?combinedDate)                
       
-          offset #{offset} limit #{limit}
+                offset #{offset} limit #{limit}
+    
     
     "
   end
