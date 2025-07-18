cf6f450814068e21b8a92561f2e9d2d20f977d1d
fantasticlife <michael@smethur.st>
2025-07-18 15:02:37 +0100

    included brought to attention of parliament date for current work packages

--- lib/sparql/queries/work_packages_current.rb
+++ lib/sparql/queries/work_packages_current.rb
@@ -4,27 +4,32 @@ module Sparql::Queries::WorkPackagesCurrent
   def work_packages_current_query( limit, offset )
     "
       PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-       PREFIX : <https://id.parliament.uk/schema/>
-       PREFIX id: <https://id.parliament.uk/>
-       SELECT ?Paper ?Papername ?workPackage ?businessItem ?step ?businessItemDate ?procedure ?procedureName ?calculationStyle ?calculationStyleName where {
+             PREFIX : <https://id.parliament.uk/schema/>
+             PREFIX id: <https://id.parliament.uk/>
+              SELECT ?Paper ?Papername ?workPackage ?businessItem ?step ?combinedDate ?procedure ?procedureName ?calculationStyle ?calculationStyleName where {
 
-        ?Paper a :WorkPackagedThing .  
-            ?Paper :name ?Papername ;
-            :workPackagedThingHasWorkPackage ?workPackage.
-         optional {?workPackage :workPackageHasBusinessItem ?businessItem .
-           ?businessItem :businessItemHasProcedureStep ?step;
-                  :businessItemDate ?businessItemDate.
-           filter (?step in (id:isWn7s3K, id:cspzmb6w, id:ITNO9JWr, id:otscOTzB))}
-           ?workPackage :workPackageHasProcedure ?procedure.
-         ?procedure :name ?procedureName.
-        optional { ?workPackage :workPackageHasCalculationStyle ?calculationStyle. 
-           ?calculationStyle :name ?calculationStyleName. }
-  MINUS {  ?workPackage   :workPackageHasBusinessItem ?bi2.
-  ?bi2 :businessItemHasProcedureStep ?stepId2.
-    ?stepId2 :procedureStepHasProcedureStepCollectionMembership/:procedureStepCollectionMembershipHasProcedureStepCollection id:TRohjSuI}
-       } Order by desc (?businessItemDate)
-
-       LIMIT #{limit} OFFSET #{offset}
+  
+              ?Paper a :WorkPackagedThing .  
+                  ?Paper :name ?Papername ;
+                  :workPackagedThingHasWorkPackage ?workPackage.
+              optional {   ?workPackage :workPackageHasBusinessItem ?businessItem .
+                ?businessItem :businessItemHasProcedureStep ?step;
+                       :businessItemDate ?businessItemDate.
+         filter (?step in (id:isWn7s3K, id:cspzmb6w, id:ITNO9JWr, id:otscOTzB))}
+         optional {    ?workPackage :workPackageHasBusinessItem ?businessItem2 .
+                ?businessItem2 :businessItemHasProcedureStep ?step2;
+                               :businessItemDate ?businessItemDate2.
+         filter (?step2 in (id:AmYrFxwO))}
+                 ?workPackage :workPackageHasProcedure ?procedure.
+               ?procedure :name ?procedureName.
+              optional { ?workPackage :workPackageHasCalculationStyle ?calculationStyle. 
+                 ?calculationStyle :name ?calculationStyleName. }
+        MINUS {  ?workPackage   :workPackageHasBusinessItem ?bi2.
+       ?bi2 :businessItemHasProcedureStep ?stepId2.
+          ?stepId2 :procedureStepHasProcedureStepCollectionMembership/:procedureStepCollectionMembershipHasProcedureStepCollection id:TRohjSuI}
+     BIND(COALESCE(?businessItemDate, ?businessItemDate2) AS ?combinedDate)
+     } Order by desc(?combinedDate) ?Papername
+        limit #{limit} offset #{offset}
     "
   end
 end
