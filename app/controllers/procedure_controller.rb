9bf07c3274148c792d4e1af2bd63ebb63ab6368d
fantasticlife <michael@smethur.st>
2025-07-23 11:22:45 +0100

    main procedure page now lists current work packages

--- app/controllers/procedure_controller.rb
+++ app/controllers/procedure_controller.rb
@@ -7,6 +7,10 @@ class ProcedureController < ApplicationController
   include Sparql::Queries::ProceduresMain
   include Sparql::Get::Procedure
   include Sparql::Queries::Procedure
+  include Sparql::Get::ProcedureWorkPackageCountCurrent
+  include Sparql::Queries::ProcedureWorkPackageCountCurrent
+  include Sparql::Get::ProcedureWorkPackagesCurrent
+  include Sparql::Queries::ProcedureWorkPackagesCurrent
   include Sparql::Get::Response
 
   def index
@@ -35,12 +39,33 @@ class ProcedureController < ApplicationController
   def show
     procedure_id = params[:procedure]
     @procedure = get_procedure( procedure_id )
+  
+    # We get the page number passed as a parameter.
+    page = params['page']
+    @page = ( page || "1" ).to_i
+    
+    # We get the number of results per page passed as a parameter.
+    results_per_page = params['results-per-page']
+    @results_per_page = ( results_per_page || $DEFAULT_RESULTS_PER_PAGE ).to_i
+    
+    # We get the count of current work packages in the procedure.
+    @result_count = get_procedure_work_package_current_count( procedure_id )
+    
+    # If this is not the first page and the number of the first work package on this page exceeds the total number of work packages ...
+    if @page != 1 && ( ( ( @page - 1 ) * @results_per_page ) + 1 > @result_count )
+      raise ActionController::RoutingError.new("Not Found")
+    end
+    
+    # We get the set of work packages for this procedure on this page with this many results per page.
+    @work_packages = get_procedure_work_packages_current( procedure_id, @page, @results_per_page )
     
     @page_title = @procedure.label
     @description = @procedure.description
     @crumb << { label: 'Procedures', url: procedure_list_url }
     @crumb << { label: @procedure.label, url: nil }
     @section = 'procedures'
-    @subsection = 'overview'
+    @subsection = 'work-packages-current'
+    
+    render :template => 'procedure_work_package/current'
   end
 end
