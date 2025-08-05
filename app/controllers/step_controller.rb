4fca937426066b965f02f4da53fcc71d4831c5f4
fantasticlife <michael@smethur.st>
2025-08-05 13:11:47 +0100

    added setting of rss and csv urls in step show action

--- app/controllers/step_controller.rb
+++ app/controllers/step_controller.rb
@@ -25,6 +25,8 @@ class StepController < ApplicationController
     
     @page_title = @step.display_label
     @description = "#{@step.display_label}."
+    @rss_url = step_business_item_list_url( :format => 'rss' )
+    @csv_url = step_business_item_list_url( :format => 'csv' )
     @crumb << { label: 'Steps', url: step_type_list_url }
     @crumb << { label: @step.step_type_label, url: step_type_show_url( :step_type => @step.step_type_id ) }
     @crumb << { label: @step.label, url: nil }
