d7edb46cd6bc12272037509d4236d58baff8621c
fantasticlife <michael@smethur.st>
2025-08-07 23:30:58 +0100

    new getter and query for all business items for a step - for csv and rss

---
+++ lib/sparql/get/step_business_items_all.rb
@@ -0,0 +1,33 @@
+module Sparql::Get::StepBusinessItemsAll
+  
+  def get_step_business_items_all( step )
+  
+    # We get the step business items query.
+    request_body = step_business_items_all_query( step )
+    
+    # We get the SPARQL response as a CSV.
+    csv = get_sparql_response_as_csv( request_body )
+    
+    # We construct an array to hold the business items.
+    business_items = []
+  
+    # For each row in the CSV ...
+    csv.each do |row|
+  
+      # ... we create a new business item object ...
+      business_item = BusinessItem.new
+      business_item.identifier = row['businessItem']
+      business_item.date = row['businessItemDate'].to_date if row['businessItemDate']
+      business_item.work_package_identifier = row['workPackage']
+      business_item.work_packageable_thing_label = row['workpackagedThingName']
+      business_item.procedure_identifier = row['procedure']
+      business_item.procedure_label = row['procedureName']
+      
+      # ... and add it to the array of business items.
+      business_items << business_item
+    end
+  
+    # We return the array of business items.
+    business_items
+  end
+end
---
+++ lib/sparql/queries/step_business_items_all.rb
@@ -0,0 +1,26 @@
+module Sparql::Queries::StepBusinessItemsAll
+
+  # A SPARQL query to get all business items for a step.
+  def step_business_items_all_query( step_id )
+    "
+      PREFIX id: <https://id.parliament.uk/>
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      SELECT ?businessItem ?businessItemDate ?workPackage ?procedure ?procedureName ?workPackagedThing ?workpackagedThingName WHERE {
+
+        ?Step a :ProcedureStep;
+                  :name ?stepName;
+                  :procedureStepHasBusinessItem ?businessItem.
+          filter (?Step IN (id:#{step_id}))
+        optional{ ?businessItem :businessItemDate ?businessItemDate.}
+        optional {?businessItem :businessItemHasBusinessItemWebLink ?businessItemLink }
+        ?businessItem :businessItemHasWorkPackage ?workPackage.
+        ?workPackage :workPackageHasProcedure ?procedure;
+                     :workPackageHasWorkPackagedThing ?workPackagedThing.
+        ?procedure :name ?procedureName.
+        ?workPackagedThing :name ?workpackagedThingName. 
+      } order by DESC(?businessItemDate)  ?workpackagedThingName
+    "
+  end
+end
