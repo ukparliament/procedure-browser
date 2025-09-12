6fb9f157b150dbd515d93ba8f153fe115771a040
James Jefferies <james@shedcode.co.uk>
2025-09-12 18:12:32 +0100

    Think I prefer this way of doing it

--- app/helpers/enabling_legislation_a_to_z_helper.rb
+++ app/helpers/enabling_legislation_a_to_z_helper.rb
@@ -2,14 +2,20 @@ module EnablingLegislationAToZHelper
 
   def a_to_z_letter_link(letter, selected_letter)
     letter_downcase = letter.letter.downcase
-    css_class = letter_downcase == selected_letter ? 'selected-letter' : ''
 
-    link_to(letter.letter, enabling_legislation_atoz_show_url(letter: letter_downcase), class: css_class)
+    if letter_downcase == selected_letter
+      link_to(letter.letter, enabling_legislation_atoz_show_url(letter: letter_downcase), class: 'selected-letter')
+    else
+      link_to(letter.letter, enabling_legislation_atoz_show_url(letter: letter_downcase))
+    end
   end
 
   def all_letter_link
-    css_class = on_all_page? ? 'selected-letter' : ''
-    link_to('All', enabling_legislation_list_url, class: css_class)
+    if on_all_page?
+      link_to('All', enabling_legislation_list_url, class: 'selected-letter')
+    else
+      link_to('All', enabling_legislation_list_url)
+    end
   end
 
   def on_all_page?
