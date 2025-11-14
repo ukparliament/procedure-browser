5c078efd2099202769b84ccbf14546cd3f10e903
fantasticlife <michael@smethur.st>
2025-11-14 14:11:04 +0000

    added homepage description

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
 
   def index
+    @description = 'Procedure Browser makes available data for instruments subject to parliamentary procedure, compiled by the House of Commons Library.'
   end
 end
