62ff14e0129b3a567c8fa48bb165a56eb9b5b956
fantasticlife <michael@smethur.st>
2025-12-16 20:06:02 +0000

    stub query and getter for an item of legislation by its legislation.gov.uk uri

---
+++ lib/sparql/get/enabling_legislation_by_uri.rb
@@ -0,0 +1,28 @@
+module Sparql::Get::EnablingLegislationByUri
+
+  # A method to get an item of enabling legislation by its legislation.gov.uk URI.
+  def get_enabling_legislation_by_uri( enabling_legislation_uri )
+  
+    # We get the enabling legislation by uri query.
+    request_body = enabling_legislation_by_uri_query( enabling_legislation_uri )
+  
+    # We get the SPARQL response as a CSV.
+    #csv = get_sparql_response_as_csv( request_body )
+    
+    # We take the one and only row from the CSV ...
+    #csv.take( 1 ).each do |row|
+    
+      # ... and create a new enabling legislation object.
+      #enabling_legislation = EnablingLegislation.new
+      #enabling_legislation.identifier = row['Act']
+      #enabling_legislation.label = row['Name']
+      #enabling_legislation.date = row['Date'].to_date if row['Date']
+      #enabling_legislation.year = row['Year']
+      #enabling_legislation.act_number = row['Number']
+      #enabling_legislation.uri = row['URL']
+      
+      # We return the enabling legislation object.
+      #return enabling_legislation
+    #end
+  end
+end
---
+++ lib/sparql/queries/enabling_legislation_by_uri.rb
@@ -0,0 +1,9 @@
+module Sparql::Queries::EnablingLegislationByUri
+
+  # A SPARQL query to get an item of enabling legislation by its legislation.gov.uk URI.
+  def enabling_legislation_by_uri_query( enabling_legislation_uri )
+    "
+      
+    "
+  end
+end
