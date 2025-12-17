5db693b70610f87d61a0f191f9715c0bb35a9306
fantasticlife <michael@smethur.st>
2025-12-17 10:34:09 +0000

    new query to get an item of legislation by its legislation.gov.uk uri

--- lib/sparql/queries/enabling_legislation_by_uri.rb
+++ lib/sparql/queries/enabling_legislation_by_uri.rb
@@ -3,7 +3,19 @@ module Sparql::Queries::EnablingLegislationByUri
   # A SPARQL query to get an item of enabling legislation by its legislation.gov.uk URI.
   def enabling_legislation_by_uri_query( enabling_legislation_uri )
     "
-      
+      PREFIX : <https://id.parliament.uk/schema/>
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX id: <https://id.parliament.uk/>
+      SELECT * WHERE {
+          ?Act a :ActOfParliament.
+          ?Act :actOfParliamentName ?Name.
+          OPTIONAL {?Act :actOfParliamentNumber ?Number}
+          OPTIONAL {?Act :actOfParliamentYear ?Year}
+          OPTIONAL{  ?Act :actOfParliamentRoyalAssentDate ?Date}
+          OPTIONAL {?Act :actOfParliamentUrl ?URL}
+          FILTER (?URL='#{enabling_legislation_uri}'^^xsd:string)
+      }  
     "
   end
 end
