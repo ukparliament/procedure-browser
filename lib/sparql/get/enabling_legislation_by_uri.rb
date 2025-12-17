d5fb51069950ba60ccf5b3a6f30feb77d844b241
fantasticlife <michael@smethur.st>
2025-12-17 10:33:30 +0000

    uncommented getter for an item of enabling legislation by its legislation.gov.uk uri

--- lib/sparql/get/enabling_legislation_by_uri.rb
+++ lib/sparql/get/enabling_legislation_by_uri.rb
@@ -7,22 +7,22 @@ module Sparql::Get::EnablingLegislationByUri
     request_body = enabling_legislation_by_uri_query( enabling_legislation_uri )
   
     # We get the SPARQL response as a CSV.
-    #csv = get_sparql_response_as_csv( request_body )
+    csv = get_sparql_response_as_csv( request_body )
     
     # We take the one and only row from the CSV ...
-    #csv.take( 1 ).each do |row|
+    csv.take( 1 ).each do |row|
     
       # ... and create a new enabling legislation object.
-      #enabling_legislation = EnablingLegislation.new
-      #enabling_legislation.identifier = row['Act']
-      #enabling_legislation.label = row['Name']
-      #enabling_legislation.date = row['Date'].to_date if row['Date']
-      #enabling_legislation.year = row['Year']
-      #enabling_legislation.act_number = row['Number']
-      #enabling_legislation.uri = row['URL']
+      enabling_legislation = EnablingLegislation.new
+      enabling_legislation.identifier = row['Act']
+      enabling_legislation.label = row['Name']
+      enabling_legislation.date = row['Date'].to_date if row['Date']
+      enabling_legislation.year = row['Year']
+      enabling_legislation.act_number = row['Number']
+      enabling_legislation.uri = row['URL']
       
       # We return the enabling legislation object.
-      #return enabling_legislation
-    #end
+      return enabling_legislation
+    end
   end
 end
