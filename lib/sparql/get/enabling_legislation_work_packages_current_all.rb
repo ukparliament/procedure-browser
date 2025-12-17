0cf6152082a78be503743c955216ecd3e3875b2d
fantasticlife <michael@smethur.st>
2025-12-17 20:21:34 +0000

    new query and getter for all current work packages for an item of enabling legislation. added because html is paginated

---
+++ lib/sparql/get/enabling_legislation_work_packages_current_all.rb
@@ -0,0 +1,36 @@
+module Sparql::Get::EnablingLegislationWorkPackagesCurrentAll
+  
+  def get_enabling_legislation_work_packages_current_all( enabling_thing_id )
+  
+    
+    # We get the enabling legislation current work packages query.
+    request_body = enabling_legislation_work_packages_current_all_query( enabling_thing_id )
+    
+    # We get the SPARQL response as a CSV.
+    csv = get_sparql_response_as_csv( request_body )
+    
+    # We construct an array to hold the work packages.
+    work_packages = []
+  
+    # For each row in the CSV ...
+    csv.each do |row|
+  
+      # ... we create a new work package object ...
+      work_package = WorkPackage.new
+      work_package.identifier = row['workPackage']
+      work_package.work_packageable_thing_identifer = row['workPackagedThing']
+      work_package.work_packageable_thing_label = row['workPackagedThingName']
+      work_package.made_available_on = row['combinedDate'].to_date if row['combinedDate']
+      work_package.procedure_identifier = row['procedure']
+      work_package.procedure_label = row['procedureName']
+      work_package.calculation_style_identifier = row['calculationStyle']
+      work_package.calculation_style_label = row['calculationStyleName']
+      
+      # ... and add it to the array of work packages.
+      work_packages << work_package
+    end
+  
+    # We return the array of work packages.
+    work_packages
+  end
+end
