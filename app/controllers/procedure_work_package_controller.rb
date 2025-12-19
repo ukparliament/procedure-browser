a19a1b657d0586b51d7cf40a245cc353fc5d0a4e
fantasticlife <michael@smethur.st>
2025-12-19 13:07:37 +0000

    request for a list of work packages for a proceduee now responds to csv. added new query and getter because html is paginated but csv is not

--- app/controllers/procedure_work_package_controller.rb
+++ app/controllers/procedure_work_package_controller.rb
@@ -7,6 +7,8 @@ class ProcedureWorkPackageController < ApplicationController
   include Sparql::Queries::ProcedureWorkPackageCount
   include Sparql::Get::ProcedureWorkPackages
   include Sparql::Queries::ProcedureWorkPackages
+  include Sparql::Get::ProcedureWorkPackagesAll
+  include Sparql::Queries::ProcedureWorkPackagesAll
   include Sparql::Get::ProcedureWorkPackageCountCurrent
   include Sparql::Queries::ProcedureWorkPackageCountCurrent
   include Sparql::Get::ProcedureWorkPackagesCurrent
@@ -18,35 +20,48 @@ class ProcedureWorkPackageController < ApplicationController
     # We get the procedure.
     procedure_id = params[:procedure]
     @procedure = get_procedure( procedure_id )
-  
-    # We get the page number passed as a parameter.
-    page = params['page']
-    @page = ( page || "1" ).to_i
-    
-    # We get the number of results per page passed as a parameter.
-    results_per_page = params['results-per-page']
-    @results_per_page = ( results_per_page || $DEFAULT_RESULTS_PER_PAGE ).to_i
-    
-    # We get the count of all work packages in the procedure.
-    @result_count = get_procedure_work_package_count( procedure_id )
-    
-    # If this is not the first page and the number of the first work package on this page exceeds the total number of work packages ...
-    if @page != 1 && ( ( ( @page - 1 ) * @results_per_page ) + 1 > @result_count )
-      raise ActionController::RoutingError.new("Not Found")
-    end
-    
-    # We get the set of work packages for this procedure on this page with this many results per page.
-    @work_packages = get_procedure_work_packages( procedure_id, @page, @results_per_page )
     
     @page_title = "#{@procedure.label} - All work packages"
-    @multiline_page_title = "#{@procedure.label} <span class='subhead'>All work packages</span>".html_safe
-    @description = "All work packages subject to #{@procedure.label}."
-    @rss_url = procedure_work_package_list_url( :format => 'rss' )
-    @crumb << { label: 'Procedures', url: procedure_list_url }
-    @crumb << { label: @procedure.label, url: procedure_show_url }
-    @crumb << { label: 'Work packages', url: nil }
-    @section = 'procedures'
-    @subsection = 'work-packages'
+    
+    respond_to do |format|
+      format.csv {
+        # We get the set of work packages for this procedure.
+        @work_packages = get_procedure_work_packages_all( procedure_id )
+        response.headers['Content-Disposition'] = "attachment; filename=\"#{csv_title_from_page_title ( @page_title )}.csv\""
+        render :template => 'work_package/index'
+      }
+      format.any( :html, :rss ) {
+        # We get the page number passed as a parameter.
+        page = params['page']
+        @page = ( page || "1" ).to_i
+    
+        # We get the number of results per page passed as a parameter.
+        results_per_page = params['results-per-page']
+        @results_per_page = ( results_per_page || $DEFAULT_RESULTS_PER_PAGE ).to_i
+    
+        # We get the count of all work packages in the procedure.
+        @result_count = get_procedure_work_package_count( procedure_id )
+    
+        # If this is not the first page and the number of the first work package on this page exceeds the total number of work packages ...
+        if @page != 1 && ( ( ( @page - 1 ) * @results_per_page ) + 1 > @result_count )
+          raise ActionController::RoutingError.new("Not Found")
+        end
+    
+        # We get the set of work packages for this procedure on this page with this many results per page.
+        @work_packages = get_procedure_work_packages( procedure_id, @page, @results_per_page )
+    
+        
+        @multiline_page_title = "#{@procedure.label} <span class='subhead'>All work packages</span>".html_safe
+        @description = "All work packages subject to #{@procedure.label}."
+        @rss_url = procedure_work_package_list_url( :format => 'rss' )
+        @csv_url = procedure_work_package_list_url( :format => 'csv' )
+        @crumb << { label: 'Procedures', url: procedure_list_url }
+        @crumb << { label: @procedure.label, url: procedure_show_url }
+        @crumb << { label: 'Work packages', url: nil }
+        @section = 'procedures'
+        @subsection = 'work-packages'
+      }
+    end
   end
   
   def current
