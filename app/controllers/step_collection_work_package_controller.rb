b80567bc9d82a3954645813f0df4fab5c1ba7dea
fantasticlife <michael@smethur.st>
2025-12-20 20:54:09 +0000

    request for current work packages for a step collection now responds to request for csv, unpaginated

--- app/controllers/step_collection_work_package_controller.rb
+++ app/controllers/step_collection_work_package_controller.rb
@@ -13,6 +13,8 @@ class StepCollectionWorkPackageController < ApplicationController
   include Sparql::Queries::StepCollectionWorkPackageCountCurrent
   include Sparql::Get::StepCollectionWorkPackagesCurrent
   include Sparql::Queries::StepCollectionWorkPackagesCurrent
+  include Sparql::Get::StepCollectionWorkPackagesCurrentAll
+  include Sparql::Queries::StepCollectionWorkPackagesCurrentAll
   include Sparql::Get::Response
 
   def index
@@ -72,34 +74,49 @@ class StepCollectionWorkPackageController < ApplicationController
     step_collection_id = params[:step_collection]
     @step_collection = get_step_collection( step_collection_id )
   
-    # We get the page number passed as a parameter.
-    page = params['page']
-    @page = ( page || "1" ).to_i
-    
-    # We get the number of results per page passed as a parameter.
-    results_per_page = params['results-per-page']
-    @results_per_page = ( results_per_page || $DEFAULT_RESULTS_PER_PAGE ).to_i
-    
-    # We get the count of current work packages having actualised steps from the step collection.
-    @result_count = get_step_collection_work_package_count_current( step_collection_id )
-    
-    # If this is not the first page and the number of the first work package on this page exceeds the total number of work packages ...
-    if @page != 1 && ( ( ( @page - 1 ) * @results_per_page ) + 1 > @result_count )
-      raise ActionController::RoutingError.new("Not Found")
-    end
-    
-    # We get the set of current work packages for this step collection on this page with this many results per page.
-    @work_packages = get_step_collection_work_packages_current( step_collection_id, @page, @results_per_page )
-    
     @page_title = "#{@step_collection.label} - Work packages before Parliament"
-    @multiline_page_title = "#{@step_collection.label} <span class='subhead'>Work packages before Parliament</span>".html_safe
-    @description = "Work packages before Parliament with business items actualising one or more steps in the '#{@step_collection.label}' step collection."
-    @rss_url = step_collection_work_package_current_list_url( :format => 'rss' )
-    @crumb << { label: 'Step collections', url: step_collection_list_url }
-    @crumb << { label: @step_collection.label, url: step_collection_show_url }
-    @crumb << { label: 'Work packages', url: step_collection_work_package_list_url }
-    @crumb << { label: 'Before Parliament', url: nil }
-    @section = 'step-collections'
-    @subsection = 'work-packages-current'
+    
+    respond_to do |format|
+      format.csv {
+      
+        # We get the set of current work packages for this step collection.
+        @work_packages = get_step_collection_work_packages_current_all( step_collection_id )
+      
+        response.headers['Content-Disposition'] = "attachment; filename=\"#{csv_title_from_page_title ( @page_title )}.csv\""
+        render :template => 'work_package/index'
+      }
+      format.any( :html, :rss ) {
+        
+        # We get the page number passed as a parameter.
+        page = params['page']
+        @page = ( page || "1" ).to_i
+    
+        # We get the number of results per page passed as a parameter.
+        results_per_page = params['results-per-page']
+        @results_per_page = ( results_per_page || $DEFAULT_RESULTS_PER_PAGE ).to_i
+    
+        # We get the count of current work packages having actualised steps from the step collection.
+        @result_count = get_step_collection_work_package_count_current( step_collection_id )
+    
+        # If this is not the first page and the number of the first work package on this page exceeds the total number of work packages ...
+        if @page != 1 && ( ( ( @page - 1 ) * @results_per_page ) + 1 > @result_count )
+          raise ActionController::RoutingError.new("Not Found")
+        end
+    
+        # We get the set of current work packages for this step collection on this page with this many results per page.
+        @work_packages = get_step_collection_work_packages_current( step_collection_id, @page, @results_per_page )
+    
+        @multiline_page_title = "#{@step_collection.label} <span class='subhead'>Work packages before Parliament</span>".html_safe
+        @description = "Work packages before Parliament with business items actualising one or more steps in the '#{@step_collection.label}' step collection."
+        @rss_url = step_collection_work_package_current_list_url( :format => 'rss' )
+        @csv_url = step_collection_work_package_current_list_url( :format => 'csv' )
+        @crumb << { label: 'Step collections', url: step_collection_list_url }
+        @crumb << { label: @step_collection.label, url: step_collection_show_url }
+        @crumb << { label: 'Work packages', url: step_collection_work_package_list_url }
+        @crumb << { label: 'Before Parliament', url: nil }
+        @section = 'step-collections'
+        @subsection = 'work-packages-current'
+      }
+    end
   end
 end
