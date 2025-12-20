c2dfe6dd1e148ca5dd623522d61f6052a9a6ecc4
fantasticlife <michael@smethur.st>
2025-12-20 14:33:18 +0000

    request for work packages for a calculation style now responds to csv, unpaginated

--- app/controllers/calculation_style_work_package_controller.rb
+++ app/controllers/calculation_style_work_package_controller.rb
@@ -9,6 +9,8 @@ class CalculationStyleWorkPackageController < ApplicationController
   include Sparql::Queries::CalculationStyleWorkPackageCount
   include Sparql::Get::CalculationStyleWorkPackages
   include Sparql::Queries::CalculationStyleWorkPackages
+  include Sparql::Get::CalculationStyleWorkPackagesAll
+  include Sparql::Queries::CalculationStyleWorkPackagesAll
   include Sparql::Get::CalculationStyleWorkPackageCountCurrent
   include Sparql::Queries::CalculationStyleWorkPackageCountCurrent
   include Sparql::Get::CalculationStyleWorkPackagesCurrent
@@ -21,35 +23,50 @@ class CalculationStyleWorkPackageController < ApplicationController
     
     # We get the calculation style.
     @calculation_style = get_calculation_style( calculation_style_id )
-  
-    # We get the page number passed as a parameter.
-    page = params['page']
-    @page = ( page || "1" ).to_i
-    
-    # We get the number of results per page passed as a parameter.
-    results_per_page = params['results-per-page']
-    @results_per_page = ( results_per_page || $DEFAULT_RESULTS_PER_PAGE ).to_i
-    
-    # We get the count of all work packages with this calculation style.
-    @result_count = get_calculation_style_work_package_count( @calculation_style.id )
-    
-    # If this is not the first page and the number of the first work package on this page exceeds the total number of work packages ...
-    if @page != 1 && ( ( ( @page - 1 ) * @results_per_page ) + 1 > @result_count )
-      raise ActionController::RoutingError.new("Not Found")
-    end
-    
-    # We get the set of work packages with this calculation style on this page with this many results per page.
-    @calculation_style_work_packages = get_calculation_style_work_packages( @calculation_style.id, @page, @results_per_page )
     
     @page_title = "Work packages with the '#{@calculation_style.label}' calculation style"
-    @multiline_page_title = "'#{@calculation_style.label}' calculation style <span class='subhead'>All work packages</span>".html_safe
-    @description = "Work packages using the '#{@calculation_style.label}' calculation style."
-    @rss_url = calculation_style_work_package_list_url( :format => 'rss' )
-    @crumb << { label: 'Calculation styles', url: calculation_style_list_url }
-    @crumb << { label: @calculation_style.label, url: calculation_style_show_url }
-    @crumb << { label: 'Work packages', url: nil }
-    @section = 'calculation-styles'
-    @subsection = 'work-packages'
+    
+    respond_to do |format|
+      format.csv {
+      
+        # We get the set of work packages with this calculation style.
+        @work_packages = get_calculation_style_work_packages_all( @calculation_style.id )
+        
+        response.headers['Content-Disposition'] = "attachment; filename=\"#{csv_title_from_page_title ( @page_title )}.csv\""
+        render :template => 'work_package/index'
+      }
+      format.any( :html, :rss ) {
+      
+        # We get the page number passed as a parameter.
+        page = params['page']
+        @page = ( page || "1" ).to_i
+    
+        # We get the number of results per page passed as a parameter.
+        results_per_page = params['results-per-page']
+        @results_per_page = ( results_per_page || $DEFAULT_RESULTS_PER_PAGE ).to_i
+    
+        # We get the count of all work packages with this calculation style.
+        @result_count = get_calculation_style_work_package_count( @calculation_style.id )
+    
+        # If this is not the first page and the number of the first work package on this page exceeds the total number of work packages ...
+        if @page != 1 && ( ( ( @page - 1 ) * @results_per_page ) + 1 > @result_count )
+          raise ActionController::RoutingError.new("Not Found")
+        end
+    
+        # We get the set of work packages with this calculation style on this page with this many results per page.
+        @work_packages = get_calculation_style_work_packages( @calculation_style.id, @page, @results_per_page )
+    
+        @multiline_page_title = "'#{@calculation_style.label}' calculation style <span class='subhead'>All work packages</span>".html_safe
+        @description = "Work packages using the '#{@calculation_style.label}' calculation style."
+        @rss_url = calculation_style_work_package_list_url( :format => 'rss' )
+        @csv_url = calculation_style_work_package_list_url( :format => 'csv' )
+        @crumb << { label: 'Calculation styles', url: calculation_style_list_url }
+        @crumb << { label: @calculation_style.label, url: calculation_style_show_url }
+        @crumb << { label: 'Work packages', url: nil }
+        @section = 'calculation-styles'
+        @subsection = 'work-packages'
+      }
+    end
   end
 
   def current
