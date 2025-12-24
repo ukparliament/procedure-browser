c9968a0ca81c6a310b4acc4c352da67d4ff39252
fantasticlife <michael@smethur.st>
2025-12-24 11:25:53 +0000

    tried another form of host setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   
   routes.default_url_options[:host] ||= "api.parliament.uk"
+  Rails.application.routes.default_url_options[:host] = 'api.parliament.uk'
   
   
   config.asset_host = ENV.fetch("ASSET_HOST", "https://procedure-browser-159b715822a4.herokuapp.com/")
