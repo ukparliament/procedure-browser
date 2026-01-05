281c257551c822979711f54771576312000202b6
fantasticlife <michael@smethur.st>
2026-01-05 16:19:23 +0000

    added 404s for missing identifiers for legislatures

--- lib/sparql/get/legislature.rb
+++ lib/sparql/get/legislature.rb
@@ -9,16 +9,30 @@ module Sparql::Get::Legislature
     # We get the SPARQL response as a CSV.
     csv = get_sparql_response_as_csv( request_body )
     
-    # We take the one and only row from the CSV ...
-    csv.take( 1 ).each do |row|
-    
-      # ... and create a new legislature object.
-      legislature = Legislature.new
-      legislature.identifier = row['Legislature']
-      legislature.label = row['LegislatureName']
+    # If the legislature doesn't exist, the csv will be an empty array.
+    # If the array is an empty array ...
+    if csv.empty?
       
-      # We return the legislature object.
-      return legislature
+      # ... we render a 404 ...
+      render_404
+      
+      # ... and return nil.
+      return nil
+      
+    # Otherwise, if the CSV is not an empty array ...
+    else
+    
+      # ... we take the one and only row from the CSV ...
+      csv.take( 1 ).each do |row|
+    
+        # ... and create a new legislature object.
+        legislature = Legislature.new
+        legislature.identifier = row['Legislature']
+        legislature.label = row['LegislatureName']
+      
+        # We return the legislature object.
+        return legislature
+      end
     end
   end
 end
