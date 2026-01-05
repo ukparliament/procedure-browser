e12a05d1f8452eb37439ca3cbe2adee4ec3fc6f9
fantasticlife <michael@smethur.st>
2026-01-05 11:52:21 +0000

    added 404s for missing identifiers for enabling legislation

--- app/controllers/enabling_legislation_work_packageable_thing_controller.rb
+++ app/controllers/enabling_legislation_work_packageable_thing_controller.rb
@@ -14,34 +14,38 @@ class EnablingLegislationWorkPackageableThingController < ApplicationController
     # We get the enabling legislation.
     enabling_legislation = params[:enabling_legislation]
     @enabling_legislation = get_enabling_legislation( enabling_legislation )
+    
+    # If we find an item of enabling legislation with this identifier ...
+    if @enabling_legislation
   
-    # We get the page number passed as a parameter.
-    page = params['page']
-    @page = ( page || "1" ).to_i
+      # ... we get the page number passed as a parameter.
+      page = params['page']
+      @page = ( page || "1" ).to_i
     
-    # We get the number of results per page passed as a parameter.
-    results_per_page = params['results-per-page']
-    @results_per_page = ( results_per_page || $DEFAULT_RESULTS_PER_PAGE ).to_i
+      # We get the number of results per page passed as a parameter.
+      results_per_page = params['results-per-page']
+      @results_per_page = ( results_per_page || $DEFAULT_RESULTS_PER_PAGE ).to_i
     
-    # We get the count of all work packageable things for the item of enabling legislation.
-    @result_count = get_enabling_legislation_work_packageable_thing_count( enabling_legislation )
+      # We get the count of all work packageable things for the item of enabling legislation.
+      @result_count = get_enabling_legislation_work_packageable_thing_count( enabling_legislation )
     
-    # If this is not the first page and the number of the first work package on this page exceeds the total number of work packages ...
-    if @page != 1 && ( ( ( @page - 1 ) * @results_per_page ) + 1 > @result_count )
-      raise ActionController::RoutingError.new("Not Found")
+      # If this is not the first page and the number of the first work package on this page exceeds the total number of work packages ...
+      if @page != 1 && ( ( ( @page - 1 ) * @results_per_page ) + 1 > @result_count )
+        raise ActionController::RoutingError.new("Not Found")
+      end
+    
+      # We get the set of work packageable things for an item of legislation on this page with this many results per page.
+      @enabling_legislation_work_packageable_things = get_enabling_legislation_work_packageable_things( enabling_legislation, @page, @results_per_page ) 
+    
+      @page_title = "#{@enabling_legislation.label} - work packageable things"
+      @multiline_page_title = "#{@enabling_legislation.label} <span class='subhead'>Work packageable things</span>".html_safe
+      @description = "Work packageable things enabled by #{@enabling_legislation.label}."
+      @rss_url = enabling_legislation_work_packageable_thing_list_url( :format => 'rss' )
+      @crumb << { label: 'Enabling legislation', url: enabling_legislation_list_url }
+      @crumb << { label: @enabling_legislation.label, url: enabling_legislation_show_url }
+      @crumb << { label: 'Work packageable things', url: nil }
+      @section = 'enabling-legislation'
+      @subsection = 'work-packageable-things'
     end
-    
-    # We get the set of work packageable things for an item of legislation on this page with this many results per page.
-    @enabling_legislation_work_packageable_things = get_enabling_legislation_work_packageable_things( enabling_legislation, @page, @results_per_page ) 
-    
-    @page_title = "#{@enabling_legislation.label} - work packageable things"
-    @multiline_page_title = "#{@enabling_legislation.label} <span class='subhead'>Work packageable things</span>".html_safe
-    @description = "Work packageable things enabled by #{@enabling_legislation.label}."
-    @rss_url = enabling_legislation_work_packageable_thing_list_url( :format => 'rss' )
-    @crumb << { label: 'Enabling legislation', url: enabling_legislation_list_url }
-    @crumb << { label: @enabling_legislation.label, url: enabling_legislation_show_url }
-    @crumb << { label: 'Work packageable things', url: nil }
-    @section = 'enabling-legislation'
-    @subsection = 'work-packageable-things'
   end
 end
