ab0eefbd8acacf0edcaa9074e3071aed33144d87
James Jefferies <james@shedcode.co.uk>
2026-01-17 17:16:48 +0000

    Making a start with actual search

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,15 @@
+class SearchesController < ApplicationController
+  #include PagyConcern
+
+  def index
+    if params[:search]
+      @search_term = params[:search]
+      # basic_search = PgSearch.multisearch(@search_term).includes(:searchable)
+      # @member_results = basic_search.where(searchable_type: 'Member')
+      # @non_member_results = basic_search.where.not(searchable_type: %w[AlternativeName NameChange Member])
+      # @name_change_results = basic_search.where(searchable_type: %w[AlternativeName NameChange])
+
+      # @total = @member_results.count + @non_member_results.count + @name_change_results.count
+    end
+  end
+end
