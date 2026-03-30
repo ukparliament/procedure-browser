f8b9b9e99f59fa840edaebdd7e690f7c41739144
fantasticlife <michael@smethur.st>
2026-03-30 10:33:49 +0100

    all legislatures query now an array of title, link and query

--- lib/sparql/queries/legislatures.rb
+++ lib/sparql/queries/legislatures.rb
@@ -2,17 +2,27 @@ module Sparql::Queries::Legislatures
 
   # A SPARQL query to get all legislatures.
   def legislatures_query
-    "
-      PREFIX : <https://id.parliament.uk/schema/>
-      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-      PREFIX id: <https://id.parliament.uk/>
-      select * where { 
+    [ 
   
-  
-       ?Legislature a :Legislature ;
-                    :name ?LegislatureName. 
-     
-            } Order by ?LegislatureName
-    "
+      # The title of the SPARQL query.
+      'A full list of legislatures',
+    
+      # The link to the SPARQL query.
+      'https://api.parliament.uk/s/fdba8abc',
+    
+      # The SPARQL query.
+      "
+        PREFIX : <https://id.parliament.uk/schema/>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX id: <https://id.parliament.uk/>
+        SELECT *
+        # An example of a SPARQL comment.
+        WHERE {
+          ?Legislature a :Legislature ;
+          :name ?LegislatureName.
+        }
+        ORDER BY ?LegislatureName
+      "
+    ]
   end
 end
