7a03a7990858da8bbb8eb6f81d1160daec8d3ee3
fantasticlife <michael@smethur.st>
2026-04-24 13:39:26 +0100

    added new query for procedure steps with counts, commented out until triplestire performance improves

--- lib/sparql/queries/procedure_step_type_steps.rb
+++ lib/sparql/queries/procedure_step_type_steps.rb
@@ -25,5 +25,33 @@ module Sparql::Queries::ProcedureStepTypeSteps
           filter (?stepType in (id:#{step_type_id}))
        } order by ?stepType ?stepName
     "
+    
+    #"
+    #  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    #        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    #        PREFIX : <https://id.parliament.uk/schema/>
+    #        PREFIX id: <https://id.parliament.uk/>
+    #        SELECT distinct ?Procedure ?ProcedureName  ?stepType ?stepTypeLabel ?step ?stepName ?legislature ?legislatureName ?CommonsId ?LordsId  (COUNT(?bi) AS ?biCount) WHERE {
+    #          ?Procedure a :Procedure;
+    #                     :name ?ProcedureName.
+    #             filter (?Procedure in (id:#{procedure_id}))
+    #          optional {  ?Procedure :procedureHasProcedureRoute ?Route.
+    #            ?Route :procedureRouteIsFromProcedureStep|:procedureRouteIsToProcedureStep ?step.
+    #       OPTIONAL { ?step :procedureStepHasBusinessItem ?bi }
+    #            ?step :name ?stepName.
+    #          ?step :procedureStepHasProcedureStepType ?stepType.
+    #            ?stepType :name ?stepTypeLabel.}
+    #            Optional {?step :procedureStepInLegislature ?legislature.
+    #            ?legislature :name ?legislatureName.}
+    #            Optional {?step :procedureStepHasHouse ?CommonsId
+    #            filter (?CommonsId IN (id:1AFu55Hs))}
+    #            Optional {?step :procedureStepHasHouse ?LordsId.
+    #              filter (?LordsId IN (id:WkUWUBMx))}
+    #            filter (?stepType in (id:#{step_type_id}))
+    #         } 
+    #  Group by ?Procedure ?ProcedureName  ?stepType ?stepTypeLabel ?step ?stepName ?legislature ?legislatureName ?CommonsId ?LordsId
+    #  order by ?stepType ?stepName
+    #"
+    
   end
 end
