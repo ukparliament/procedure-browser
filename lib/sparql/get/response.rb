f774717815abff86bb16cee7eb2aab1eb05427dd
James Jefferies <james@shedcode.co.uk>
2026-05-05 22:06:49 +0100

    Put all of the response in there

--- lib/sparql/get/response.rb
+++ lib/sparql/get/response.rb
@@ -41,7 +41,7 @@ module Sparql::Get::Response
     CSV.parse(body, headers: true)
 
   rescue CSV::MalformedCSVError => e
-    Rails.logger.error "CSV body (first 500): #{body[0..500].inspect}"
+    Rails.logger.error "API returns body: #{body}"
     raise
   end
 
