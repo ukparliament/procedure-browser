188a571603160df5d62d12c59ad4103e483dbe24
fantasticlife <michael@smethur.st>
2026-05-07 19:10:27 +0100

    added csv view for secondary legislation work packages

--- app/controllers/work_package_secondary_legislation_controller.rb
+++ app/controllers/work_package_secondary_legislation_controller.rb
@@ -5,36 +5,50 @@ class WorkPackageSecondaryLegislationController < ApplicationController
   include Sparql::Queries::SecondaryLegislationWorkPackageCount
   include Sparql::Get::SecondaryLegislationWorkPackages
   include Sparql::Queries::SecondaryLegislationWorkPackages
+  include Sparql::Get::SecondaryLegislationWorkPackagesAll
+  include Sparql::Queries::SecondaryLegislationWorkPackagesAll
   include Sparql::Get::Response
 
   def index
   
-    # We get the page number passed as a parameter.
-    page = params['page']
-    @page = ( page || "1" ).to_i
-    
-    # We get the number of results per page passed as a parameter.
-    results_per_page = params['results-per-page']
-    @results_per_page = ( results_per_page || $DEFAULT_RESULTS_PER_PAGE ).to_i
-    
-    # We get the count of all work packages.
-    @result_count = get_secondary_legislation_work_package_count
-    
-    # If this is not the first page and the number of the first work package on this page exceeds the total number of work packages ...
-    if @page != 1 && ( ( ( @page - 1 ) * @results_per_page ) + 1 > @result_count )
-      raise ActionController::RoutingError.new("Not Found")
-    end
-    
-    # We get the set of work packages on this page with this many results per page.
-    @work_packages = get_secondary_legislation_work_packages( @page, @results_per_page )
-    
     @page_title = 'Secondary legislation work packages'
-    @multiline_page_title = "Work packages <span class='subhead'>Secondary legislation</span>".html_safe
-    @description = 'Work packages for secondary legislation.'
-    @rss_url = work_package_secondary_legislation_list_url( :format => 'rss' )
-    @crumb << { label: 'Work packages', url: work_package_list_url }
-    @crumb << { label: 'Secondary legislation', url: nil }
-    @section = 'work-packages'
-    @subsection = 'secondary-legislation'
+  
+    respond_to do |format|
+      format.csv {
+        @work_packages = get_secondary_legislation_work_packages_all
+        response.headers['Content-Disposition'] = "attachment; filename=\"#{csv_title_from_page_title ( @page_title )}.csv\""
+        render :template => 'work_package/index'
+      }
+      format.any( :html, :rss ) {
+  
+        # We get the page number passed as a parameter.
+        page = params['page']
+        @page = ( page || "1" ).to_i
+    
+        # We get the number of results per page passed as a parameter.
+        results_per_page = params['results-per-page']
+        @results_per_page = ( results_per_page || $DEFAULT_RESULTS_PER_PAGE ).to_i
+    
+        # We get the count of all work packages.
+        @result_count = get_secondary_legislation_work_package_count
+    
+        # If this is not the first page and the number of the first work package on this page exceeds the total number of work packages ...
+        if @page != 1 && ( ( ( @page - 1 ) * @results_per_page ) + 1 > @result_count )
+          raise ActionController::RoutingError.new("Not Found")
+        end
+    
+        # We get the set of work packages on this page with this many results per page.
+        @work_packages = get_secondary_legislation_work_packages( @page, @results_per_page )
+    
+        @multiline_page_title = "Work packages <span class='subhead'>Secondary legislation</span>".html_safe
+        @description = 'Work packages for secondary legislation.'
+        @csv_url = work_package_secondary_legislation_list_url( :format => 'csv' )
+        @rss_url = work_package_secondary_legislation_list_url( :format => 'rss' )
+        @crumb << { label: 'Work packages', url: work_package_list_url }
+        @crumb << { label: 'Secondary legislation', url: nil }
+        @section = 'work-packages'
+        @subsection = 'secondary-legislation'
+      }
+    end
   end
 end
---
+++ lib/sparql/get/secondary_legislation_work_packages_all.rb
@@ -0,0 +1,48 @@
+module Sparql::Get::SecondaryLegislationWorkPackagesAll
+
+  # A method to get an array of all secondary legislation work packages.
+  def get_secondary_legislation_work_packages_all
+
+    # We get the secondary legislation work packages query.
+    request_body = secondary_legislation_work_packages_all_query
+  
+    # We get the SPARQL response as a CSV.
+    csv = get_sparql_response_as_csv( request_body )
+  
+    # We construct an array to hold the work packages.
+    work_packages = []
+  
+    # For each row in the CSV ...
+    csv.each do |row|
+  
+      # ... we create a new work package object ...
+      work_package = WorkPackage.new
+      work_package.identifier = row['workPackage']
+      work_package.work_packageable_thing_identifer = row['Paper']
+      work_package.work_packageable_thing_label = row['Papername']
+      work_package.making_available_identifier = row['businessItem']
+      work_package.made_available_on = row['combinedDate'].to_date if row['combinedDate']
+      work_package.procedure_identifier = row['procedure']
+      work_package.procedure_label = row['procedureName']
+      work_package.calculation_style_identifier = row['calculationStyle']
+      work_package.calculation_style_label = row['calculationStyleName']
+      
+      if row['hasCommitteeConcernsFlag'] == 'true'
+        work_package.has_committee_concerns = true
+      else
+        work_package.has_committee_concerns = false
+      end
+      if row['hasMotionTabledFlag'] == 'true'
+        work_package.has_motion_tabled = true
+      else
+        work_package.has_motion_tabled = false
+      end
+      
+      # ... and add it to the array of work packages.
+      work_packages << work_package
+    end
+  
+    # We return the array of work packages.
+    work_packages
+  end
+end
