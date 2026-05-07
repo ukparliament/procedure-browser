1107273421741c64e88d0f72cad11fb2c9e01097
fantasticlife <michael@smethur.st>
2026-05-07 17:55:07 +0100

    added csv view for primary legislation work packages

--- app/controllers/work_package_primary_legislation_controller.rb
+++ app/controllers/work_package_primary_legislation_controller.rb
@@ -5,36 +5,50 @@ class WorkPackagePrimaryLegislationController < ApplicationController
   include Sparql::Queries::PrimaryLegislationWorkPackageCount
   include Sparql::Get::PrimaryLegislationWorkPackages
   include Sparql::Queries::PrimaryLegislationWorkPackages
+  include Sparql::Get::PrimaryLegislationWorkPackagesAll
+  include Sparql::Queries::PrimaryLegislationWorkPackagesAll
   include Sparql::Get::Response
 
   def index
   
-    # We get the page number passed as a parameter.
-    page = params['page']
-    @page = ( page || "1" ).to_i
-    
-    # We get the number of results per page passed as a parameter.
-    results_per_page = params['results-per-page']
-    @results_per_page = ( results_per_page || $DEFAULT_RESULTS_PER_PAGE ).to_i
-    
-    # We get the count of all work packages.
-    @result_count = get_primary_legislation_work_package_count
-    
-    # If this is not the first page and the number of the first work package on this page exceeds the total number of work packages ...
-    if @page != 1 && ( ( ( @page - 1 ) * @results_per_page ) + 1 > @result_count )
-      raise ActionController::RoutingError.new("Not Found")
-    end
-    
-    # We get the set of work packages on this page with this many results per page.
-    @work_packages = get_primary_legislation_work_packages( @page, @results_per_page )
-    
     @page_title = 'Primary legislation work packages'
-    @multiline_page_title = "Work packages <span class='subhead'>Primary legislation</span>".html_safe
-    @description = 'Work packages for primary legislation.'
-    @rss_url = work_package_primary_legislation_list_url( :format => 'rss' )
-    @crumb << { label: 'Work packages', url: work_package_list_url }
-    @crumb << { label: 'Primary legislation', url: nil }
-    @section = 'work-packages'
-    @subsection = 'primary-legislation'
+  
+    respond_to do |format|
+      format.csv {
+        @work_packages = get_primary_legislation_work_packages_all
+        response.headers['Content-Disposition'] = "attachment; filename=\"#{csv_title_from_page_title ( @page_title )}.csv\""
+        render :template => 'work_package/index'
+      }
+      format.any( :html, :rss ) {
+      
+        # We get the page number passed as a parameter.
+        page = params['page']
+        @page = ( page || "1" ).to_i
+    
+        # We get the number of results per page passed as a parameter.
+        results_per_page = params['results-per-page']
+        @results_per_page = ( results_per_page || $DEFAULT_RESULTS_PER_PAGE ).to_i
+    
+        # We get the count of all work packages.
+        @result_count = get_primary_legislation_work_package_count
+    
+        # If this is not the first page and the number of the first work package on this page exceeds the total number of work packages ...
+        if @page != 1 && ( ( ( @page - 1 ) * @results_per_page ) + 1 > @result_count )
+          raise ActionController::RoutingError.new("Not Found")
+        end
+    
+        # We get the set of work packages on this page with this many results per page.
+        @work_packages = get_primary_legislation_work_packages( @page, @results_per_page )
+    
+        @multiline_page_title = "Work packages <span class='subhead'>Primary legislation</span>".html_safe
+        @description = 'Work packages for primary legislation.'
+        @rss_url = work_package_primary_legislation_list_url( :format => 'rss' )
+        @csv_url = work_package_primary_legislation_list_url( :format => 'csv' )
+        @crumb << { label: 'Work packages', url: work_package_list_url }
+        @crumb << { label: 'Primary legislation', url: nil }
+        @section = 'work-packages'
+        @subsection = 'primary-legislation'
+      }
+    end
   end
 end
