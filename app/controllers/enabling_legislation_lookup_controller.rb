83a132f36adc7692ba63d9fe0f3fc228ff43479a
fantasticlife <michael@smethur.st>
2026-06-10 10:21:57 +0100

    expanded lookup controllers to present prettier 404s

--- app/controllers/enabling_legislation_lookup_controller.rb
+++ app/controllers/enabling_legislation_lookup_controller.rb
@@ -18,6 +18,20 @@ class EnablingLegislationLookupController < ApplicationController
     
       # ... we redirect to the list of instruments before Parliament enabled by that legislation.
       redirect_to enabling_legislation_show_path( legislation.id )
+      
+    # Otherwise, if we do not find an item of legislation with this URI ...
+    else
+      
+      # ... we set the not found page meta information ...
+      @page_title = 'Enabling legislation not found'
+      @multiline_page_title = "Enabling legislation <span class='subhead'>Not found</span>".html_safe
+      @description = 'We were unable to find that item of enabling legislation.'
+      @crumb << { label: 'Enabling legislation', url: enabling_legislation_list_url }
+      @crumb << { label: 'Not found', url: nil }
+      @section = 'enabling-legislation'
+      
+      # ... and render the not found template, returning a status code of 404.
+      render template: "enabling_legislation_lookup/not_found", status: :not_found, layout: true
     end
   end
 end
