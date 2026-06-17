d902a3ec0475e51466cffe38949f9004e749df32
fantasticlife <michael@smethur.st>
2026-06-17 11:22:40 +0100

    simplifed javascript for bookmarklet to point straight to lookup url

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -1,31 +1,6 @@
 (function () {
-  var url = document.URL;
-  if (
-    window.location.origin == "https://www.legislation.gov.uk" &&
-    window.location.pathname.split("/")[1] == "ukpga"
-  ) {
-    window.location.href =
-      "https://api.parliament.uk/procedure-browser/enabling-legislation/lookup?legislation-gov-uk-uri=" +
-      window.location.origin +
-      "/id/ukpga/" +
-      window.location.pathname.split("/")[2] +
-      "/" +
-      window.location.pathname.split("/")[3];
-  } else if (
-    window.location.origin == "https://www.legislation.gov.uk" &&
-    window.location.pathname.split("/")[1] == "ukdsi"
-  ) {
-    window.location.href =
-      "https://api.parliament.uk/procedure-browser/work-packageable-things/lookup?legislation-gov-uk-uri=" +
-      window.location;
-  } else if (
-    window.location.origin == "https://www.legislation.gov.uk" &&
-    window.location.pathname.split("/")[1] == "uksi"
-  ) {
-    window.location.href =
-      "https://api.parliament.uk/procedure-browser/work-packageable-things/lookup?legislation-gov-uk-uri=" +
-      window.location;
-  } else {
-    alert("Sorry. This URL is not flippable.");
-  }
+	if (window.location.origin == 'https://www.legislation.gov.uk' ) {
+	window.location.href ='https://api.parliament.uk/procedure-browser/lookup?legislation-gov-uk-uri=' + window.location; }
+
+	else {alert('Sorry. This URL is not flippable.')};
 })();
