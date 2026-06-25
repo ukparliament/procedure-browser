e990089ae7ce59e4bc60a993caab5104eed529fa
fantasticlife <michael@smethur.st>
2026-06-25 17:35:08 +0100

    work package reading list controller plugged into query

--- app/controllers/work_package_reading_list_controller.rb
+++ app/controllers/work_package_reading_list_controller.rb
@@ -3,6 +3,8 @@ class WorkPackageReadingListController < ApplicationController
   # We include modules required to get all work packages and a work package.
   include Sparql::Get::WorkPackage
   include Sparql::Queries::WorkPackage
+  include Sparql::Get::WorkPackageBusinessItemsReadingList
+  include Sparql::Queries::WorkPackageBusinessItemsReadingList
   include Sparql::Get::Response
   include Timeline::Timeline
 
@@ -12,6 +14,9 @@ class WorkPackageReadingListController < ApplicationController
     # We get the work package.
     @work_package = get_work_package( work_package_id )
     
+    # We get the business items being the reading list for the work package.
+    @business_items = get_work_package_business_items_reading_list( work_package_id )
+
     @page_title = "Reading list for #{@work_package.work_packageable_thing_label}"
     @multiline_page_title = "#{@work_package.work_packageable_thing_label} <span class='subhead'>Reading list</span>".html_safe
     @description = "A reading list for #{@work_package.work_packageable_thing_label}."
