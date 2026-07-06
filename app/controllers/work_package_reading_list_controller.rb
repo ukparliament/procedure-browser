84eaad2e21bc09eb9d51e7276ba44f7a49a3e67f
fantasticlife <michael@smethur.st>
2026-07-06 21:50:16 +0100

    work package reading list controller issues redirect to documents

--- app/controllers/work_package_reading_list_controller.rb
+++ app/controllers/work_package_reading_list_controller.rb
@@ -1,30 +1,7 @@
 class WorkPackageReadingListController < ApplicationController
 
-  # We include modules required to get all work packages and a work package.
-  include Sparql::Get::WorkPackage
-  include Sparql::Queries::WorkPackage
-  include Sparql::Get::WorkPackageBusinessItemsReadingList
-  include Sparql::Queries::WorkPackageBusinessItemsReadingList
-  include Sparql::Get::Response
-  include Timeline::Timeline
 
   def index
-    work_package_id = params[:work_package]
-  
-    # We get the work package.
-    @work_package = get_work_package( work_package_id )
-    
-    # We get the business items being the reading list for the work package.
-    @business_items = get_work_package_business_items_reading_list( work_package_id )
-
-    @page_title = "Reading list for #{@work_package.work_packageable_thing_label}"
-    @multiline_page_title = "#{@work_package.work_packageable_thing_label} <span class='subhead'>Reading list</span>".html_safe
-    @description = "A reading list for #{@work_package.work_packageable_thing_label}."
-    @rss_url = work_package_reading_list_list_url( :format => 'rss' )
-    @crumb << { label: 'Work packages', url: work_package_list_url }
-    @crumb << { label: @work_package.work_packageable_thing_label, url: work_package_show_url }
-    @crumb << { label: 'Reading list', url: nil }
-    @section = 'work-packages'
-    @subsection = 'reading-list'
+    redirect_to( work_package_document_list_url, :status => :moved_permanently )
   end
 end
